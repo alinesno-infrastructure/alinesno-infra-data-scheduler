2a5276fb70803c3ad42c70674f3653176bb17c64
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:47 +0800

    新增 `TaskDefinitionEntity` 类，定义任务相关字段及注释。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/entity/TaskDefinitionEntity.java
@@ -0,0 +1,94 @@
+package com.alinesno.infra.data.scheduler.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Map;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("task_definition")
+@TableComment(value = "任务定义表")
+public class TaskDefinitionEntity extends InfraBaseEntity {
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.BIGINT, length = 32)
+    @ColumnComment("任务编码")
+    private long code;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.BIGINT, length = 32)
+    @ColumnComment("流程编码")
+    private long processCode ;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
+    @ColumnComment("任务名称")
+    private String name;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
+    @ColumnComment("任务描述")
+    private String description;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
+    @ColumnComment("项目编码")
+    private long projectCode;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
+    @ColumnComment("任务类型")
+    private String taskType;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
+    @ColumnComment("环境编码")
+    private long environmentCode;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
+    @ColumnComment("任务参数")
+    private String taskParams;
+
+    @TableField(exist = false)
+    private Map<String, String> taskParamMap;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.INT)
+    @ColumnComment("失败重试次数")
+    private int failRetryTimes;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.INT)
+    @ColumnComment("失败重试间隔")
+    private int failRetryInterval;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.INT)
+    @ColumnComment("超时时间")
+    private int timeout;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.INT)
+    @ColumnComment("延迟时间")
+    private int delayTime;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 512)
+    @ColumnComment("资源ID")
+    private String resourceId;
+
+    @TableField
+    @ColumnType(value = MySqlTypeConstant.INT)
+    @ColumnComment("任务排序")
+    private int orderNum;
+
+}
