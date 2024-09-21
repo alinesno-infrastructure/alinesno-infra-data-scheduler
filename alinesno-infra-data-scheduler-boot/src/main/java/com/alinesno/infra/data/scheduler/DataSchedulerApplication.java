7dd3557b12c7a8ba93baf189b19583d84d9413b6
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:47 +0800

    新增DataSchedulerApplication类，集成Spring Boot应用启动。

---
+++ alinesno-infra-data-scheduler-boot/src/main/java/com/alinesno/infra/data/scheduler/DataSchedulerApplication.java
@@ -0,0 +1,18 @@
+package com.alinesno.infra.data.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 集成一个Java开发示例工具
+ * @author LuoAnDong
+ * @since 2023年8月3日 上午6:23:43
+ */
+@SpringBootApplication
+public class DataSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSchedulerApplication.class, args);
+	}
+
+}
