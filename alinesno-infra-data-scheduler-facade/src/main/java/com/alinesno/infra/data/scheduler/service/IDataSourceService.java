024690fe8299965ad7f21de07d2e4596af87d0e1
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:48 +0800

    新增IDataSourceService接口实现。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/IDataSourceService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.entity.DataSourceEntity;
+
+public interface IDataSourceService extends IBaseService<DataSourceEntity> {
+}
