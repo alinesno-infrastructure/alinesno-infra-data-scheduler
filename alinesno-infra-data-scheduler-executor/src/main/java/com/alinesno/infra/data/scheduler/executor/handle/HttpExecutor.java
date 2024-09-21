fc6bfbae81ad2e1f1d43d72bdd939575340e166d
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:50 +0800

    新增HttpExecutor类，实现HTTP任务执行逻辑。

---
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/HttpExecutor.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.scheduler.executor.handle;
+
+import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
+import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class HttpExecutor extends BaseExecutorService {
+
+    @Override
+    public void execute(TaskInfoBean taskInfo) {
+        log.debug("HttpExecutor execute");
+    }
+}
