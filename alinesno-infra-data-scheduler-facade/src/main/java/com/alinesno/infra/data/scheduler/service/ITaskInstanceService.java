9e5d0ba64011c0f11fa66c58dcc7cb5a3136eab4
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:49 +0800

    修复并完善了TaskInstanceService接口的实现，确保与TaskInstanceEntity的交互逻辑正确无误。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/ITaskInstanceService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.entity.TaskInstanceEntity;
+
+public interface ITaskInstanceService extends IBaseService<TaskInstanceEntity> {
+}
