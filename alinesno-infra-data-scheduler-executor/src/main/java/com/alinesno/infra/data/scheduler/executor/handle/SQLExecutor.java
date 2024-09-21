077139e19c65e7f04d8d16fd6627e555ec8c0719
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:49 +0800

    新增SQLExecutor类，实现任务执行逻辑。

---
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/SQLExecutor.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.data.scheduler.executor.handle;
+
+import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
+import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SQLExecutor extends BaseExecutorService {
+
+    @Override
+    public void execute(TaskInfoBean task) {
+        log.debug("SQL Executor");
+    }
+
+}
