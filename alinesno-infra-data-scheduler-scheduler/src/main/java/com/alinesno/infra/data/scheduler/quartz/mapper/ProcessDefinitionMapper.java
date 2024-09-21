214e0cddee6bf7f4d68283e89cb9a5c38b88239e
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:49 +0800

    新增ProcessDefinitionMapper接口文件。

---
+++ alinesno-infra-data-scheduler-scheduler/src/main/java/com/alinesno/infra/data/scheduler/quartz/mapper/ProcessDefinitionMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.data.scheduler.quartz.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.entity.ProcessDefinitionEntity;
+
+/**
+ * ProcessDefinitionMapper接口
+ */
+public interface ProcessDefinitionMapper extends IBaseMapper<ProcessDefinitionEntity> {
+}
