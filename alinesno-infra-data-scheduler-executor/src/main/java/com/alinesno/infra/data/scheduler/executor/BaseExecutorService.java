2c6f99b95f5332fd14409607b95541fbee3fba4c
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:48 +0800

    新增BaseExecutorService类，作为执行器服务的基础实现。

---
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/BaseExecutorService.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.scheduler.executor;
+
+import com.alinesno.infra.data.scheduler.entity.TaskDefinitionEntity;
+import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+/**
+ * BaseExecutorService
+ */
+@Slf4j
+public abstract class BaseExecutorService implements IExecutorService {
+
+
+}
