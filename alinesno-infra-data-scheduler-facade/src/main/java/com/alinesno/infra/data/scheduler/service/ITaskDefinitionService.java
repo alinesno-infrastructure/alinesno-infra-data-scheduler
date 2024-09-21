950bdaded50943025cdda54129ae4a23bb15bc8e
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:49 +0800

    新增 TaskDefinitionService 接口实现。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/ITaskDefinitionService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.entity.TaskDefinitionEntity;
+
+public interface ITaskDefinitionService extends IBaseService<TaskDefinitionEntity> {
+}
