0468fdebe8ad8c44a9336d70df09876bc325356b
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:50 +0800

    新增IResourceService接口实现，为资源管理提供服务。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/IResourceService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.entity.ResourceEntity;
+
+public interface IResourceService extends IBaseService<ResourceEntity> {
+}
