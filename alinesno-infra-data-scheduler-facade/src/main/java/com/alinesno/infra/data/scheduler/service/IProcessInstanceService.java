957ace5b90e798b29329d3f7e38dfb7aa1f38886
李娜(Allen) <allenAip@gmail.com>
2024-09-21 08:06:48 +0800

    新增IProcessInstanceService接口实现。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/IProcessInstanceService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.entity.ProcessInstanceEntity;
+
+public interface IProcessInstanceService extends IBaseService<ProcessInstanceEntity> {
+}
