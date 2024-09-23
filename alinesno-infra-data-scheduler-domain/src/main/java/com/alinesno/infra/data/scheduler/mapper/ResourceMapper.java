fd6efa6377299fccb250dd9814fa2c45d467411b
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    新增 ResourceMapper 接口文件。

---
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/mapper/ResourceMapper.java
@@ -0,0 +1,17 @@
+package com.alinesno.infra.data.scheduler.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.entity.EnvironmentEntity;
+import com.alinesno.infra.data.scheduler.entity.ResourceEntity;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface ResourceMapper extends IBaseMapper<ResourceEntity> {
+
+}
