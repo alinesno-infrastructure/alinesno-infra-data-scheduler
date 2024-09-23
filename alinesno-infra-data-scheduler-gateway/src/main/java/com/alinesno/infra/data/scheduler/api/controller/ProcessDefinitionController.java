7f9da30e5556932d41cda7ba1f0189b2f04375b9
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:56 +0800

    创建了 `ProcessDefinitionController` 控制器类，包含数据表格获取接口和分类树选择接口。

---
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/ProcessDefinitionController.java
@@ -0,0 +1,64 @@
+package com.alinesno.infra.data.scheduler.api.controller;
+
+import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
+import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.data.scheduler.entity.ProcessDefinitionEntity;
+import com.alinesno.infra.data.scheduler.service.ICategoryService;
+import com.alinesno.infra.data.scheduler.service.IProcessDefinitionService;
+import io.swagger.annotations.Api;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 处理与TransEntity相关的请求的Controller。
+ * 继承自BaseController类并实现ITransService接口。
+ *
+ * @version 1.0.0
+ * @author  luoxiaodong
+ */
+@Slf4j
+@Api(tags = "Trans")
+@RestController
+@Scope(SpringInstanceScope.PROTOTYPE)
+@RequestMapping("/api/infra/data/scheduler/processDefinition")
+public class ProcessDefinitionController extends BaseController<ProcessDefinitionEntity, IProcessDefinitionService> {
+
+    @Autowired
+    private IProcessDefinitionService service;
+
+    @Autowired
+    private ICategoryService catalogService ;
+
+    /**
+     * 获取TransEntity的DataTables数据。
+     *
+     * @param request HttpServletRequest对象。
+     * @param model Model对象。
+     * @param page DatatablesPageBean对象。
+     * @return 包含DataTables数据的TableDataInfo对象。
+     */
+    @ResponseBody
+    @PostMapping("/datatables")
+    public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
+        log.debug("page = {}", ToStringBuilder.reflectionToString(page));
+        return this.toPage(model, this.getFeign(), page);
+    }
+
+    @GetMapping("/catalogTreeSelect")
+    public AjaxResult catalogTreeSelect(){
+        return AjaxResult.success("success" , catalogService.selectCatalogTreeList()) ;
+    }
+
+    @Override
+    public IProcessDefinitionService getFeign() {
+        return this.service;
+    }
+}
