d72d53ab2ef0340be300cac8c4633a264b1a8bd6
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    优化了资源管理页面布局，添加了侧边栏菜单与文件列表组件。

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/resources/index.vue
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/resources/index.vue
@@ -1,15 +1,31 @@
 <template>
-
-    <div>
-        hello world !!
+    <div class="app-container" style="padding:10px;margin-top:0px">
+      <el-container class="el-container">
+          <AsideMenu></AsideMenu>
+          <el-main class="el-main">
+            <!-- <router-view></router-view> -->
+             <FileList />
+          </el-main>
+      </el-container>
     </div>
-
-</template>
-
-<script>
-</script>
-
-<style scoped lang="scss">
-</style>
-
-
+  </template>
+  
+  <script setup>
+  import AsideMenu from './AsideMenu'
+  import FileList from './FileList'
+  
+  </script>
+  
+  <style scoped>
+  
+  .file-wrapper {
+    width: 100% !important;
+  }
+  
+  
+  .el-main {
+    padding: 0px 16px !important;
+    overflow: hidden;
+  }
+  </style>
+  
