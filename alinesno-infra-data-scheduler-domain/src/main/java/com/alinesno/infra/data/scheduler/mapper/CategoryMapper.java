181591eff9d1778ff7eefcc4b59e41dd4378e1b1
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:56 +0800

    新增 CategoryMapper 接口文件。

---
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/mapper/CategoryMapper.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.data.scheduler.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.entity.CategoryEntity;
+
+/**
+ * 【请填写功能名称】Mapper接口
+ *
+ * @version 1.0.0
+ * @autor luoxiaodong
+ */
+public interface CategoryMapper extends IBaseMapper<CategoryEntity> {
+}
