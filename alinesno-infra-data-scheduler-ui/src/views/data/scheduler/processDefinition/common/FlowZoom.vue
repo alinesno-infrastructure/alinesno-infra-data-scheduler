b4496e046aa60c79aa479f1327921d3f2988c056
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    创建 FlowZoom 组件文件。

---
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/FlowZoom.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flow-design-operate">
+    <!-- <div class="flow-fullscreen">
+      <span class="flow-fullscreen-icon"></span>
+    </div>  -->
+    <div class="flow-design-zoom">
+      <div class="flow-design-zoom-less" @click="onZoomOut"></div>
+      <span>{{ zoomValue }}%</span>
+      <div class="flow-design-zoom-more" @click="onZoomIn"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const INIT_ZOOM_VALUE = ref(100);
+const MIN_ZOOM_VALUE = ref(50);
+const MAX_ZOOM_VALUE = ref(200);
+
+</script>
