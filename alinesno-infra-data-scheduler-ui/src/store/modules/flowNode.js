a0d552988f65ee920d94b743107f0ddb1e510085
林晓(Lily) <lilyAip@gmail.com>
2024-09-23 23:54:36 +0800

    feat:重构 flowNode.js 文件，引入 Pinia 库并优化节点管理逻辑

--- alinesno-infra-data-scheduler-ui/src/store/modules/flowNode.js
+++ alinesno-infra-data-scheduler-ui/src/store/modules/flowNode.js
@@ -1,75 +1,63 @@
-
-import { getStartNode, addNode, addBranch, delNode, delBranchNode, addCondition, updateNode } from '@/utils/nodeUtil';
+import { defineStore } from 'pinia';
+import { getStartNode } from '@/utils/nodeUtil';
 
 const flowNodeStore = defineStore(
   'flowNode',
   {
     state: () => ({
-      //  节点数据
-      node: getStartNode(),
-      //  缩略图
-      mapImg: '',
-      // 意见分支
-      suggestBranchEnable: true,
-      // 并行节点
-      parallelBranchEnable: true,
+      // 节点数据数组
+      nodes: [getStartNode()],
+      // 缩略图
+      mapImg: ''
     }),
+    getters: {
+      // 可以在这里定义任何需要的 getter
+      currentNode: (state) => state.nodes,
+      currentMapImage: (state) => state.mapImg
+    },
     actions: {
       /**
-       *  初始节点
+       * 设置当前节点
+       * @param {Object} state - 当前状态对象
+       * @param {Object} node - 新的节点对象
        */
       setNode(state, node) {
-        console.log('state = ' + state + ' , node = ' + node)
-        if (node) {
-          this.node = node;
-        } else {
-          this.node = getStartNode();
+        // 这里假设你想要替换整个节点数组中的某个特定节点
+        // 如果你需要替换第一个节点（例如初始节点），你可以这样做：
+        if (state.nodes.length > 0) {
+          state.nodes[0] = node;
         }
-      },
-      /**
-       *  获取getters
-       */
-      getters() {
-        return {
-          node: this.node,
-          mapImg: this.mapImg,
-          suggestBranchEnable: this.suggestBranchEnable,
-          parallelBranchEnable: this.parallelBranchEnable,
-        }; 
       },
       /**
        * 添加节点
+       * @param {Object} data - 包含新节点信息的对象
        */
-      addNode(state, data) {
-        console.log('state = ' + state + ' , data = ' + data.nodeType);
-        if (data.nodeType == 0) {
-          //  开始
-          if (this.node.hasOwnProperty('name')) {
-            data.addNode.childNode = this.node;
-            data.addNode.childNode.pid = data.addNode.id;
-            data.addNode.pid = 0;
-          }
-          this.node = data.addNode;
-        } else {
-          if (data.id) {
-            data.currNode.conditionNodes.forEach((conditionNode, i) => {
-              if (conditionNode.id == data.id) {
-                // 获取当前操作节点
-                addNode(this.node, conditionNode, data.addNode);
-              }
-            });
+      addNode(data) {
+        console.log('data.nodeType = ', JSON.stringify(data));
+
+        const { addNode, currNode } = data;
+
+        // 检查 currNode 是否存在且有 id 属性
+        if (currNode && currNode.id) {
+          // 找到当前的 currNode
+          const currentIndex = this.nodes.findIndex(node => node.id === currNode.id);
+
+          if (currentIndex !== -1) {
+            // 在当前 currNode 之后插入新节点
+            this.nodes.splice(currentIndex + 1, 0, addNode);
           } else {
-            // 获取当前操作节点
-            addNode(this.node, data.currNode, data.addNode);
+            // 如果找不到 currNode，则直接添加到数组末尾
+            this.nodes.push(addNode);
           }
+        } else {
+          // 如果 currNode 不存在或为空对象，直接添加到数组末尾
+          this.nodes.push(addNode);
         }
-        // 更新地图
-        // updateMap(state);
-        //console.log('node', state.node);
-        console.info('nodeJson ' + JSON.stringify(this.node));
+
+        console.info('Updated nodes: ', JSON.stringify(this.nodes));
       }
     }
-  })
+  }
+)
 
 export default flowNodeStore
-
