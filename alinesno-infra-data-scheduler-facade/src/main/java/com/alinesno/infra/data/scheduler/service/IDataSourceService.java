8c24e880f42120060e31154d56340d085020c35d
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    feat:添加数据库连接校验接口 checkDbConnect

--- alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/IDataSourceService.java
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/IDataSourceService.java
@@ -1,7 +1,16 @@
 package com.alinesno.infra.data.scheduler.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.api.CheckDbConnectResult;
 import com.alinesno.infra.data.scheduler.entity.DataSourceEntity;
 
 public interface IDataSourceService extends IBaseService<DataSourceEntity> {
+
+    /**
+     * 数据库连接校验
+     * @param dbListEntity
+     * @return
+     */
+    CheckDbConnectResult checkDbConnect(DataSourceEntity dbListEntity);
+
 }
