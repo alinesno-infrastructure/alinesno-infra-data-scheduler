34b2aa3cc9348369969cc734d42ea22f3e375100
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    新增 ResourceServiceImpl 类，实现 ResourceService 接口。

---
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/service/impl/ResourceServiceImpl.java
@@ -0,0 +1,19 @@
+package com.alinesno.infra.data.scheduler.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.data.scheduler.entity.ResourceEntity;
+import com.alinesno.infra.data.scheduler.mapper.ResourceMapper;
+import com.alinesno.infra.data.scheduler.service.IResourceService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 【请填写功能名称】Service业务层处理
+ *
+ * @version 1.0.0
+ * @autor luoxiaodong
+ */
+@Slf4j
+@Service
+public class ResourceServiceImpl extends IBaseServiceImpl<ResourceEntity, ResourceMapper> implements IResourceService {
+}
