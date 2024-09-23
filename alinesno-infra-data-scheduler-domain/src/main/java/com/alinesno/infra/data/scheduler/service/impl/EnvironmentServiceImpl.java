4f7d17677dcc21f3a7ccee79cb1872b6339d5ed4
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:56 +0800

    创建了 `EnvironmentServiceImpl` 类，实现了环境服务的业务逻辑。

---
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/service/impl/EnvironmentServiceImpl.java
@@ -0,0 +1,19 @@
+package com.alinesno.infra.data.scheduler.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.data.scheduler.entity.EnvironmentEntity;
+import com.alinesno.infra.data.scheduler.mapper.EnvironmentMapper;
+import com.alinesno.infra.data.scheduler.service.IEnvironmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 【请填写功能名称】Service业务层处理
+ *
+ * @version 1.0.0
+ * @autor luoxiaodong
+ */
+@Slf4j
+@Service
+public class EnvironmentServiceImpl extends IBaseServiceImpl<EnvironmentEntity, EnvironmentMapper> implements IEnvironmentService {
+}
