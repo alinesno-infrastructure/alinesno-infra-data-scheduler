fd46c5c934829a71235fc0fe6f100efb45239ff3
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    新增TaskDefinitionServiceImpl实现类，提供任务定义服务接口的具体实现。

---
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/service/impl/TaskDefinitionServiceImpl.java
@@ -0,0 +1,19 @@
+package com.alinesno.infra.data.scheduler.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.data.scheduler.entity.TaskDefinitionEntity;
+import com.alinesno.infra.data.scheduler.mapper.TaskDefinitionMapper;
+import com.alinesno.infra.data.scheduler.service.ITaskDefinitionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 【请填写功能名称】Service业务层处理
+ *
+ * @version 1.0.0
+ * @autor luoxiaodong
+ */
+@Slf4j
+@Service
+public class TaskDefinitionServiceImpl extends IBaseServiceImpl<TaskDefinitionEntity, TaskDefinitionMapper> implements ITaskDefinitionService {
+}
