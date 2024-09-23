b0c0aadfe8784a90853e582fdbd8185b41b0c23e
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:56 +0800

    新增 TaskDefinitionMapper 接口文件。

---
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/mapper/TaskDefinitionMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.scheduler.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.entity.TaskDefinitionEntity;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface TaskDefinitionMapper extends IBaseMapper<TaskDefinitionEntity> {
+
+}
