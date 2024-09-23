7da397ef6429eba84404174f82cb76fe2b382816
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    新增流程节点展示组件 index.vue

---
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/FlowNode/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flow-row-container">
+    <FlowApproverNode v-if="node" :node="node" :readable="readable" />
+    <FlowNode v-if="node && node.childNode && node.childNode.hasOwnProperty('name')" :node="node.childNode" :readable="readable" />
+  </div>
+</template>
+
+<script setup>
+
+import FlowApproverNode from './Approver/index.vue';
+
+const props = defineProps({
+  node: {
+    type: Object,
+    default: () => ({}),  // 对象类型的默认值应通过函数返回
+  },
+  readable: {
+    type: Boolean,
+    default: false,  // 基本类型可以直接指定默认值
+  },
+});
+
+</script>
