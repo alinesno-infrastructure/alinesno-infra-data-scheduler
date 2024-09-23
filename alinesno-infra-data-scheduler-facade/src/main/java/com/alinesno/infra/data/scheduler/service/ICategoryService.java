174634f60c6e6175b2ce21b80a4cd8aa7b09b868
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    新增 CategoryService 接口及相关方法。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/ICategoryService.java
@@ -0,0 +1,35 @@
+package com.alinesno.infra.data.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.api.TreeSelectDto;
+import com.alinesno.infra.data.scheduler.entity.CategoryEntity;
+
+import java.util.List;
+
+/**
+ * 【请填写功能名称】Service接口
+ *
+ * @version 1.0.0
+ * @autor luoxiaodong
+ */
+public interface ICategoryService extends IBaseService<CategoryEntity> {
+    /**
+     * 查询出指令类型列表
+     * @param promptCatalog
+     * @return
+     */
+    List<CategoryEntity> selectCatalogList(CategoryEntity promptCatalog);
+
+    /**
+     * 保存用户类型
+     * @param entity
+     */
+    void insertCatalog(CategoryEntity entity);
+
+    /**
+     * 查询类型列表树
+     * @return
+     */
+    List<TreeSelectDto> selectCatalogTreeList();
+    
+}
