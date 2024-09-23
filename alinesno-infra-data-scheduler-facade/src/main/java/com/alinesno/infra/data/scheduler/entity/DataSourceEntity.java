aeed3f08be1c96657d0919aa85dd77a257d0e613
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    feat:优化DataSourceEntity类，添加读取源相关字段并加密处理。

--- alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/entity/DataSourceEntity.java
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/entity/DataSourceEntity.java
@@ -1,12 +1,11 @@
 package com.alinesno.infra.data.scheduler.entity;
 
+import cn.afterturn.easypoi.excel.annotation.Excel;
 import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
-import com.alinesno.infra.data.scheduler.enums.DbTypeEnums;
+import com.alinesno.infra.common.security.mapper.AESEncryptHandler;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
-import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
-import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -15,27 +14,80 @@ import lombok.EqualsAndHashCode;
 @TableName("datasource")
 public class DataSourceEntity extends InfraBaseEntity {
 
-  @TableField(exist = false)
-  private String userName;
+  @ColumnComment("描述")
+  @Excel(name = "描述")
+  @TableField("reader_desc")
+  private String readerDesc;
 
-  @TableField
-  @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
-  @ColumnComment("数据库名称")
-  private String name;
+  // fields
+  /**
+   * 读取源名称
+   */
+  @ColumnComment("读取源名称")
+  @Excel(name = "读取源名称")
+  @TableField("reader_name")
+  private String readerName;
+  /**
+   * 读取源连接
+   */
+  @ColumnComment("读取源连接")
+  @Excel(name = "读取源连接")
+  @TableField(value = "reader_url" , typeHandler = AESEncryptHandler.class)
+  private String readerUrl;
 
-  @TableField
-  @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
-  @ColumnComment("数据库描述")
-  private String note;
+  /**
+   * 读取源用户名
+   */
+  @ColumnComment("读取源用户名")
+  @Excel(name = "读取源用户名")
+  @TableField(value = "reader_username" , typeHandler = AESEncryptHandler.class)
+  private String readerUsername;
+  /**
+   * 读取源密码
+   */
+  @ColumnComment("读取源密码")
+  @Excel(name = "读取源密码")
+  @TableField(value = "reader_passwd" , typeHandler = AESEncryptHandler.class)
+  private String readerPasswd;
 
-  @TableField
-  @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 128)
-  @ColumnComment("数据库类型")
-  private DbTypeEnums type;
+  /**
+   * 读取源连接
+   */
+  @ColumnComment("access_key")
+  @Excel(name = "access_key")
+  @TableField(value = "access_key" , typeHandler = AESEncryptHandler.class)
+  private String accessKey ;
 
-  @TableField
-  @ColumnType(value = MySqlTypeConstant.LONGTEXT)
-  @ColumnComment("数据库连接参数")
-  private String connectionParams;
+  @ColumnComment("secret_key")
+  @Excel(name = "secret_key")
+  @TableField(value = "secret_key" , typeHandler = AESEncryptHandler.class)
+  private String secretKey ;
+
+  /**
+   * 读取源连接端口
+   */
+  @ColumnComment("读取源连接端口")
+  @Excel(name = "读取源连接端口")
+  @TableField("reader_port")
+  private String readerPort;
+
+  /**
+   * 读取源类型
+   */
+  @ColumnComment("读取源类型")
+  @Excel(name = "读取源类型")
+  @TableField("reader_type")
+  private String readerType;
+
+  @ColumnComment("数据来源组织")
+  @Excel(name = "数据来源组织")
+  @TableField("owner")
+  private String owner;
+
+  // 类型（读取/写入)
+  @ColumnComment("类型（读取/写入)")
+  @Excel(name = "类型（读取/写入)")
+  @TableField("operation_type")
+  private String operationType;
 
 }
