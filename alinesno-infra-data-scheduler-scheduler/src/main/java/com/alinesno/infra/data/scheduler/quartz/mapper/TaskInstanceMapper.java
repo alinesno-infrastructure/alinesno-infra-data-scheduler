fc660c0e066669a3daf98151a35433eaa9a84765
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    新增TaskInstanceMapper接口文件。

---
+++ alinesno-infra-data-scheduler-scheduler/src/main/java/com/alinesno/infra/data/scheduler/quartz/mapper/TaskInstanceMapper.java
@@ -0,0 +1,11 @@
+package com.alinesno.infra.data.scheduler.quartz.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.entity.TaskInstanceEntity;
+
+/**
+ * ProcessDefinitionMapper接口
+ */
+public interface TaskInstanceMapper extends IBaseMapper<TaskInstanceEntity> {
+
+}
