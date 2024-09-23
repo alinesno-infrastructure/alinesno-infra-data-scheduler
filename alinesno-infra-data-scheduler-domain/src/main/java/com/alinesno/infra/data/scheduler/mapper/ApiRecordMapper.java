0cade99df0f6efc09da3715bf2c62e3301e63c2e
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    创建了 ApiRecordMapper 接口文件，用于 API 记录的持久层操作。

---
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/mapper/ApiRecordMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.scheduler.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.entity.ApiRecordEntity;
+
+/**
+ * <p>
+ * 持久层接口
+ * </p>
+ *
+ * @author luoxiaodong
+ * @version 1.0.0
+ */
+public interface ApiRecordMapper extends IBaseMapper<ApiRecordEntity> {
+
+}
