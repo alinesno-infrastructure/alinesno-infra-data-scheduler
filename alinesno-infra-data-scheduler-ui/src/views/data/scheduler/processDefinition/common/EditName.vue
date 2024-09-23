02e122b774c8707c7405d3f4ce6c95593f1bc883
李娜(Allen) <allenAip@gmail.com>
2024-09-23 22:13:57 +0800

    修改EditName.vue文件，添加基于v-if和v-focus的条件渲染，优化节点名称编辑体验。

---
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/EditName.vue
@@ -0,0 +1,10 @@
+<template>
+  <span class="node-name-title">
+    <span v-if="!isInput" @click.stop="clickEvent()">{{ value }}</span>
+    <a-input v-if="isInput" type="text" @blur="blurEvent()" @focus="$event.currentTarget.select()" v-focus v-model="value" :style="{ width: width }" />
+  </span>
+</template>
+
+<script setup>
+  import { flowMixin } from './flowMixin';
+</script>
