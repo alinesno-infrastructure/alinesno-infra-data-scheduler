d78e753bddcf302af2955eadba4f4b877352255c
李娜(Allen) <allenAip@gmail.com>
2024-09-24 23:01:35 +0800

    调整FlowZoom.vue文件，优化缩放功能显示与控制。

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/FlowZoom.vue
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/FlowZoom.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="flow-design-operate">
-    <!-- <div class="flow-fullscreen">
-      <span class="flow-fullscreen-icon"></span>
-    </div>  -->
+
     <div class="flow-design-zoom">
       <div class="flow-design-zoom-less" @click="onZoomOut"></div>
       <span>{{ zoomValue }}%</span>
@@ -13,8 +11,6 @@
 
 <script setup>
 
-const INIT_ZOOM_VALUE = ref(100);
-const MIN_ZOOM_VALUE = ref(50);
-const MAX_ZOOM_VALUE = ref(200);
+const zoomValue = ref(100)
 
 </script>
