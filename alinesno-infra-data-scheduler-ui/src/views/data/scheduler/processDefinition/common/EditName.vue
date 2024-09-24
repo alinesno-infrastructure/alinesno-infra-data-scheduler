efcf2ae2ae1d2b07764cde6009bf6e93cb7cc442
李娜(Allen) <allenAip@gmail.com>
2024-09-24 23:01:35 +0800

    优化 EditName 组件，增加输入框功能并实现在点击后启用输入与点击外禁用输入，并同步更新节点名称。

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/EditName.vue
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/EditName.vue
@@ -1,10 +1,44 @@
 <template>
   <span class="node-name-title">
-    <span v-if="!isInput" @click.stop="clickEvent()">{{ value }}</span>
-    <a-input v-if="isInput" type="text" @blur="blurEvent()" @focus="$event.currentTarget.select()" v-focus v-model="value" :style="{ width: width }" />
+    <span v-if="!isInput" @click.stop="clickEvent">{{ editValue }}</span>
+    <el-input v-else type="text" @blur="blurEvent" v-model="editValue" :style="{ width: width }" />
   </span>
 </template>
 
 <script setup>
-  import { flowMixin } from './flowMixin';
+import flowNodeStore from '@/store/modules/flowNode'
+
+// 定义 props
+const props = defineProps({
+  value: {
+    type: String,
+    required: false,
+  },
+  nodeId: {
+    type: String,
+    required: false,
+  },
+  width: {
+    type: String,
+    required: false,
+    default: '85%',
+  },
+});
+
+// 使用 ref 来创建一个可以被修改的 value 变量
+const editValue = ref(props.value);
+
+// 定义响应式变量
+const isInput = ref(false);
+
+// 方法
+const clickEvent = () => {
+  isInput.value = true;
+};
+
+const blurEvent = () => {
+  isInput.value = false;
+  console.log('editValue:' + editValue.value);
+  flowNodeStore().updateNodeName(props.nodeId,editValue.value);
+};
 </script>
