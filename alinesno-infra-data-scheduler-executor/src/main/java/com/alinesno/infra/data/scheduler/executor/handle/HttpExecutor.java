ec3629de3174186b17bfc4769e5f187f1ffd6164
陈伟(Victor) <victorAip@gmail.com>
2024-09-26 00:17:14 +0800

    修复HttpExecutor服务类的@Service注解并添加服务名标签

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/HttpExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/HttpExecutor.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@Service
+@Service("httpExecutor")
 public class HttpExecutor extends BaseExecutorService {
 
     @Override
