6f860c2f76913ecf93d16223b1e32dce2cc97113
陈伟(Victor) <victorAip@gmail.com>
2024-09-26 00:17:15 +0800

    修复并优化PythonExecutor服务类的注解应用

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/PythonExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/PythonExecutor.java
@@ -3,12 +3,10 @@ package com.alinesno.infra.data.scheduler.executor.handle;
 import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
 import lombok.extern.slf4j.Slf4j;
-
-import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@Service
+@Service("pythonExecutor")
 public class PythonExecutor extends BaseExecutorService {
 
     @Override
