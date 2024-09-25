6fde286da186d6a3c7666259a0cad11ecf573728
陈伟(Victor) <victorAip@gmail.com>
2024-09-26 00:17:15 +0800

    feat:集成Quartz定时任务服务实现，并添加Job和Trigger创建逻辑。

--- alinesno-infra-data-scheduler-scheduler/src/main/java/com/alinesno/infra/data/scheduler/quartz/service/impl/QuartzSchedulerServiceImpl.java
+++ alinesno-infra-data-scheduler-scheduler/src/main/java/com/alinesno/infra/data/scheduler/quartz/service/impl/QuartzSchedulerServiceImpl.java
@@ -1,8 +1,11 @@
 package com.alinesno.infra.data.scheduler.quartz.service.impl;
 
+import com.alinesno.infra.data.scheduler.constants.PipeConstants;
+import com.alinesno.infra.data.scheduler.quartz.job.QuartzJob;
 import com.alinesno.infra.data.scheduler.scheduler.IQuartzSchedulerService;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.Scheduler;
+import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,8 +27,25 @@ public class QuartzSchedulerServiceImpl implements IQuartzSchedulerService {
 
     }
 
+    @SneakyThrows
     @Override
-    public void addJob(String id, String jobCron) {
+    public void addJob(String jobId, String cron) {
 
+        JobDetail jobDetail = JobBuilder.newJob(QuartzJob.class)
+                .usingJobData(PipeConstants.PROCESS_ID, jobId)
+                .withIdentity(jobId , PipeConstants.JOB_GROUP_NAME)
+                .build();//执行
+
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(cron);
+        CronTrigger trigger = TriggerBuilder.newTrigger()
+                .usingJobData(PipeConstants.PROCESS_ID, jobId)
+                .withIdentity(jobId , PipeConstants.TRIGGER_GROUP_NAME)
+                .withSchedule(scheduleBuilder)
+                .startNow()
+                .build();
+
+        scheduler.scheduleJob(jobDetail, trigger);
+
+        log.debug("Quartz 创建了job:{}" , jobDetail.getKey());
     }
 }
