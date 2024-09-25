7d9555b41a06c8c9af56ce1334f79a0703fedafb
陈伟(Victor) <victorAip@gmail.com>
2024-09-26 00:17:15 +0800

    优化MavenExecutor类，添加@Service注解并调整服务引用。

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/MavenExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/MavenExecutor.java
@@ -3,12 +3,10 @@ package com.alinesno.infra.data.scheduler.executor.handle;
 import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
 import lombok.extern.slf4j.Slf4j;
-
-import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@Service
+@Service("mavenExecutor")
 public class MavenExecutor extends BaseExecutorService {
 
     @Override
