d19cc2ba8ba1de533b834f30c2cbc697b7fe686b
陈伟(Victor) <victorAip@gmail.com>
2024-09-26 00:17:15 +0800

    修复ShellExecutor执行逻辑，并添加延迟测试。

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/ShellExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/ShellExecutor.java
@@ -1,18 +1,20 @@
 package com.alinesno.infra.data.scheduler.executor.handle;
 
-import com.alinesno.infra.data.scheduler.entity.TaskDefinitionEntity;
 import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@Service
+@Service("shellExecutor")
 public class ShellExecutor extends BaseExecutorService {
 
+    @SneakyThrows
     @Override
     public void execute(TaskInfoBean taskInfo) {
         log.debug("执行Shell任务");
+        Thread.sleep(5000);
     }
 
 }
