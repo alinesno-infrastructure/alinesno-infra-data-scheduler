95594b894acb2c72783b9932612c33dd2bb932df
刘芳(Luna) <lunaAip@gmail.com>
2024-09-26 19:24:10 +0800

    修复并扩展MavenExecutor类，添加执行逻辑以打包项目。

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/MavenExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/MavenExecutor.java
@@ -2,16 +2,48 @@ package com.alinesno.infra.data.scheduler.executor.handle;
 
 import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.maven.shared.invoker.*;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
 
 @Slf4j
 @Service("mavenExecutor")
 public class MavenExecutor extends BaseExecutorService {
 
+    @SneakyThrows
     @Override
     public void execute(TaskInfoBean task) {
         log.debug("maven executor");
+
+        InvocationRequest request = new DefaultInvocationRequest();
+        request.setPomFile(new File("pom.xml"));
+        request.setGoals(Collections.singletonList("package"));
+
+        Invoker invoker = new DefaultInvoker();
+        invoker.setMavenHome(new File(getM2Home(task.getEnvironment()))) ;
+
+        invoker.setLogger(new PrintStreamLogger(System.err, InvokerLogger.ERROR) {
+
+        });
+
+        invoker.setOutputHandler(new InvocationOutputHandler() {
+            @Override
+            public void consumeLine(String s) throws IOException {
+                log.debug("-->> {}" , s);
+            }
+        });
+
+        invoker.execute(request);
+        if (invoker.execute(request).getExitCode() == 0) {
+            log.debug("success");
+        } else {
+            log.error("error");
+        }
     }
 
 }
