e4fdff85db9e0cbdac513f3be3394484fbc2ade7
刘芳(Luna) <lunaAip@gmail.com>
2024-09-26 19:24:10 +0800

    优化SQLExecutor执行逻辑，添加参数解析与数据源获取，并实现SQL脚本执行。

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/SQLExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/SQLExecutor.java
@@ -1,9 +1,16 @@
 package com.alinesno.infra.data.scheduler.executor.handle;
 
+import com.alinesno.infra.data.scheduler.api.ParamsDto;
 import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Slf4j
 @Service("sqlExecutor")
@@ -11,7 +18,22 @@ public class SQLExecutor extends BaseExecutorService {
 
     @Override
     public void execute(TaskInfoBean task) {
-        log.debug("SQL Executor");
+
+        ParamsDto paramsDto = getParamsDto(task) ;
+        String rawScript = paramsDto.getRawScript();
+
+        log.debug("SQL Executor rawScript: {}", rawScript) ;
+
+        // 创建一个数据源
+        DataSource dataSource = getDataSource(paramsDto.getDataSourceId()) ;
+
+        try (Connection connection = dataSource.getConnection()) {
+            // 执行SQL脚本
+            ScriptUtils.executeSqlScript(connection, new ByteArrayResource(rawScript.getBytes()));
+            log.debug("SQL script executed successfully.");
+        } catch (SQLException e) {
+            log.error("Failed to execute SQL script: " + e.getMessage());
+        }
     }
 
 }
