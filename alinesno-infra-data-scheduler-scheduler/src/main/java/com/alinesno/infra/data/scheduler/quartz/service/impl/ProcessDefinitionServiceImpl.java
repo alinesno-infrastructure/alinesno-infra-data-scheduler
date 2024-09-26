82db66ffb9aa4f334760b2d64e7785b574aa1575
刘芳(Luna) <lunaAip@gmail.com>
2024-09-26 19:24:10 +0800

    修复并优化ProcessDefinitionServiceImpl类的逻辑和方法实现。

--- alinesno-infra-data-scheduler-scheduler/src/main/java/com/alinesno/infra/data/scheduler/quartz/service/impl/ProcessDefinitionServiceImpl.java
+++ alinesno-infra-data-scheduler-scheduler/src/main/java/com/alinesno/infra/data/scheduler/quartz/service/impl/ProcessDefinitionServiceImpl.java
@@ -17,11 +17,17 @@ import com.alinesno.infra.data.scheduler.service.IProcessDefinitionService;
 import com.alinesno.infra.data.scheduler.service.IProcessInstanceService;
 import com.alinesno.infra.data.scheduler.service.ITaskDefinitionService;
 import com.alinesno.infra.data.scheduler.service.ITaskInstanceService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
 import java.util.Date;
 import java.util.List;
 
@@ -41,26 +47,57 @@ public class ProcessDefinitionServiceImpl extends IBaseServiceImpl<ProcessDefini
     @Autowired
     private ITaskInstanceService taskInstanceService ;
 
-    @Async
+    @Value("${alinesno.data.scheduler.workspacePath:#{systemProperties['java.io.tmpdir']}}")
+    private String workspacePath;
+
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)  // 此方法不需要开启事务
     @Override
     public void runProcess(TaskInfoBean task, List<TaskDefinitionEntity> taskDefinition) {
 
-        IExecutorService executorService  ;
-
         // 任务实例运行
         ProcessDefinitionEntity process = task.getProcess() ;
+        process.setRunCount(process.getRunCount()+1);
+        updateById(process) ;
+
+        // 执行运行实例及任务
+        runProcessInstance(task, taskDefinition, process);
+
+        // 更新流程运行成功次数
+        process.setSuccessCount(process.getSuccessCount()+1);
+        updateById(process);
+    }
+
+    /**
+     * 运行流程实例
+     * @param task
+     * @param taskDefinition
+     * @param process
+     */
+    @SneakyThrows
+    private void runProcessInstance(TaskInfoBean task, List<TaskDefinitionEntity> taskDefinition, ProcessDefinitionEntity process) {
+        IExecutorService executorService;
 
         // 记录任务流程实例开始
-        long count = processInstanceService.count() + 1 ;
-        ProcessInstanceEntity processInstance = ProcessUtils.fromTaskToProcessInstance(process , count) ;
+        long count = processInstanceService.count(new LambdaQueryWrapper<ProcessInstanceEntity>().eq(ProcessInstanceEntity::getProcessId , process.getId())) + 1 ;
+        ProcessInstanceEntity processInstance = ProcessUtils.fromTaskToProcessInstance(process, count) ;
+        processInstance.setProcessId(process.getId()) ;
+        processInstance.setState(ProcessStatusEnums.RUNNING.getCode());
+
+        // 生成实例工作空间
+        String instanceWorkspace = workspacePath + File.separator + process.getId() + File.separator + count ;
+        FileUtils.forceMkdir(new File(instanceWorkspace)); // 创建工作空间
+        processInstance.setWorkspace(instanceWorkspace);
+
         processInstanceService.save(processInstance) ;
+
+        task.setWorkspace(instanceWorkspace);
 
         // 任务开始更新状态
         for(TaskDefinitionEntity t : taskDefinition){
             task.setTask(t);
 
             // 记录任务实例开始
-            TaskInstanceEntity taskInstance = ProcessUtils.fromTaskToTaskInstance(process , t) ;
+            TaskInstanceEntity taskInstance = ProcessUtils.fromTaskToTaskInstance(process, t) ;
 
             String beanName = t.getTaskType() + "Executor" ;
             log.debug("TaskExecutor BeanName:{}" , beanName);
@@ -81,10 +118,11 @@ public class ProcessDefinitionServiceImpl extends IBaseServiceImpl<ProcessDefini
     }
 
     @Override
-    public long saveProcessDefinition(ProcessDefinitionDto dto) {
+    public long commitProcessDefinition(ProcessDefinitionDto dto) {
         long projectId = dto.getProjectId() ;
 
         ProcessDefinitionEntity processDefinition = ProcessUtils.fromDtoToEntity(dto) ;
+        processDefinition.setOnline(false);
         this.save(processDefinition) ;
 
         long processId = processDefinition.getId() ;
