db30b3d3ef4967c24ce1ee0083588600b3a0b43d
王磊(Levi) <leviAip@gmail.com>
2024-09-28 17:44:29 +0800

    修复并添加PipeConstants类中的日志文件名常量。

--- alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/constants/PipeConstants.java
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/constants/PipeConstants.java
@@ -6,4 +6,6 @@ public interface PipeConstants {
 
     String TRIGGER_GROUP_NAME = "quartz_scheduler_trigger"; // 触发器组名称
     String JOB_GROUP_NAME = "quartz_job"; // 任务组名称
+
+    String RUNNING_LOGGER = "running.log" ;
 }
