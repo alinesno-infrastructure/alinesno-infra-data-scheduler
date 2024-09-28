c862cddaa53f9ec0393d2a2f54e5a3cdc13c5abc
李伟(Michael) <michaelAip@gmail.com>
2024-09-28 23:56:16 +0800

    feat:优化CheckoutExecutor类，集成GitClone代码并支持参数化配置。

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/CheckoutExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/CheckoutExecutor.java
@@ -1,8 +1,11 @@
 package com.alinesno.infra.data.scheduler.executor.handle;
 
+import com.alinesno.infra.data.scheduler.api.ParamsDto;
 import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
+import com.alinesno.infra.data.scheduler.executor.utils.GitRepositoryUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.CredentialsProvider;
@@ -10,6 +13,8 @@ import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * 使用GitClone代码
@@ -22,24 +27,32 @@ public class CheckoutExecutor extends BaseExecutorService {
     public void execute(TaskInfoBean task) {
         log.debug("checkout executor");
 
-        // 远程仓库的 URL
-        String remoteUrl = "https://github.com/your-username/your-repo.git";
+        ParamsDto paramsDto = getParamsDto(task) ;
 
-        // 本地目标目录
-        File localPath = new File(getWorkspace(task));
+        String remoteUrl = paramsDto.getGitUrl() ;
+        String branch = paramsDto.getGitBranch() ;
+        File localPath = new File(getWorkspace(task) , Objects.requireNonNull(GitRepositoryUtils.getRepositoryNameFromUrl(remoteUrl)));
 
-        // 设置认证信息（可选，对于需要认证的仓库）
-        CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider("your-username", "your-password");
 
         try {
-            log.debug("开始克隆仓库...");
+            writeLog(task , "开始克隆仓库...");
             // 克隆仓库
-            Git.cloneRepository()
+            CloneCommand cloneCommand = Git.cloneRepository()
                     .setURI(remoteUrl)
-                    .setDirectory(localPath)
-                    .setCredentialsProvider(credentialsProvider)  // 如果需要认证，设置认证提供者
-                    .call();
-            log.debug("仓库克隆成功: " + localPath.getAbsolutePath());
+                    .setBranch(branch)
+                    .setDirectory(localPath) ;
+
+            if( paramsDto.getGitUsername() != null && !paramsDto.getGitUsername().isEmpty()){
+                CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider(paramsDto.getGitUsername(), paramsDto.getGitPassword());
+                cloneCommand.setCredentialsProvider(credentialsProvider) ;
+            }
+
+            Git git = cloneCommand.call();
+            writeLog(task , "仓库: " +remoteUrl+ "  克隆成功:" + localPath.getAbsolutePath());
+
+            File[] gitFiles = localPath.listFiles() ;
+            Arrays.stream(gitFiles).forEach(file -> writeLog(task , "-->> 文件: " + file.getName()));
+
         } catch (GitAPIException e) {
             log.error("克隆仓库时发生错误: " , e);
         }
