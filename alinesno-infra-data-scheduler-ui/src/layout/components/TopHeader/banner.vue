573399af907309b0da6d15829a6016005bed14c6
王磊(Levi) <leviAip@gmail.com>
2024-09-28 17:44:29 +0800

    优化banner组件，添加项目选择功能及弹窗管理。

--- alinesno-infra-data-scheduler-ui/src/layout/components/TopHeader/banner.vue
+++ alinesno-infra-data-scheduler-ui/src/layout/components/TopHeader/banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <router-link tag="div" class="header-logo-bar" to="/index">
+    <div class="header-logo-bar">
       <div class="header-logo" v-if="enableLogo" @click="enterDomain">
         <img :src="saasLogoUrl" alt="" />
       </div>
@@ -10,48 +10,87 @@
       <div v-if="saasUrl" class="dashboard-home" @click="dashboardHome()">
         <i class="fa-solid fa-house"></i> 工作台
       </div>
-    </router-link>
+      <div class="dashboard-home" @click="choiceCurrentProject()">
+        <i :class="projectInfo.projectIcons"></i> {{ projectInfo.projectName }}
+      </div>
+    </div>
+
+    <!-- 项目选择弹窗管理 -->
+    <el-dialog :title="title" v-model="open" v-if="open" width="1200px" append-to-body>
+      <choiceProject />
+
+      <template #footer>
+        <div class="dialog-footer">
+          <!-- <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button> -->
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+
+    </el-dialog>
+
   </div>
 </template>
 
-<script>
+<script setup>
 
-export default {
-  name: 'TopHeader',
-  components: {
-  },
-  computed: {
-  },
-  data() {
-
-    let saasTitle = 'AIP智能设施'
-    let enableLogo = true;
-    let saasUrl = 'http://alinesno-infra-plat-console-admin.beta.plat.infra.linesno.com' ;
+const TopHeader = ref('') ;
+const saasTitle = 'AIP智能设施'
+const enableLogo = ref(true);
+const saasUrl = ref('http://alinesno-infra-plat-console-admin.beta.plat.infra.linesno.com') ;
     let saasLogoUrl = 'http://data.linesno.com/logo_2.png' ; 
-    let displayUrl = ''; 
 
+import { 
+  getCurrentProject
+} from "@/api/data/scheduler/project";
 
-    return {
-      saasTitle,
-      saasUrl,
-      saasLogoUrl,
-      enableLogo,
-      displayUrl,
-      domainName: null,
-    }
-  },
-  created() {
-  },
-  methods: {
-    dashboardHome() {
-      window.location.href = this.saasUrl
-    },
-    // 进入企业官网
-    enterDomain() {
-      if (this.domainName) { // 跳转进入官网
-        window.open(this.domainName)
-      }
-    }
+import choiceProject from '@/views/data/scheduler/project/choiceProject'
+
+/** 参数管理 */
+const open = ref(false);
+const loading = ref(true);
+const title = ref("");
+
+const projectInfo = ref({
+  id: 0 , 
+  projectIcons: 'fa-solid fa-lemon' ,
+  projectName: '请选择当前项目'
+})
+
+// 进入企业官网
+function enterDomain() {
+  if (this.domainName) { // 跳转进入官网
+    window.open(this.domainName)
   }
 }
+
+function dashboardHome() {
+  window.location.href = saasUrl.value
+}
+
+/** 取消按钮 */
+function cancel() {
+  open.value = false;
+}
+
+function choiceCurrentProject(){
+ console.log('选择项目.') 
+  open.value = true;
+  title.value = "请选择项目";
+}
+
+/** 获取当前项目信息 */
+function handleCurrentProject(){
+  getCurrentProject().then(res => {
+    projectInfo.value = res.data
+  })
+}
+
+// handleCurrentProject() ; 
+
 </script>
+
+<style lang="scss" scoped>
+  .dashboard-home{
+    cursor: pointer;
+  }
+</style>
