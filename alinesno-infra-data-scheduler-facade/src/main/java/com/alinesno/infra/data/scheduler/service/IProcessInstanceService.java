9966bf82a59e83fb529a0c40c00740baa94fc00c
王磊(Levi) <leviAip@gmail.com>
2024-09-28 17:44:29 +0800

    修复并扩展IProcessInstanceService接口，添加readLog方法以支持日志读取功能。

--- alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/IProcessInstanceService.java
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/IProcessInstanceService.java
@@ -1,7 +1,18 @@
 package com.alinesno.infra.data.scheduler.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.api.LogReadResultDto;
 import com.alinesno.infra.data.scheduler.entity.ProcessInstanceEntity;
 
+/**
+ */
 public interface IProcessInstanceService extends IBaseService<ProcessInstanceEntity> {
+
+    /**
+     * 读取日志
+     * @param processInstanceId
+     * @return
+     */
+    LogReadResultDto readLog(long processInstanceId , String start);
+
 }
