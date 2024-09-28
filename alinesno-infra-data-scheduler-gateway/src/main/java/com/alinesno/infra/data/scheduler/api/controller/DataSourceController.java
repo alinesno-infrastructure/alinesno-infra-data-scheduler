3d607c5e3bff9c78c475cc3f5b51bea243639b39
周健(James) <jamesAip@gmail.com>
2024-09-28 23:14:03 +0800

    修复并优化DataSourceController类的方法和结构。

--- alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/DataSourceController.java
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/DataSourceController.java
@@ -8,6 +8,7 @@ import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import com.alinesno.infra.data.scheduler.api.CheckDbConnectResult;
 import com.alinesno.infra.data.scheduler.api.DataSourceDto;
 import com.alinesno.infra.data.scheduler.entity.DataSourceEntity;
+import com.alinesno.infra.data.scheduler.enums.SinkReaderEnums;
 import com.alinesno.infra.data.scheduler.service.IDataSourceService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +21,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.lang.exception.RpcServiceRuntimeException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 处理与BusinessLogEntity相关的请求的Controller。
@@ -74,7 +79,6 @@ public class DataSourceController extends BaseController<DataSourceEntity, IData
         BeanUtils.copyProperties(dto, dbEntity) ;
 
         dbEntity.setReaderName(dto.getReaderType().toUpperCase());
-
 //        DbParserUtils.parserJdbcUrl(dbEntity , dto.getJdbcUrl()) ;
 
         try {
@@ -84,161 +88,54 @@ public class DataSourceController extends BaseController<DataSourceEntity, IData
         }
     }
 
-//    /**
-//     * 获取到所有数据库源
-//     * @return
-//     */
-//    @GetMapping("/allDataSource")
-//    public AjaxResult allDataSource(){
-//        return AjaxResult.success(service.list()) ;
-//    }
-//
-//    /**
-//     * 获取到所有数据库源
-//     * @return
-//     */
-//    @GetMapping("/allDataSource")
-//    public AjaxResult allDataSource(){
-//
-//        SinkReaderEnums[] sinkArr = SinkReaderEnums.values() ;
-//        SourceReaderEnums[] sourceArr = SourceReaderEnums.values() ;
-//
-//        List<Map<String, Object>> map = new ArrayList<>() ;
-//        for(SinkReaderEnums sink : sinkArr){
-//            Map<String, Object> mapItem = new HashMap<>() ;
-//
-//            mapItem.put("code", sink.getCode()) ;
-//            mapItem.put("icon", sink.getIcon()) ;
-//            mapItem.put("readerDesc",sink.getDesc()) ;
-//            mapItem.put("sourceType", "sink") ;
-//
-//            map.add(mapItem) ;
-//        }
-//
-//        for(SourceReaderEnums source : sourceArr){
-//            Map<String, Object> mapItem = new HashMap<>() ;
-//
-//            mapItem.put("code", source.getCode()) ;
-//            mapItem.put("icon", source.getIcon()) ;
-//            mapItem.put("readerDesc",source.getDesc()) ;
-//            mapItem.put("sourceType", "source") ;
-//
-//            map.add(mapItem) ;
-//        }
-//
-//        return AjaxResult.success(map) ;
-//    }
-//
-//    @PostMapping("/saveDb")
-//    public AjaxResult saveDb(@Validated @RequestBody DataSourceDto dto ) {
-//
-//        DataSourceEntity dbEntity = new DataSourceEntity() ;
-//
-//        BeanUtils.copyProperties(dto, dbEntity) ;
-//        dbEntity.setReaderName(dto.getReaderType().toUpperCase());
-//
-////        DbParserUtils.parserJdbcUrl(dbListEntity , dto.get()) ;
-//
-//        try {
-//            return super.save(null, dbEntity) ;
-//        } catch (Exception e) {
-//            throw new RpcServiceRuntimeException(e.getMessage()) ;
-//        }
-//    }
-//
-//    /**
-//     * 获取到数据库源ID
-//     * @param dto
-//     * @return
-//     */
-//    @PostMapping("/fetchData")
-//    public AjaxResult fetchData(@Validated @RequestBody FetchDataDto dto) {
-//        log.debug("dto = {}", dto) ;
-//
-//        DataSourceEntity resourceSource = service.getById(dto.getSourceId()) ;
-//        String readerType = resourceSource.getReaderType() ;
-//        IDataSourceReader dataSourceReader = SpringUtils.getBean(readerType.toLowerCase() + PipeConstants.READER_SUFFIX) ;
-//
-//        SourceReader reader = DataSourceMapping.getDataSource(resourceSource , dto) ;
-//        List<Map<String, Object>> rows = dataSourceReader.fetchData(reader) ;
-//
-//        AjaxResult ajax =  AjaxResult.success(rows);
-//        ajax.put("fieldMeta" , dataSourceReader.analyseSourceField(reader));
-//
-//        return ajax ;
-//    }
-//
-//    /**
-//     * 获取到数据库源ID的表结构
-//     * @param sourceId
-//     * @return
-//     */
-//    @GetMapping("/fetchTableData")
-//    public AjaxResult fetchTableData(String sourceId) {
-//        log.debug("sourceId = {}", sourceId) ;
-//
-//        DataSourceEntity resourceSource = service.getById(sourceId) ;
-//        String readerType = resourceSource.getReaderType() ;
-//        IDataSourceReader dataSourceReader = SpringUtils.getBean(readerType.toLowerCase() + PipeConstants.READER_SUFFIX) ;
-//
-//        SourceReader reader = DataSourceMapping.getDataSource(resourceSource , null) ;
-//        List<TableMetaBean> rows = dataSourceReader.fetchTableData(reader) ;
-//
-//        return AjaxResult.success(rows);
-//    }
-//
-//    /**
-//     * 获取到数据库源ID的表结构
-//     * @param sourceId
-//     * @return
-//     */
-//    @GetMapping("/fetchTableMetaData")
-//    public AjaxResult fetchTableMetaData(String sourceId , String tableName) {
-//        log.debug("sourceId = {}", sourceId) ;
-//
-//        DataSourceEntity resourceSource = service.getById(sourceId) ;
-//        String readerType = resourceSource.getReaderType() ;
-//        IDataSourceReader dataSourceReader = SpringUtils.getBean(readerType.toLowerCase() + PipeConstants.READER_SUFFIX) ;
-//
-//        SourceReader reader = DataSourceMapping.getDataSource(resourceSource , null) ;
-//        reader.setQuerySql("select * from " + tableName);
-//
-//        List<FieldMetaBean> rows = dataSourceReader.analyseSourceField(reader) ;
-//
-//        return AjaxResult.success(rows);
-//    }
-//
-//    /**
-//     * 检查数据库连接
-//     * @return
-//     */
-//    @GetMapping("/checkConnection")
-//    public AjaxResult checkConnection(String sourceId){
-//        DataSourceEntity resourceSource = service.getById(sourceId) ;
-//        String readerType = resourceSource.getReaderType() ;
-//        IDataSourceReader dataSourceReader = SpringUtils.getBean(readerType.toLowerCase() + PipeConstants.READER_SUFFIX) ;
-//
-//        SourceReader reader = DataSourceMapping.getDataSource(resourceSource , null) ;
-//        dataSourceReader.checkConnection(reader) ;
-//
-//        return AjaxResult.success();
-//    }
-//
-//    /**
-//     * 检查数据库连接
-//     * @return
-//     */
-//    @PostMapping("/checkConnectionByObj")
-//    public AjaxResult checkConnectionByObj(@Validated @RequestBody DataSourceDto dto){
-//
-//        String readerType = dto.getReaderType() ;
-//        IDataSourceReader dataSourceReader = SpringUtils.getBean(readerType.toLowerCase() + PipeConstants.READER_SUFFIX) ;
-//
-//        SourceReader reader = DataSourceMapping.getDataSourceByDto(dto) ;
-//        dataSourceReader.checkConnection(reader) ;
-//
-//        return AjaxResult.success();
-//    }
+    /**
+     * 获取到所有数据库源
+     * @return
+     */
+    @GetMapping("/listDataSource")
+    public AjaxResult listDataSource(){
+        return AjaxResult.success(service.list()) ;
+    }
+
+    @PostMapping("/saveDb")
+    public AjaxResult saveDb(@Validated @RequestBody DataSourceDto dto ) {
+
+        DataSourceEntity dbEntity = new DataSourceEntity() ;
+
+        BeanUtils.copyProperties(dto, dbEntity) ;
+        dbEntity.setReaderName(dto.getReaderType().toUpperCase());
+        dbEntity.setOperationType("source");
+
+        try {
+            return super.save(null, dbEntity) ;
+        } catch (Exception e) {
+            throw new RpcServiceRuntimeException(e.getMessage()) ;
+        }
+    }
+
+    /**
+     * 获取到所有数据库源
+     * @return
+     */
+    @GetMapping("/allDataSource")
+    public AjaxResult allDataSource(){
+
+        SinkReaderEnums[] sinkArr = SinkReaderEnums.values() ;
+
+        List<Map<String, Object>> map = new ArrayList<>() ;
+        for(SinkReaderEnums sink : sinkArr){
+            Map<String, Object> mapItem = new HashMap<>() ;
+
+            mapItem.put("code", sink.getCode()) ;
+            mapItem.put("icon", sink.getIcon()) ;
+            mapItem.put("readerDesc",sink.getDesc()) ;
+            mapItem.put("sourceType", "sink") ;
+
+            map.add(mapItem) ;
+        }
+
+        return AjaxResult.success(map) ;
+    }
 
 
     @Override
