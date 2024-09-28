d90fc5dd8f6d0d89c282a5d07582fe46fea64f73
周健(James) <jamesAip@gmail.com>
2024-09-28 23:14:03 +0800

    feat:添加资源控制器相关功能及云存储消费者集成

--- alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/ResourceController.java
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/ResourceController.java
@@ -1,21 +1,30 @@
 package com.alinesno.infra.data.scheduler.api.controller;
 
+import cn.hutool.core.util.IdUtil;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.data.scheduler.adapter.CloudStorageConsumer;
 import com.alinesno.infra.data.scheduler.entity.ResourceEntity;
 import com.alinesno.infra.data.scheduler.service.ICategoryService;
 import com.alinesno.infra.data.scheduler.service.IResourceService;
 import io.swagger.annotations.Api;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Objects;
 
 /**
  * 处理与TransEntity相关的请求的Controller。
@@ -37,6 +46,12 @@ public class ResourceController extends BaseController<ResourceEntity, IResource
     @Autowired
     private ICategoryService catalogService ;
 
+    @Autowired
+    private CloudStorageConsumer storageConsumer ;
+
+    @Value("${alinesno.file.local.path:${java.io.tmpdir}}")
+    private String localPath  ;
+
     /**
      * 获取TransEntity的DataTables数据。
      *
@@ -50,6 +65,38 @@ public class ResourceController extends BaseController<ResourceEntity, IResource
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
         log.debug("page = {}", ToStringBuilder.reflectionToString(page));
         return this.toPage(model, this.getFeign(), page);
+    }
+
+    /**
+     * 文件上传
+     * @return
+     */
+    @SneakyThrows
+    @PostMapping("/importData")
+    public AjaxResult importData(@RequestPart("file") MultipartFile file, String type , String updateSupport){
+
+        // 新生成的文件名称
+        String fileSuffix = Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().lastIndexOf(".")+1);
+        String newFileName = IdUtil.getSnowflakeNextId() + "." + fileSuffix;
+
+        // 复制文件
+        File targetFile = new File(localPath , newFileName);
+        FileUtils.writeByteArrayToFile(targetFile, file.getBytes());
+
+        log.debug("newFileName = {} , targetFile = {}" , newFileName , targetFile.getAbsoluteFile());
+
+        if("img".equals(type)){  // 图片上传类型
+            AjaxResult ajaxResult = storageConsumer.upload(targetFile , "qiniu-kodo" , progress -> {
+                System.out.println("total bytes: " + progress.getTotalBytes());
+                System.out.println("current bytes: " + progress.getCurrentBytes());
+                System.out.println("progress: " + Math.round(progress.getRate() * 100) + "%");
+            }) ;
+
+            log.debug("ajaxResult= {}" , ajaxResult);
+            return ajaxResult ;
+        }
+
+        return AjaxResult.success();
     }
 
     @GetMapping("/catalogTreeSelect")
