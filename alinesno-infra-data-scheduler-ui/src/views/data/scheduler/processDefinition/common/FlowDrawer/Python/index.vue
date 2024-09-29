7a0a34a2272e62112defce917ce82a308af38cb5
周健(James) <jamesAip@gmail.com>
2024-09-30 00:16:14 +0800

    优化 Python 流程绘制器组件，调整宽度至 50%，优化布局。

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/FlowDrawer/Python/index.vue
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/FlowDrawer/Python/index.vue
@@ -1,6 +1,6 @@
 <template>
     <a-drawer :headerStyle="headerStyle" :bodyStyle="bodyStyle" :closable="true" :visible="visible"
-        :after-visible-change="afterVisibleChange" width="40%" placement="right" @close="onClose">
+        :after-visible-change="afterVisibleChange" width="50%" placement="right" @close="onClose">
         <template #title>
             <img :src="branchIcon2" class="anticon" />
             <span class="flow-ant-drawer-title">
@@ -14,9 +14,10 @@
                     <el-form-item label="节点名称" prop="name">
                         <el-input v-model="form.name" placeholder="请输入节点名称" />
                     </el-form-item>
+                    <!-- 
                     <el-form-item label="描述" prop="desc">
                         <el-input v-model="form.desc" resize="none" :rows="3" type="textarea" placeholder="请输入节点描述" />
-                    </el-form-item>
+                    </el-form-item> 
                     <el-form-item label="超时告警" prop="delivery">
                         <el-switch v-model="form.delivery" />
                     </el-form-item>
@@ -27,6 +28,7 @@
                             <el-radio :label="3">3次</el-radio>
                         </el-radio-group>
                     </el-form-item>
+                    -->
                     <el-form-item label="环境名称" prop="env">
                         <el-radio-group v-model="form.env">
                             <el-radio :label="'Sponsor'">沙箱环境</el-radio>
@@ -37,19 +39,42 @@
                         <CodeEditor ref="codeEditorRef" :lang="python" />
                     </el-form-item>
                     <el-form-item label="资源" prop="resourceId">
-                        <el-input v-model="form.resourceId" placeholder="请选择资源" />
+                        <!-- <el-input v-model="form.resourceId" placeholder="请选择资源" /> -->
+                        <el-tree-select
+                            v-model="form.resourceId"
+                            :data="resourceData"
+                            multiple
+                            placeholder="请选择资源"
+                            :render-after-expand="false"
+                            style="width: 500px"
+                        />
                     </el-form-item>
-                    <el-form-item label="自定义参数" prop="customParams">
-                        <el-input v-model="form.customParams" placeholder="请输入自定义参数" />
+                    <el-form-item label="自定义参数">
+                        <el-button type="primary" bg text @click="paramsDialog = true">
+                            <i class="fa-solid fa-screwdriver-wrench"></i>&nbsp;配置任务参数
+                        </el-button>
                     </el-form-item>
                 </el-form>
 
                 <div class="flow-setting-footer">
-                    <el-button type="primary" bg size="large" @click="submitForm('ruleForm')">确认提交</el-button>
+                    <el-button type="primary" bg size="large" @click="submitForm('ruleForm')">确认保存</el-button>
                     <el-button @click="onClose" size="large" text bg>取消</el-button>
                 </div>
             </div>
         </div>
+
+        <el-dialog title="任务环境变量" v-model="paramsDialog" append-to-body destroy-on-close class="scrollbar">
+            <ContextParam ref="taskParamRef" :context="form.context" />
+            <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="paramsDialog = false">取消</el-button>
+                <el-button type="primary" @click="callTaskParamRef()">
+                确认 
+                </el-button>
+            </div>
+            </template>
+        </el-dialog>
+        
         <FlowDrawerFooter @close="onClose" @save="onSave" />
     </a-drawer>
 </template>
@@ -60,6 +85,7 @@ import flowNodeStore from '@/store/modules/flowNode'
 
 import { branchIcon2 } from '@/utils/flowMixin';
 import CodeEditor from '../../CodeEditor.vue';
+import ContextParam from "../../../params/contextParam.vue";
 
 const { proxy } = getCurrentInstance();
 
@@ -70,6 +96,10 @@ const headerStyle = ref({
     'background-image': 'linear-gradient(90deg, #ff6a00, #f78b3e), linear-gradient(#ff6a00, #ff6a00)',
     'border-radius': '0px 0px 0 0',
 })
+
+const paramsDialog = ref(false) 
+const taskParamRef = ref(null)
+const resourceData = ref([])
 
 const data = reactive({
     form: {
@@ -144,6 +174,12 @@ function showDrawer(_node) {
 
     visible.value = true;
     node.value = _node;
+    
+    nextTick(() => {
+        getAllResource().then(res => {
+            resourceData.value = res.data
+        })
+    })
 }
 
 /**
