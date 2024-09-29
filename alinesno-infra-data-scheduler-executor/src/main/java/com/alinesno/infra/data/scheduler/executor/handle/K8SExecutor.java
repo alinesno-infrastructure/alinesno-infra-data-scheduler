1d0fd8052851e192ebec2abd36678e4620e95398
周健(James) <jamesAip@gmail.com>
2024-09-30 00:16:14 +0800

    新增K8S操作执行器类。

---
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/K8SExecutor.java
@@ -0,0 +1,19 @@
+package com.alinesno.infra.data.scheduler.executor.handle;
+
+import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
+import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * K8S操作执行器
+ */
+@Slf4j
+@Service("K8SExecutor")
+public class K8SExecutor extends BaseExecutorService {
+
+    @Override
+    public void execute(TaskInfoBean task) {
+        log.debug("K8SExecutor execute");
+    }
+}
