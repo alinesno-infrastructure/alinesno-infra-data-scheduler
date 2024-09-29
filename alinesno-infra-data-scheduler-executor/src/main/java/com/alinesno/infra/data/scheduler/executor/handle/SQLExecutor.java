81d3bac1dae78e3345e37731ead459b32c4a9481
周健(James) <jamesAip@gmail.com>
2024-09-30 00:16:14 +0800

    feat:集成Hutool SQLExecutor工具处理SQL执行与结果展示

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/SQLExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/SQLExecutor.java
@@ -1,16 +1,18 @@
 package com.alinesno.infra.data.scheduler.executor.handle;
 
+import cn.hutool.db.sql.SqlExecutor;
 import cn.hutool.db.sql.SqlFormatter;
 import com.alinesno.infra.data.scheduler.api.ParamsDto;
 import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 @Slf4j
@@ -32,14 +34,55 @@ public class SQLExecutor extends BaseExecutorService {
         DataSource dataSource = getDataSource(paramsDto.getDataSourceId()) ;
 
         try (Connection connection = dataSource.getConnection()) {
-            // 执行SQL脚本
-            ScriptUtils.executeSqlScript(connection, new ByteArrayResource(rawScript.getBytes()));
-            log.debug("SQL script executed successfully.");
+            ResultSet resultSet = SqlExecutor.callQuery(connection, rawScript) ;
+
+            writeLog(task , "SQL 脚本执行结果: ");
+            printResultSet(task ,resultSet) ;
+
             writeLog(task , "SQL 脚本执行成功.");
+            DataSourceUtils.releaseConnection(connection, dataSource);
         } catch (SQLException e) {
-            log.error("Failed to execute SQL script: " + e.getMessage());
             writeLog(task , "SQL 脚本执行失败: " + e.getMessage());
         }
     }
 
+    /**
+     * 打印结果集
+     * @param task
+     * @param resultSet
+     * @throws SQLException
+     */
+    public void printResultSet(TaskInfoBean task, ResultSet resultSet) throws SQLException {
+        // 获取结果集的元数据
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        // 打印表头
+        StringBuilder tableHeader = new StringBuilder();
+        for (int i = 1; i <= columnCount; i++) {
+            if (i > 1){
+                tableHeader.append(", ");
+            }
+            tableHeader.append(metaData.getColumnLabel(i));
+        }
+        log.debug(tableHeader.toString());
+        writeLog(task , tableHeader.toString());
+
+        // 遍历结果集并打印每一行
+        while (resultSet.next()) {
+            StringBuilder row = new StringBuilder();
+            for (int i = 1; i <= columnCount; i++) {
+                if (i > 1) {
+                    row.append(", ");
+                }
+                // 获取当前列的值
+                Object value = resultSet.getObject(i);
+                row.append(value != null ? value : "NULL");
+            }
+
+            // 打印当前列的值
+            log.debug(row.toString());
+            writeLog(task , row.toString());
+        }
+    }
 }
