0e369280e9386a6cb93553c1554c19366f61c159
赵阳(Yang) <yangAip@gmail.com>
2024-09-30 00:16:14 +0800

    修复并扩展ShellExecutorTest类的测试用例，包含Maven与Python脚本执行测试。

--- alinesno-infra-data-scheduler-executor/src/test/java/com/alinesno/infra/data/scheduler/executor/shell/ShellExecutorTest.java
+++ alinesno-infra-data-scheduler-executor/src/test/java/com/alinesno/infra/data/scheduler/executor/shell/ShellExecutorTest.java
@@ -1,10 +1,15 @@
 package com.alinesno.infra.data.scheduler.executor.shell;
 
+import cn.hutool.core.util.IdUtil;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class ShellExecutorTest {
@@ -19,7 +24,7 @@ public class ShellExecutorTest {
                 echo "Start of script"
                 for i in 1 2 3 4 5; do
                     echo "Number $i"
-                    mvn clean package -f /Users/luodong/GitRepository/alinesno-infra-base-storage/pom.xml
+                    mvn clean package -f pom.xml
                 done
                 echo "End of script"
                 """;
@@ -50,12 +55,38 @@ public class ShellExecutorTest {
                 echo "Start of script"
                 for i in 1 2 3 4 5; do
                     echo "Number $i"
-                    mvn clean package -f /Users/luodong/GitRepository/alinesno-infra-base-storage/pom.xml
+                    mvn clean package -f pom.xml
                 done
                 echo "End of script"
                 """;
 
         String res = ShellHandle.execCommand("/bin/sh", "-c", command);
         System.out.println("thread id:" + Thread.currentThread().getId() + ", result:" + res.substring(0, 5));
+    }
+
+    // 测试执行 python
+    @Test
+    public void testPython() throws IOException, InterruptedException {
+
+        // 多行的python脚本
+        String command =
+              """
+              # 定义一个数字列表
+              numbers = [1, 2, 3, 4, 5]
+              
+              # 使用 for 循环遍历列表中的每个数字
+              for number in numbers:
+                  # 计算当前数字的平方
+                  square = number ** 2
+                  # 打印数字和它的平方
+                  print(f"数字 {number} 的平方是 {square}")
+              """ ;
+
+        // 将python脚本写到临时文件
+        String tempFile = "/tmp/test" + IdUtil.getSnowflakeNextIdStr() + ".py" ;
+        FileUtils.writeStringToFile(new File(tempFile), command  , Charset.defaultCharset() , false);
+
+        String res = ShellHandle.execCommand("cmd.exe", "/C", "python " + tempFile);
+        System.out.println("thread id:" + Thread.currentThread().getId() + ", result:" + new String(res.getBytes() , StandardCharsets.UTF_8));
     }
 }
