4d2bd21f5f9b0a6bfc2ef76e286142c2d6b4c8c0
李伟(Michael) <michaelAip@gmail.com>
2024-10-01 00:18:41 +0800

    创建了 `CredentialServiceImpl` 类，实现了 `ICredentialService` 接口。

---
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/service/impl/CredentialServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.data.scheduler.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.data.scheduler.entity.CredentialEntity;
+import com.alinesno.infra.data.scheduler.mapper.CredentialMapper;
+import com.alinesno.infra.data.scheduler.service.ICredentialService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 【请填写功能名称】Service业务层处理
+ *
+ * @version 1.0.0
+ * @autor luoxiaodong
+ */
+@Slf4j
+@Service
+public class CredentialServiceImpl extends IBaseServiceImpl<CredentialEntity, CredentialMapper> implements ICredentialService {
+
+}
