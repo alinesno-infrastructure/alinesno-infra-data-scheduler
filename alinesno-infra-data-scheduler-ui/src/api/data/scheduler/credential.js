2468cd6c85806114d49f1efd288f20ad28efd725
李伟(Michael) <michaelAip@gmail.com>
2024-10-01 00:18:41 +0800

    创建了环境接口相关API文件。

---
+++ alinesno-infra-data-scheduler-ui/src/api/data/scheduler/credential.js
@@ -0,0 +1,91 @@
+import request from '@/utils/request'
+import { parseStrEmpty } from "@/utils/ruoyi";
+
+/**
+ * 环境接口操作
+ * 
+ * @author luoxiaodong
+ * @since 1.0.0
+ */
+
+// 接口配置项
+var prefix = '/api/infra/data/scheduler/credential/' ;
+var managerUrl = {
+    datatables : prefix +"datatables" ,
+    createUrl: prefix + 'add' ,
+    saveUrl: prefix + 'save' ,
+    updateUrl: prefix +"modify" ,
+    statusUrl: prefix +"changeStatus" ,
+    cleanUrl: prefix + "clean",
+    detailUrl: prefix +"detail",
+    removeUrl: prefix + "delete" ,
+    exportUrl: prefix + "exportExcel",
+    changeField: prefix + "changeField",
+    getAllCredential: prefix + "getAllCredential",
+    downloadfile: prefix + "downloadfile"
+}
+
+// 修改字段
+export function changStatusField(data){
+  return request({
+    url: managerUrl.changeField ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 查询所有的资源列表
+export function getAllCredential() {
+  return request({
+    url: managerUrl.getAllCredential, 
+    method: 'get'
+  })
+}
+
+// 查询环境列表
+export function listCredential(query) {
+  return request({
+    url: managerUrl.datatables ,
+    method: 'post',
+    params: query
+  })
+}
+
+// 查询环境详细
+export function getCredential(databaseId) {
+  return request({
+    url: managerUrl.detailUrl + '/' + parseStrEmpty(databaseId),
+    method: 'get'
+  })
+}
+
+// 新增环境
+export function addCredential(data) {
+  return request({
+    url: managerUrl.saveUrl ,
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改环境
+export function updateCredential(data) {
+  return request({
+    url: managerUrl.updateUrl ,
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除环境
+export function delCredential(databaseId) {
+  return request({
+    url: managerUrl.removeUrl + '/' + parseStrEmpty(databaseId),
+    method: 'delete'
+  })
+}
+
+export function downloadFile(databaseId) {
+    return request({
+    })
+}
