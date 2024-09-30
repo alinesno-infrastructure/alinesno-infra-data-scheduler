eacee70e09df2725751a2dcfa0bd24adf7643778
李伟(Michael) <michaelAip@gmail.com>
2024-10-01 00:18:40 +0800

    feat:实现ICredentialService接口，提供CredentialEntity服务方法。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/ICredentialService.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.data.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.entity.CredentialEntity;
+
+/**
+ * CredentialService接口
+ */
+public interface ICredentialService extends IBaseService<CredentialEntity> {
+}
