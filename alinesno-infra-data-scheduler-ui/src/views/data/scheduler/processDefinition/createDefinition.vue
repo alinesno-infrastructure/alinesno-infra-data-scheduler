b3dcbf330c39334c653ca823e41738e1196c52b8
王磊(Levi) <leviAip@gmail.com>
2024-10-01 08:54:36 +0800

    修复并优化createDefinition.vue文件，调整按钮位置与样式，优化流程设计布局。

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/createDefinition.vue
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/createDefinition.vue
@@ -1,36 +1,36 @@
 <template>
   <div class="app-container">
-      <el-page-header @back="goBack" content="配置任务编排"></el-page-header>
+    <el-page-header @back="goBack" content="配置任务编排"></el-page-header>
 
-      <div class="form-container">
-          <div class="designer-wrap">
+    <div class="form-container">
+      <div class="designer-wrap">
 
-            <div style="position: absolute;top: 20px;z-index: 1000;right: 20px;">
-              <el-button type="primary" size="large" bg text @click="submitProcessDefinition()">
-                <i class="fa-solid fa-paper-plane"></i>&nbsp;提交流程
-              </el-button>
-            </div>
+        <div style="position: absolute;top: 20px;z-index: 1000;right: 20px;">
+          <el-button type="primary" size="large" bg text @click="submitProcessDefinition()">
+            <i class="fa-solid fa-paper-plane"></i>&nbsp;提交流程
+          </el-button>
+        </div>
 
-            <div class="designer-content-box" :style="{ height: readable ? '100vh' : 'calc(100vh - 50px)' }">
-              <div class="flow-design-wrap">
-                <div id="flow-design" class="flow-design-container" :style="zoomStyle">
-                  <div id="flow-design-content" class="flow-design-content">
-                    <FlowStartNode :node="nodeData" />
+        <div class="designer-content-box" :style="{ height: readable ? '100vh' : 'calc(100vh - 50px)' }">
+          <div class="flow-design-wrap">
+            <div id="flow-design" class="flow-design-container" :style="zoomStyle">
+              <div id="flow-design-content" class="flow-design-content">
+                <FlowStartNode :node="nodeData" />
 
-                    <FlowNode :node="item" :readable="readable" v-for="(item , index) in nodeDataArr" :key="index" />
+                <FlowNode :node="item" :readable="readable" v-for="(item, index) in nodeDataArr" :key="index" />
 
-                    <FlowEndNode :node="nodeData" :readable="readable" />
-                  </div>
-                </div>
-                <FlowHelper />
-                <FlowTips />
-                <!-- <FlowZoom /> -->
+                <FlowEndNode :node="nodeData" :readable="readable" />
               </div>
             </div>
-
-
+            <FlowHelper />
+            <FlowTips />
+            <!-- <FlowZoom /> -->
           </div>
+        </div>
+
+
       </div>
+    </div>
 
   </div>
 </template>
@@ -44,9 +44,10 @@ import { ElLoading } from 'element-plus'
 import flowNodeStore from '@/store/modules/flowNode'
 
 import { commitProcessDefinition } from '@/api/data/scheduler/processDefinition'
-import { getStartNode } from '@/utils/nodeUtil';
 
-import FlowZoom from './common/FlowZoom';
+// import { getStartNode } from '@/utils/nodeUtil';
+// import FlowZoom from './common/FlowZoom';
+
 import FlowHelper from './common/FlowHelper';
 import FlowTips from './common/FlowTips';
 import FlowNode from './common/FlowNode/index';
@@ -54,32 +55,32 @@ import FlowStartNode from './common/FlowNode/Start';
 import FlowEndNode from './common/FlowNode/End';
 
 // 定义props
-const props = defineProps({
-  node: {
-    type: Object,
-    default: getStartNode,
-  },
-  navable: {
-    type: Boolean,
-    default: true,
-  },
-  readable: {
-    type: Boolean,
-    default: false,
-  },
-});
+// const props = defineProps({
+//   node: {
+//     type: Object,
+//     default: getStartNode,
+//   },
+//   navable: {
+//     type: Boolean,
+//     default: true,
+//   },
+//   readable: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
 
 const nodeDataArr = ref(flowNodeStore().nodes);
 
 /** 返回 */
 function goBack() {
-   router.push({path:'/data/scheduler/processDefinition/addDefinition',query:{}});
+  router.push({ path: '/data/scheduler/processDefinition/addDefinition', query: {} });
 }
 
 /**
  * 提交流程流程定义
  */
-function submitProcessDefinition(){
+function submitProcessDefinition() {
   let nodes = flowNodeStore().getAllNodes();
   console.log('submitProcessDefinition:' + JSON.stringify(nodes))
 
@@ -90,20 +91,25 @@ function submitProcessDefinition(){
   })
 
   const formDataStr = localStorage.getItem('processDefinitionFormData');
-   const formData = JSON.parse(formDataStr);
+  const formData = JSON.parse(formDataStr);
 
   let data = {
     taskFlow: nodes,
-    context: formData 
+    context: formData
   }
 
   // 提交流程信息
   commitProcessDefinition(data).then(response => {
-      console.log(response);
-      proxy.$modal.msgSuccess("流程提交成功");
-      loading.close();
+    console.log(response);
+    proxy.$modal.msgSuccess("流程提交成功");
+    loading.close();
   })
 }
 
+/** 初始化数据 */
+// onMounted(() => {
+//   console.log('onMounted');
+//   flowNodeStore().initNodes();
+// })
 
 </script>
