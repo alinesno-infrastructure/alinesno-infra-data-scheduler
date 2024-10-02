20096ba7365dab12af06ab1cc9855da1a6b248ad
陈伟(Victor) <victorAip@gmail.com>
2024-10-03 00:15:49 +0800

    新增CredentialDto类，用于封装凭据实体信息。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/api/CredentialDto.java
@@ -0,0 +1,32 @@
+package com.alinesno.infra.data.scheduler.api;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * CredentialEntity凭据实体信息
+ */
+@Data
+public class CredentialDto implements Serializable {
+
+    @NotBlank(message = "标识不能为空")
+    private String credentialId;
+    private long projectId ; // 所属项目
+
+    @NotBlank(message = "用户名不能为空")
+    private String username;
+
+    @NotBlank(message = "密码不能为空")
+    private String password; // 注意：在实际应用中应考虑对密码进行加密存储
+
+    private String type;
+
+    @NotBlank(message = "描述不能为空")
+    private String description;
+
+    @NotBlank(message = "范围不能为空")
+    private String credentialScope ;
+
+}
