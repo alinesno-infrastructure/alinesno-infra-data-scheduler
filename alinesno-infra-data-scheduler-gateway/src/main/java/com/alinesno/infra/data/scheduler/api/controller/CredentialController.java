4e0c186ae04f2abd676d0eebdd3263d49aa755de
陈伟(Victor) <victorAip@gmail.com>
2024-10-03 00:15:49 +0800

    feat:添加CredentialController的saveCred接口及对应逻辑

--- alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/CredentialController.java
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/CredentialController.java
@@ -3,20 +3,23 @@ package com.alinesno.infra.data.scheduler.api.controller;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.data.scheduler.api.CredentialDto;
 import com.alinesno.infra.data.scheduler.entity.CredentialEntity;
 import com.alinesno.infra.data.scheduler.service.ICredentialService;
 import io.swagger.annotations.Api;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.builder.ToStringBuilder;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 /**
  * 处理与TransEntity相关的请求的Controller。
@@ -48,6 +51,23 @@ public class CredentialController extends BaseController<CredentialEntity, ICred
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
         log.debug("page = {}", ToStringBuilder.reflectionToString(page));
         return this.toPage(model, this.getFeign(), page);
+    }
+
+    /**
+     * 保存认证
+     * @return
+     */
+    @PostMapping("/saveCred")
+    public AjaxResult saveCred(@RequestBody @Validated CredentialDto dto) {
+
+        CredentialEntity entity = new CredentialEntity() ;
+        BeanUtils.copyProperties(dto, entity);
+
+        entity.setUpdateTime(new Date());
+        entity.setType("Account");
+        service.save(entity) ;
+
+        return ok() ;
     }
 
     @Override
