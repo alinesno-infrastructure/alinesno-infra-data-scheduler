534555b5d151df1bd56ed0a091bbe94aaf0d646a
周健(James) <jamesAip@gmail.com>
2024-10-04 00:16:32 +0800

    修复:调整标签位置为左侧导航栏。

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/configuration/index.vue
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/configuration/index.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="app-container">
     <el-tabs :tab-position="tabPosition" class="demo-tabs">
-      <el-tab-pane label="运行监控">
+      <!-- <el-tab-pane label="运行监控">
         <RunningMonitor />
-      </el-tab-pane>
+      </el-tab-pane> -->
       <el-tab-pane label="环境管理">
         <EnvironmentManage />
       </el-tab-pane>
       <el-tab-pane label="凭据管理">
         <CredentialManage />
       </el-tab-pane>
-      <el-tab-pane label="运行命令">
+      <!-- <el-tab-pane label="运行命令">
         <CommandManage />
       </el-tab-pane>
       <el-tab-pane label="节点管理">
         <NodeManage />
-      </el-tab-pane>
+      </el-tab-pane> -->
     </el-tabs>
  </div>
 </template>
 
 <script setup>
-const tabPosition = ref('right')
+const tabPosition = ref('left')
 
 import RunningMonitor from './components/runningMonitor.vue'
 import EnvironmentManage from './components/environmentManage.vue'
