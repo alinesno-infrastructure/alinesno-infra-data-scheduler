87f4281d206d98f617260d1f019f759ed3977a8a
周健(James) <jamesAip@gmail.com>
2024-10-04 00:16:32 +0800

    修复并优化JarExecutor执行流程，整合资源下载与日志记录功能。

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/JarExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/JarExecutor.java
@@ -1,41 +1,26 @@
 package com.alinesno.infra.data.scheduler.executor.handle;
 
-import com.alinesno.infra.data.scheduler.api.ParamsDto;
-import com.alinesno.infra.data.scheduler.constants.PipeConstants;
-import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
+import com.alinesno.infra.data.scheduler.executor.AbstractExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
-import com.alinesno.infra.data.scheduler.executor.shell.ShellHandle;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.util.List;
-
 @Slf4j
 @Service("jarExecutor")
-public class JarExecutor extends BaseExecutorService {
+public class JarExecutor extends AbstractExecutorService {
 
     @SneakyThrows
     @Override
     public void execute(TaskInfoBean taskInfo) {
         log.debug("jarExecutor execute");
 
-        ParamsDto params = getParamsDto(taskInfo) ;
-        List<String> resources = downloadResource(params.getResourceId() , getWorkspace(taskInfo)) ;
-
-        for (String resource : resources) {
-            writeLog(taskInfo, "resource: " + resource);
+        for (String resource : getResources()) {
+            writeLog("resource: " + resource);
 
             // 设置命令行
-            File logFile = new File(getWorkspace(taskInfo), PipeConstants.RUNNING_LOGGER);
-
-            log.debug("logFile: {}", logFile.getAbsoluteFile());
-
-            ShellHandle shellHandle = new ShellHandle("/bin/sh", "-c", "java -jar " + resource);
-            shellHandle.setLogPath(logFile.getAbsolutePath());
-
-            shellHandle.execute();
+            String command = "java -jar "  + resource ;
+            runCommand(command) ;
         }
     }
 
