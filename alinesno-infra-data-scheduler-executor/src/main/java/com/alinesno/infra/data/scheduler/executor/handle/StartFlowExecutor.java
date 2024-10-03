db6feaf404b8cc16d0f2a82577635eee7a7508ac
周健(James) <jamesAip@gmail.com>
2024-10-04 00:16:32 +0800

    修复并优化 StartFlowExecutor 类的执行流程记录与环境变量输出逻辑。

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/StartFlowExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/StartFlowExecutor.java
@@ -1,28 +1,22 @@
 package com.alinesno.infra.data.scheduler.executor.handle;
 
-import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
+import com.alinesno.infra.data.scheduler.executor.AbstractExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service("startFlowExecutor")
-public class StartFlowExecutor extends BaseExecutorService {
+public class StartFlowExecutor extends AbstractExecutorService {
 
     @Override
     public void execute(TaskInfoBean taskInfo) {
         log.debug("StartFlowExecutor execute");
 
-        // 输出运行空间、M2HOME
-        log.info("--->>> 运行空间: {}", getWorkspace(taskInfo));
-        log.info("--->>> PythonHome : {}", getPythonHome(taskInfo.getEnvironment()));
-        log.info("--->>> M2Home: {}", getM2Home(taskInfo.getEnvironment()));
-        log.info("--->>> JavaHome: {}", getJavaHome(taskInfo.getEnvironment()));
-
-        writeLog(taskInfo, "--->>> StartFlowExecutor execute");
-        writeLog(taskInfo, "--->>> 运行空间: " + getWorkspace(taskInfo));
-        writeLog(taskInfo, "--->>> PythonHome : " + getPythonHome(taskInfo.getEnvironment()));
-        writeLog(taskInfo, "--->>> M2Home: " + getM2Home(taskInfo.getEnvironment()));
-        writeLog(taskInfo, "--->>> JavaHome: " + getJavaHome(taskInfo.getEnvironment()));
+        writeLog("--->>> StartFlowExecutor execute");
+        writeLog("--->>> 运行空间: " + getWorkspace());
+        writeLog("--->>> PythonHome : " + getPythonHome());
+        writeLog("--->>> M2Home: " + getM2Home());
+        writeLog("--->>> JavaHome: " + getJavaHome(taskInfo.getEnvironment()));
     }
 }
