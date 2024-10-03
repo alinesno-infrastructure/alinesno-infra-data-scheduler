75f4ef5cc5e5676b38a411ea3067c3de5c486918
周健(James) <jamesAip@gmail.com>
2024-10-04 00:16:32 +0800

    修复并优化了ProcessDefinitionServiceImpl类的导入和依赖注入问题。

--- alinesno-infra-data-scheduler-scheduler/src/main/java/com/alinesno/infra/data/scheduler/quartz/service/impl/ProcessDefinitionServiceImpl.java
+++ alinesno-infra-data-scheduler-scheduler/src/main/java/com/alinesno/infra/data/scheduler/quartz/service/impl/ProcessDefinitionServiceImpl.java
@@ -1,22 +1,19 @@
 package com.alinesno.infra.data.scheduler.quartz.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.common.web.log.utils.SpringUtils;
+import com.alinesno.infra.data.scheduler.adapter.CloudStorageConsumer;
+import com.alinesno.infra.data.scheduler.api.ParamsDto;
 import com.alinesno.infra.data.scheduler.api.ProcessDefinitionDto;
-import com.alinesno.infra.data.scheduler.entity.ProcessDefinitionEntity;
-import com.alinesno.infra.data.scheduler.entity.ProcessInstanceEntity;
-import com.alinesno.infra.data.scheduler.entity.TaskDefinitionEntity;
-import com.alinesno.infra.data.scheduler.entity.TaskInstanceEntity;
+import com.alinesno.infra.data.scheduler.entity.*;
 import com.alinesno.infra.data.scheduler.enums.ProcessStatusEnums;
 import com.alinesno.infra.data.scheduler.executor.IExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
 import com.alinesno.infra.data.scheduler.quartz.mapper.ProcessDefinitionMapper;
 import com.alinesno.infra.data.scheduler.quartz.utils.ProcessUtils;
 import com.alinesno.infra.data.scheduler.scheduler.IQuartzSchedulerService;
-import com.alinesno.infra.data.scheduler.service.IProcessDefinitionService;
-import com.alinesno.infra.data.scheduler.service.IProcessInstanceService;
-import com.alinesno.infra.data.scheduler.service.ITaskDefinitionService;
-import com.alinesno.infra.data.scheduler.service.ITaskInstanceService;
+import com.alinesno.infra.data.scheduler.service.*;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -28,24 +25,38 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Slf4j
 @Component
-public class ProcessDefinitionServiceImpl extends IBaseServiceImpl<ProcessDefinitionEntity , ProcessDefinitionMapper> implements IProcessDefinitionService {
+public class ProcessDefinitionServiceImpl extends IBaseServiceImpl<ProcessDefinitionEntity, ProcessDefinitionMapper> implements IProcessDefinitionService {
 
     @Autowired
-    private IQuartzSchedulerService distSchedulerService ;
+    private IQuartzSchedulerService distSchedulerService;
 
     @Autowired
-    private ITaskDefinitionService taskDefinitionService ;
+    private IEnvironmentService environmentService;
 
     @Autowired
-    private IProcessInstanceService processInstanceService ;
+    private ITaskDefinitionService taskDefinitionService;
 
     @Autowired
-    private ITaskInstanceService taskInstanceService ;
+    private IProcessInstanceService processInstanceService;
+
+    @Autowired
+    private ITaskInstanceService taskInstanceService;
+
+    @Autowired
+    protected CloudStorageConsumer storageConsumer;
+
+    @Value("${alinesno.file.local.path:${java.io.tmpdir}}")
+    protected String localPath;
+
+    @Autowired
+    private IResourceService resourceService;
+
 
     @Value("${alinesno.data.scheduler.workspacePath:#{systemProperties['java.io.tmpdir']}}")
     private String workspacePath;
@@ -55,20 +66,21 @@ public class ProcessDefinitionServiceImpl extends IBaseServiceImpl<ProcessDefini
     public void runProcess(TaskInfoBean task, List<TaskDefinitionEntity> taskDefinition) {
 
         // 任务实例运行
-        ProcessDefinitionEntity process = task.getProcess() ;
-        process.setRunCount(process.getRunCount()+1);
-        updateById(process) ;
+        ProcessDefinitionEntity process = task.getProcess();
+        process.setRunCount(process.getRunCount() + 1);
+        updateById(process);
 
         // 执行运行实例及任务
         runProcessInstance(task, taskDefinition, process);
 
         // 更新流程运行成功次数
-        process.setSuccessCount(process.getSuccessCount()+1);
+        process.setSuccessCount(process.getSuccessCount() + 1);
         updateById(process);
     }
 
     /**
      * 运行流程实例
+     *
      * @param task
      * @param taskDefinition
      * @param process
@@ -78,36 +90,39 @@ public class ProcessDefinitionServiceImpl extends IBaseServiceImpl<ProcessDefini
         IExecutorService executorService;
 
         // 记录任务流程实例开始
-        long count = processInstanceService.count(new LambdaQueryWrapper<ProcessInstanceEntity>().eq(ProcessInstanceEntity::getProcessId , process.getId())) + 1 ;
-        ProcessInstanceEntity processInstance = ProcessUtils.fromTaskToProcessInstance(process, count) ;
-        processInstance.setProcessId(process.getId()) ;
+        long count = processInstanceService.count(new LambdaQueryWrapper<ProcessInstanceEntity>().eq(ProcessInstanceEntity::getProcessId, process.getId())) + 1;
+        ProcessInstanceEntity processInstance = ProcessUtils.fromTaskToProcessInstance(process, count);
+        processInstance.setProcessId(process.getId());
         processInstance.setState(ProcessStatusEnums.RUNNING.getCode());
 
         // 生成实例工作空间
-        String instanceWorkspace = process.getId() + File.separator + count ;
+        String instanceWorkspace = process.getId() + File.separator + count;
         FileUtils.forceMkdir(new File(workspacePath + File.separator + instanceWorkspace)); // 创建工作空间
         processInstance.setWorkspace(instanceWorkspace);
 
-        processInstanceService.save(processInstance) ;
+        processInstanceService.save(processInstance);
         task.setWorkspace(instanceWorkspace);
 
-        boolean isFailTask = false ;
+        boolean isFailTask = false;
 
         // 任务开始更新状态
-        for(TaskDefinitionEntity t : taskDefinition){
+        for (TaskDefinitionEntity t : taskDefinition) {
             task.setTask(t);
 
             // 记录任务实例开始
-            TaskInstanceEntity taskInstance = ProcessUtils.fromTaskToTaskInstance(process, t , processInstance.getId()) ;
+            TaskInstanceEntity taskInstance = ProcessUtils.fromTaskToTaskInstance(process, t, processInstance.getId());
             taskInstance.setState(ProcessStatusEnums.RUNNING.getCode());
             taskInstance.setStartTime(new Date());
             taskInstanceService.save(taskInstance);
 
-            String beanName = t.getTaskType() + "Executor" ;
-            log.debug("TaskExecutor BeanName:{}" , beanName);
+            String beanName = t.getTaskType() + "Executor";
+            log.debug("TaskExecutor BeanName:{}", beanName);
 
-            executorService = SpringUtils.getBean(beanName) ;
-            try{
+            executorService = SpringUtils.getBean(beanName);
+            try {
+                // 配置任务参数
+                configTaskParams(task, executorService);
+
                 executorService.execute(task);
 
                 // 更新任务实例结束
@@ -115,27 +130,27 @@ public class ProcessDefinitionServiceImpl extends IBaseServiceImpl<ProcessDefini
                 taskInstance.setEndTime(new Date());
                 taskInstanceService.update(taskInstance);
 
-            }catch (Exception e){
+            } catch (Exception e) {
 
-                log.error("任务执行异常：" , e);
+                log.error("任务执行异常：", e);
 
                 taskInstance.setState(ProcessStatusEnums.FAIL.getCode());
                 taskInstance.setEndTime(new Date());
                 taskInstance.setErrorMsg(e.getMessage());
                 taskInstanceService.update(taskInstance);
 
-                isFailTask = true ;
+                isFailTask = true;
 
                 // 如果异常中断则直接跳出，否则继续执行
-                if(!t.isContinueIgnore()){
+                if (!t.isContinueIgnore()) {
                     break;
                 }
             }
         }
 
-        if(isFailTask){
+        if (isFailTask) {
             processInstance.setState(ProcessStatusEnums.FAIL.getCode());
-        }else{
+        } else {
             // 任务流程结束，更新任务实例
             processInstance.setState(ProcessStatusEnums.END.getCode());
         }
@@ -144,24 +159,81 @@ public class ProcessDefinitionServiceImpl extends IBaseServiceImpl<ProcessDefini
         processInstanceService.update(processInstance);
     }
 
+    /**
+     * 配置任务参数
+     *
+     * @param task
+     * @param executorService
+     */
+    private void configTaskParams(TaskInfoBean task, IExecutorService executorService) {
+
+        // 配置参数
+        ParamsDto paramsDto = JSONObject.parseObject(task.getTask().getTaskParams(), ParamsDto.class);
+        executorService.setParams(paramsDto);
+
+        // 配置空间
+        String workspace = workspacePath + File.separator + task.getWorkspace();
+        executorService.setWorkspace(workspace);
+
+        // 配置数据库源
+        IDataSourceService dataSourceService = SpringUtils.getBean(IDataSourceService.class);
+        executorService.setDataSource(dataSourceService.getDataSource(paramsDto.getDataSourceId()));
+
+        EnvironmentEntity environment = environmentService.getById(paramsDto.getEnv());
+        executorService.setEnvironment(environment);
+
+        // 配置资源
+        List<String> resources = downloadResource(paramsDto.getResourceId(), workspace);
+        executorService.setResource(resources);
+
+        // 配置任务环境
+        executorService.setTaskInfoBean(task);
+    }
+
+    /**
+     * 下载文件资源并返回文件名称
+     *
+     * @param resourceIds
+     * @return
+     */
+    @SneakyThrows
+    protected List<String> downloadResource(List<String> resourceIds, String workspace) {
+
+        List<String> fileNameList = new ArrayList<>();
+
+        List<ResourceEntity> resourceEntities = resourceService.listByIds(resourceIds);
+
+        for (ResourceEntity resource : resourceEntities) {
+            byte[] bytes = storageConsumer.download(String.valueOf(resource.getStorageId()), progress -> log.debug("下载进度：" + progress.getRate()));
+
+            File targetFile = new File(workspace, resource.getFileName());
+            FileUtils.writeByteArrayToFile(targetFile, bytes);
+
+            fileNameList.add(targetFile.getAbsolutePath());
+        }
+
+        return fileNameList;
+    }
+
+
     @Override
     public long commitProcessDefinition(ProcessDefinitionDto dto) {
-        long projectId = dto.getProjectId() ;
+        long projectId = dto.getProjectId();
 
-        ProcessDefinitionEntity processDefinition = ProcessUtils.fromDtoToEntity(dto) ;
+        ProcessDefinitionEntity processDefinition = ProcessUtils.fromDtoToEntity(dto);
         processDefinition.setOnline(false);
-        this.save(processDefinition) ;
+        this.save(processDefinition);
 
-        long processId = processDefinition.getId() ;
-        List<TaskDefinitionEntity> taskDefinitionList = ProcessUtils.fromDtoToTaskInstance(dto , processId , projectId) ;
-        taskDefinitionService.saveBatch(taskDefinitionList) ;
+        long processId = processDefinition.getId();
+        List<TaskDefinitionEntity> taskDefinitionList = ProcessUtils.fromDtoToTaskInstance(dto, processId, projectId);
+        taskDefinitionService.saveBatch(taskDefinitionList);
 
-        log.debug("saveProcessDefinition:{}" , processId);
+        log.debug("saveProcessDefinition:{}", processId);
 
         // 生成job任务
-        distSchedulerService.addJob(processId + "" , dto.getContext().getCronExpression());
+        distSchedulerService.addJob(processId + "", dto.getContext().getCronExpression());
 
-        return processId ;
+        return processId;
     }
 
 }
