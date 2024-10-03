e9c3567cdf5e40b49a2c9b7ed077fcd8992ba3c1
周健(James) <jamesAip@gmail.com>
2024-10-04 00:16:32 +0800

    feat:添加IEnvironmentService接口方法defaultEnv，用于设置默认环境

--- alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/IEnvironmentService.java
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/IEnvironmentService.java
@@ -4,4 +4,10 @@ import com.alinesno.infra.common.facade.services.IBaseService;
 import com.alinesno.infra.data.scheduler.entity.EnvironmentEntity;
 
 public interface IEnvironmentService extends IBaseService<EnvironmentEntity> {
+
+    /**
+     * 设置默认环境
+     * @param id
+     */
+    void defaultEnv(Long id);
 }
