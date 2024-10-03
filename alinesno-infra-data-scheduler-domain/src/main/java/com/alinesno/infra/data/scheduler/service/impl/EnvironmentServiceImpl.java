19cf7b2fe2b6a11428c7daf528e88064a87178f3
周健(James) <jamesAip@gmail.com>
2024-10-04 00:16:32 +0800

    修复并优化EnvironmentServiceImpl类中的defaultEnv方法实现。

--- alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/service/impl/EnvironmentServiceImpl.java
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/service/impl/EnvironmentServiceImpl.java
@@ -4,6 +4,7 @@ import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
 import com.alinesno.infra.data.scheduler.entity.EnvironmentEntity;
 import com.alinesno.infra.data.scheduler.mapper.EnvironmentMapper;
 import com.alinesno.infra.data.scheduler.service.IEnvironmentService;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +17,21 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class EnvironmentServiceImpl extends IBaseServiceImpl<EnvironmentEntity, EnvironmentMapper> implements IEnvironmentService {
+    @Override
+    public void defaultEnv(Long id) {
+
+        // 当前环境配置成默认的，非当前id配置成非默认的
+        EnvironmentEntity environment = baseMapper.selectById(id);
+        environment.setDefaultEnv(true);
+        baseMapper.updateById(environment);
+
+        // 其它非当前id的环境配置成非默认的
+        LambdaUpdateWrapper<EnvironmentEntity> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper
+                .ne(EnvironmentEntity::getId, id)
+                .set(EnvironmentEntity::isDefaultEnv, false);
+
+        update(updateWrapper) ;
+
+    }
 }
