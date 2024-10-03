93f0932b090d0a99f2f4c59e8b89bfa33de447ab
周健(James) <jamesAip@gmail.com>
2024-10-04 00:16:32 +0800

    优化IExecutorService接口，增加任务配置及执行相关方法。

--- alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/executor/IExecutorService.java
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/executor/IExecutorService.java
@@ -1,11 +1,55 @@
 package com.alinesno.infra.data.scheduler.executor;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alinesno.infra.data.scheduler.api.ParamsDto;
+import com.alinesno.infra.data.scheduler.entity.EnvironmentEntity;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
+
+import java.util.List;
 
 /**
  * 任务执行器
  */
 public interface IExecutorService {
+
+    /**
+     * 配置任务环境
+     * @param taskInfo
+     */
+    void setTaskInfoBean(TaskInfoBean taskInfo) ;
+
+    /**
+     * 配置参数
+     */
+    void setParams(ParamsDto config);
+
+    /**
+     * 配置空间
+     */
+    void setWorkspace(String workspace) ;
+
+    /**
+     * 配置数据库源
+     */
+    void setDataSource(DruidDataSource source) ;
+
+    /**
+     * 配置环境
+     * @param environment
+     */
+    void setEnvironment(EnvironmentEntity environment);
+
+    /**
+     * 执行命令
+     * @param command
+     */
+    void runCommand(String command);
+
+    /**
+     * 配置资源
+     * @param resources
+     */
+    void setResource(List<String> resources) ;
 
     /**
      * 执行任务，用于job执行实例
