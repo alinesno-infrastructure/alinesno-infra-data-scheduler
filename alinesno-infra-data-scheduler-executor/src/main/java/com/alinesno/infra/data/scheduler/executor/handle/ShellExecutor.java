a46514bb86b9f5aebb28de345f22132f2f0c4cf5
周健(James) <jamesAip@gmail.com>
2024-10-04 00:16:32 +0800

    修复 ShellExecutor 类执行脚本逻辑，并优化日志记录与工作空间处理。

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/ShellExecutor.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/handle/ShellExecutor.java
@@ -1,47 +1,33 @@
 package com.alinesno.infra.data.scheduler.executor.handle;
 
 import com.alinesno.infra.data.scheduler.api.ParamsDto;
-import com.alinesno.infra.data.scheduler.constants.PipeConstants;
-import com.alinesno.infra.data.scheduler.executor.BaseExecutorService;
+import com.alinesno.infra.data.scheduler.executor.AbstractExecutorService;
 import com.alinesno.infra.data.scheduler.executor.bean.TaskInfoBean;
-import com.alinesno.infra.data.scheduler.executor.shell.ShellHandle;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-
 @Slf4j
 @Service("shellExecutor")
-public class ShellExecutor extends BaseExecutorService {
+public class ShellExecutor extends AbstractExecutorService {
 
     @SneakyThrows
     @Override
     public void execute(TaskInfoBean taskInfo) {
 
-        ParamsDto paramsDto = getParamsDto(taskInfo) ;
-        downloadResource(paramsDto.getResourceId() , getWorkspace(taskInfo)) ;
-
+        ParamsDto paramsDto = getParamsDto() ;
         String rawScript = paramsDto.getRawScript();
 
         log.debug("Shell Executor rawScript: {}", rawScript) ;
-
-        // 设置命令行
-        File logFile = new File(getWorkspace(taskInfo), PipeConstants.RUNNING_LOGGER);
-
-        log.debug("logFile: {}", logFile.getAbsoluteFile());
 
         // 构建多行命令行
         String command = """
                 cd %s
                 %s
-                """.formatted(getWorkspace(taskInfo) , rawScript);
+                """.formatted(getWorkspace() , rawScript);
 
-        writeLog(taskInfo, "执行SQL:" +command);
-        ShellHandle shellHandle = new ShellHandle("/bin/sh", "-c", command);
-        shellHandle.setLogPath(logFile.getAbsolutePath());
-
-        shellHandle.execute();
+        writeLog("执行SQL:" +command);
+        runCommand(command);
     }
 
 }
