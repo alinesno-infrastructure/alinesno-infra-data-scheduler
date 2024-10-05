d061306f434043c3686dd4dbdc7ff7a6055025cf
李伟(Michael) <michaelAip@gmail.com>
2024-10-05 21:10:00 +0800

    调整 CodeEditor 组件的高度并优化主题样式设置。

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/CodeEditor.vue
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/CodeEditor.vue
@@ -35,7 +35,7 @@ const props = defineProps({
   },
   height: {
     type: String,
-    default: '400px',
+    default: '600px',
   },
 });
 
@@ -56,25 +56,9 @@ const extensions = [oneDark];
 
 // 主题样式设置
 const theme = {
-//   "&": {
-//     fontSize: "9.5pt",
-//     color: "white",
-//     backgroundColor: "#034"
-//   },
-//   ".cm-content": {
-//     caretColor: "#0e9"
-//   },
-//   "&.cm-focused .cm-cursor": {
-//     borderLeftColor: "#0e9"
-//   },
-//   "&.cm-focused .cm-selectionBackground, ::selection": {
-//     backgroundColor: "#074"
-//   },
-//   ".cm-gutters": {
-//     backgroundColor: "#045",
-//     color: "#ddd",
-//     border: "none"
-//   }
+  "&": {
+    fontSize: "9.5pt",
+  }
 }
 
 /**
