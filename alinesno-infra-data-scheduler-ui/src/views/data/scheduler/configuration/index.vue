0fd27cbcdf7b4d1581b2e876df01657c74b93d06
李伟(Michael) <michaelAip@gmail.com>
2024-10-05 21:10:00 +0800

    优化导航栏布局，将“运行监控”标签移至“系统运行配置”标签下，并新增“密钥管理”和“账号凭据管理”标签。

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/configuration/index.vue
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/configuration/index.vue
@@ -1,31 +1,29 @@
 <template>
   <div class="app-container">
     <el-tabs :tab-position="tabPosition" class="demo-tabs">
-      <!-- <el-tab-pane label="运行监控">
-        <RunningMonitor />
-      </el-tab-pane> -->
+      <el-tab-pane label="系统运行配置">
+        <NodeManage />
+      </el-tab-pane> 
       <el-tab-pane label="环境管理">
         <EnvironmentManage />
       </el-tab-pane>
-      <el-tab-pane label="凭据管理">
+      <el-tab-pane label="账号凭据管理">
         <CredentialManage />
       </el-tab-pane>
-      <!-- <el-tab-pane label="运行命令">
-        <CommandManage />
+      <el-tab-pane label="密钥管理">
+        <SecretsManager/>
       </el-tab-pane>
-      <el-tab-pane label="节点管理">
-        <NodeManage />
-      </el-tab-pane> -->
     </el-tabs>
  </div>
 </template>
 
 <script setup>
-const tabPosition = ref('left')
+const tabPosition = ref('right')
 
 import RunningMonitor from './components/runningMonitor.vue'
 import EnvironmentManage from './components/environmentManage.vue'
 import CredentialManage from './components/credentialManage.vue'
+import SecretsManager from './components/secretsManager.vue'
 import CommandManage from './components/commandManage.vue'
 import NodeManage from './components/nodeManage.vue'
 
