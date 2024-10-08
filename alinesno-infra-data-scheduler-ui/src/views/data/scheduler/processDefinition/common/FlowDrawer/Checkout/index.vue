111508c809765bd5faeb5db2f69febd8cefbda61
刘芳(Luna) <lunaAip@gmail.com>
2024-10-09 00:16:21 +0800

    优化 Checkout 页面表单字段，新增类型和仓库地址输入，并根据不同类型展示额外字段。

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/FlowDrawer/Checkout/index.vue
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processDefinition/common/FlowDrawer/Checkout/index.vue
@@ -14,15 +14,23 @@
                     <el-form-item label="节点名称" prop="name">
                         <el-input v-model="form.name" disabled="disabled" placeholder="请输入节点名称" />
                     </el-form-item>
+
                     <!-- 
                     <el-form-item label="描述" prop="desc">
                         <el-input v-model="form.desc" resize="none" :rows="3" type="textarea" placeholder="请输入节点描述" />
                     </el-form-item> 
-                    -->
                     <el-form-item label="环境名称" prop="env">
                         <el-radio-group v-model="form.env">
                             <el-radio :label="'shabox'">沙箱环境</el-radio>
                             <el-radio :label="'prod'">生产环境</el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                    -->
+
+                    <el-form-item label="类型" prop="sourceType">
+                        <el-radio-group v-model="form.sourceType">
+                            <el-radio :label="'public'">开源</el-radio>
+                            <el-radio :label="'private'">私有</el-radio>
                         </el-radio-group>
                     </el-form-item>
                     <el-form-item label="仓库地址" prop="gitUrl">
@@ -35,12 +43,6 @@
                             </el-form-item>
                         </el-col>
                     </el-row>
-                    <el-form-item label="类型" prop="sourceType">
-                        <el-radio-group v-model="form.sourceType">
-                            <el-radio :label="'public'">开源</el-radio>
-                            <el-radio :label="'private'">私有</el-radio>
-                        </el-radio-group>
-                    </el-form-item>
                     <el-row v-if="form.sourceType == 'private'">
                         <el-col :span="12">
                             <el-form-item label="账号" prop="gitUsername">
@@ -53,31 +55,16 @@
                             </el-form-item>
                         </el-col>
                     </el-row>
-                    <el-form-item label="自定义参数">
-                        <el-button type="primary" bg text @click="paramsDialog = true">
-                            <i class="fa-solid fa-screwdriver-wrench"></i>&nbsp;配置任务参数
-                        </el-button>
-                    </el-form-item>
+
                 </el-form>
 
                 <div class="flow-setting-footer">
+                    <el-button type="primary" text bg @click="handleValidateTask()"><i class="fa-solid fa-truck-fast"></i>&nbsp;验证任务</el-button>
                     <el-button type="primary" bg  @click="submitForm('ruleForm')">确认保存</el-button>
                     <el-button @click="onClose" size="large" text bg>取消</el-button>
                 </div>
             </div>
         </div>
-
-        <el-dialog title="任务环境变量" v-model="paramsDialog" append-to-body destroy-on-close class="scrollbar">
-            <ContextParam ref="taskParamRef" :context="form.context" />
-            <template #footer>
-            <div class="dialog-footer">
-                <el-button @click="paramsDialog = false">取消</el-button>
-                <el-button type="primary" @click="callTaskParamRef()">
-                确认 
-                </el-button>
-            </div>
-            </template>
-        </el-dialog>
 
         <FlowDrawerFooter @close="onClose" @save="onSave" />
     </a-drawer>
@@ -88,11 +75,11 @@
 import flowNodeStore from '@/store/modules/flowNode'
 import { getAllResource } from '@/api/data/scheduler/resource'
 import { branchIcon2 } from '@/utils/flowMixin';
-import ContextParam from "../../../params/contextParam.vue";
+import { ElLoading } from 'element-plus'
+import { validateTask } from '@/api/data/scheduler/processDefinition'
 
 const { proxy } = getCurrentInstance();
 
-const codeEditorRef = ref(null)
 const node = ref({})
 const visible = ref(false)
 const headerStyle = ref({
@@ -108,16 +95,9 @@ const resourceData = ref([])
 const data = reactive({
     form: {
         name: '',
-        desc: '',
-        delivery: false,
-        retryCount: 0,
-        env: '',
         sourceType: 'private',
-        rawScript: '' ,
-        resourceId: [],
-        customParams: {},
         gitUrl: '',
-        gitBranch: '',
+        gitBranch: 'master',
         gitUsername: '',
         gitPassword: '',
     },
@@ -127,16 +107,16 @@ const data = reactive({
             { min: 3, max: 50, message: '长度在 3 到 50 个字符', trigger: 'blur' }
         ],
         gitUrl: [
-            { required: true, message: '请输入节点描述', trigger: 'blur' },
+            { required: true, message: '请配置仓库地址', trigger: 'blur' },
         ],
         gitUsername: [
-            { required: true, message: '请选择超时告警', trigger: 'change' }
+            { required: true, message: '请配置账号名', trigger: 'change' }
         ],
         gitPassword: [
-            { required: true, message: '请选择失败重试次数', trigger: 'change' }
+            { required: true, message: '请配置克隆密码', trigger: 'change' }
         ],
         gitBranch: [
-            { required: true, message: '请选择环境名称', trigger: 'change' }
+            { required: true, message: '请配置分支', trigger: 'change' }
         ]
     }
 });
@@ -184,6 +164,44 @@ function showDrawer(_node) {
         })
     })
 }
+
+/** 验证脚本任务 */
+function handleValidateTask() {
+
+    const formInstance = proxy.$refs['ruleForm'];
+    formInstance.validate((valid) => {
+        if (valid) {
+            
+            const loading = ElLoading.service({
+                lock: true,
+                text: 'Loading',
+                background: 'rgba(0, 0, 0, 0.7)',
+            })
+
+            const formDataStr = localStorage.getItem('processDefinitionFormData');
+            const formData = JSON.parse(formDataStr);
+            let data = {
+                taskParams: form.value,
+                taskType: node.value.type,
+                context: formData
+            }
+
+            // 提交流程信息
+            validateTask(data).then(response => {
+                console.log(response);
+                proxy.$modal.msgSuccess("任务执行成功,无异常.");
+                loading.close();
+            }).catch(error => {
+                loading.close();
+            })
+            
+        } else {
+            console.log('验证失败!');
+            return false;
+        }
+    });
+}
+
 
 /**
  * 关闭侧边栏
