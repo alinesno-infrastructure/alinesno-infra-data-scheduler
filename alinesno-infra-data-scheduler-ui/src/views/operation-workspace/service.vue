aed5274fbae7c34f22ab53e0d908691247410101
李娜(Allen) <allenAip@gmail.com>
2024-10-09 11:44:54 +0800

    修复并优化 service.vue 文件中的应用图标显示

--- alinesno-infra-data-scheduler-ui/src/views/operation-workspace/service.vue
+++ alinesno-infra-data-scheduler-ui/src/views/operation-workspace/service.vue
@@ -9,13 +9,14 @@
           <div class="panel-body acp-height-auto" style="padding: 0;padding-bottom: 10px;">
             <div class="acp-app-list">
               <ul>
-                <li class="app-items" v-for="item in apps" :key="item" style="width:calc(25% - 10px)">
+                <li class="app-items" v-for="(item , index) in apps" :key="index" style="width:calc(25% - 10px)">
                   <div class="app-icon">
-                    <img :src="item.icon" :alt="item.name" />
+                    <!-- <img :src="item.icon" :alt="item.name" /> -->
+                    <img :src="'http://data.linesno.com/icons/dashboard/'+(index+1)+'.svg'" :alt="item.name" />
                   </div>
                   <div class="app-info">
                     <div class="app-item-title">{{ item.name }}</div>
-                    <div class="app-item desc">{{ item.desc }}</div>
+                    <div class="app-item desc">{{ item.description == null ? '此流程定义服务未添加描述信息': item.description }}</div>
                   </div>
                   <!-- <div class="app-status">
                     <el-icon>
@@ -59,72 +60,30 @@
   </div>
 </template>
 
-<script>
+<script setup>
 
-export default {
-  data() {
-    return {
-      opertionAssets: [
+import { recentlyProcess } from '@/api/data/scheduler/dashboard'
+
+const opertionAssets = ref([
         { id: '6', title: '总项目数', count: 45 },
         { id: '1', title: '总任务', count: 45 },
         { id: '2', title: '运行中', count: 145 },
         { id: '3', title: '异常任务', count: 65 },
         { id: '4', title: '完成任务', count: 85 },
         { id: '5', title: '中断任务', count: 45 },
-      ],
-      apps: 
-      [
-        {
-          "icon": "http://data.linesno.com/icons/dashboard/8.svg",
-          "name": "全链路日志集成服务",
-          "desc": "以实现全面的日志监控和分析。"
-        },
-        {
-          "icon": "http://data.linesno.com/icons/dashboard/7.svg",
-          "name": "日志离线分析服务",
-          "desc": "通过离线分析技术，对日志数据进行深入分析"
-        },
-        {
-          "icon": "http://data.linesno.com/icons/dashboard/6.svg",
-          "name": "账户日志离线分析服务",
-          "desc": "针对账户日志数据进行离线分析和处理"
-        },
-        {
-          "icon": "http://data.linesno.com/icons/dashboard/5.svg",
-          "name": "GPT对话日志分析服务",
-          "desc": "对GPT对话日志进行分析和解析"
-        },
-        {
-          "icon": "http://data.linesno.com/icons/dashboard/4.svg",
-          "name": "应用规范安全分析服务",
-          "desc": "通过对应用程序规范和安全性进行分析"
-        },
-        {
-          "icon": "http://data.linesno.com/icons/dashboard/3.svg",
-          "name": "用户画像分析服务",
-          "desc": "通过分析用户的行为、兴趣和偏好等数据"
-        },
-        {
-          "icon": "http://data.linesno.com/icons/dashboard/2.svg",
-          "name": "推荐算法分析服务",
-          "desc": "基于推荐算法对用户行为和偏好进行分析"
-        },
-        {
-          "icon": "http://data.linesno.com/icons/dashboard/1.svg",
-          "name": "安全日志分析服务",
-          "desc": "检测和识别潜在的安全威胁和攻击行为"
-        },
-        {
-          "icon": "http://data.linesno.com/icons/dashboard/7.svg",
-          "name": "审计安全报告分析服务",
-          "desc": "审计安全报告评估和改进其安全控制和合规性"
-        }
-      ]
+      ])
 
-    }
-  }
+const apps = ref([])
+
+/** 查询出最近的业务列表 */
+function handleRecentlyProcess(){
+  recentlyProcess().then(res => {
+    console.log(res)
+    apps.value = res.data ;
+  })
 }
 
+handleRecentlyProcess()
 
 </script>
 
