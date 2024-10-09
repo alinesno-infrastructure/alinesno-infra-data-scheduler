6b0c3206a9dfd3c31c96af25f7b393dda9207697
李娜(Allen) <allenAip@gmail.com>
2024-10-09 11:44:54 +0800

    新增 DashboardController 控制器，包含查询最近8条自定义流程的方法。

---
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/DashboardController.java
@@ -0,0 +1,42 @@
+package com.alinesno.infra.data.scheduler.api.controller;
+
+import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.rest.SuperController;
+import com.alinesno.infra.data.scheduler.entity.ProcessDefinitionEntity;
+import com.alinesno.infra.data.scheduler.service.IProcessDefinitionService;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 仪表盘控制器
+ */
+@Slf4j
+@Api(tags = "Dashboard")
+@RestController
+@Scope(SpringInstanceScope.PROTOTYPE)
+@RequestMapping("/api/infra/data/scheduler/dashboard")
+public class DashboardController extends SuperController {
+
+    @Autowired
+    private IProcessDefinitionService processDefinitionService ;
+
+    /**
+     * 查询出最近的8条自定义流程
+     */
+    @GetMapping(value = "/recentlyProcess")
+    public AjaxResult recentlyProcess() {
+
+        List<ProcessDefinitionEntity> list = processDefinitionService.queryRecentlyProcess(8) ;
+
+        return AjaxResult.success(list) ;
+    }
+
+}
