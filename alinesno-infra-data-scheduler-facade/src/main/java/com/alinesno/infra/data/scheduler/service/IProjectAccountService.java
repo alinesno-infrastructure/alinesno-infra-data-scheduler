066a83b930f4185d694086bd69cad45f5e1de44a
王磊(Levi) <leviAip@gmail.com>
2024-10-11 08:01:23 +0800

    新增IProjectAccountService接口实现文件。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/service/IProjectAccountService.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.data.scheduler.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.entity.ProjectAccountEntity;
+
+/**
+ * CredentialService接口
+ */
+public interface IProjectAccountService extends IBaseService<ProjectAccountEntity> {
+}
