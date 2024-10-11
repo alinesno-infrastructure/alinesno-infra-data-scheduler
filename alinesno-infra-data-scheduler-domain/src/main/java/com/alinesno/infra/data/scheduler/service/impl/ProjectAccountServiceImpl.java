526263d3a85ceae00a930e1f5d217f123d227488
王磊(Levi) <leviAip@gmail.com>
2024-10-11 08:01:23 +0800

    创建了 `ProjectAccountServiceImpl` 类，实现了项目账户服务接口。

---
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/service/impl/ProjectAccountServiceImpl.java
@@ -0,0 +1,20 @@
+package com.alinesno.infra.data.scheduler.service.impl;
+
+import com.alinesno.infra.common.core.service.impl.IBaseServiceImpl;
+import com.alinesno.infra.data.scheduler.entity.ProjectAccountEntity;
+import com.alinesno.infra.data.scheduler.mapper.ProjectAccountMapper;
+import com.alinesno.infra.data.scheduler.service.IProjectAccountService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 【请填写功能名称】Service业务层处理
+ *
+ * @version 1.0.0
+ * @autor luoxiaodong
+ */
+@Slf4j
+@Service
+public class ProjectAccountServiceImpl extends IBaseServiceImpl<ProjectAccountEntity, ProjectAccountMapper> implements IProjectAccountService {
+
+}
