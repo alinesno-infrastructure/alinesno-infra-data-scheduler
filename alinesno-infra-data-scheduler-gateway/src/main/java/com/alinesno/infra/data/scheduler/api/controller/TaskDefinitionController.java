b57c4132e89b197e92e149bca0076faafac6ac2a
周健(James) <jamesAip@gmail.com>
2024-10-24 07:01:53 +0800

    feat:在 TaskDefinitionController 中添加 DataPermissionScope 注解

--- alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/TaskDefinitionController.java
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/TaskDefinitionController.java
@@ -2,6 +2,7 @@ package com.alinesno.infra.data.scheduler.api.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.AjaxResult;
@@ -49,6 +50,7 @@ public class TaskDefinitionController extends BaseController<TaskDefinitionEntit
      * @param page DatatablesPageBean对象。
      * @return 包含DataTables数据的TableDataInfo对象。
      */
+    @DataPermissionScope
     @ResponseBody
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
