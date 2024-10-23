1255ef8c740b5b5d43127aa1a8751af92fd66728
周健(James) <jamesAip@gmail.com>
2024-10-24 07:01:53 +0800

    feat:在 SecretsController 中添加数据权限注解@DataPermissionScope 和 @DataPermissionSave。

--- alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/SecretsController.java
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/SecretsController.java
@@ -1,6 +1,8 @@
 package com.alinesno.infra.data.scheduler.api.controller;
 
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.AjaxResult;
@@ -46,6 +48,7 @@ public class SecretsController extends BaseController<SecretsEntity, ISecretsSer
      * @param page DatatablesPageBean对象。
      * @return 包含DataTables数据的TableDataInfo对象。
      */
+    @DataPermissionScope
     @ResponseBody
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
@@ -57,6 +60,7 @@ public class SecretsController extends BaseController<SecretsEntity, ISecretsSer
      * 保存认证
      * @return
      */
+    @DataPermissionSave
     @PostMapping("/saveSec")
     public AjaxResult saveSec(@RequestBody @Validated SecretsDto dto) {
 
