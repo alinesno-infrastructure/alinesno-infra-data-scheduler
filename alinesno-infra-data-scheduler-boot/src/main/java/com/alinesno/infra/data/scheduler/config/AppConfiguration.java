b75f7fcaed09afaa806f8cceb10754d72327cb1e
周健(James) <jamesAip@gmail.com>
2024-10-24 07:01:53 +0800

    feat:在 AppConfiguration 类中添加数据作用域注解及 Forest 框架扫描配置

--- alinesno-infra-data-scheduler-boot/src/main/java/com/alinesno/infra/data/scheduler/config/AppConfiguration.java
+++ alinesno-infra-data-scheduler-boot/src/main/java/com/alinesno/infra/data/scheduler/config/AppConfiguration.java
@@ -1,10 +1,12 @@
 package com.alinesno.infra.data.scheduler.config;
 
+import com.alinesno.infra.common.extend.datasource.enable.EnableInfraDataScope;
 import com.alinesno.infra.common.facade.enable.EnableActable;
 import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
 import com.alinesno.infra.common.web.log.aspect.LogAspect;
 import com.alinesno.infra.data.scheduler.executor.handle.StartFlowExecutor;
 import com.alinesno.infra.data.scheduler.service.ICategoryService;
+import com.dtflys.forest.springboot.annotation.ForestScan;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +22,15 @@ import org.springframework.context.annotation.Configuration;
  */
 @Slf4j
 @EnableActable
+@EnableInfraDataScope
 @EnableInfraSsoApi
 @MapperScan({
         "com.alinesno.infra.data.scheduler.quartz.mapper" ,
         "com.alinesno.infra.data.scheduler.mapper"
+})
+@ForestScan({
+        "com.alinesno.infra.common.web.adapter.base.consumer" ,
+        "com.alinesno.infra.data.scheduler.adapter"
 })
 @Configuration
 public class AppConfiguration implements CommandLineRunner {
