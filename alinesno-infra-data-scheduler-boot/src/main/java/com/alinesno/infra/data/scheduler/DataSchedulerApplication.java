ac34a061dcc169b12304fafae4a7ea2eff16998a
张婷(Tina) <tinaAip@gmail.com>
2024-11-27 00:19:42 +0800

    fix:修正 `DataSchedulerApplication` 类作者名字的大小写问题

--- alinesno-infra-data-scheduler-boot/src/main/java/com/alinesno/infra/data/scheduler/DataSchedulerApplication.java
+++ alinesno-infra-data-scheduler-boot/src/main/java/com/alinesno/infra/data/scheduler/DataSchedulerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 集成一个Java开发示例工具
- * @author LuoAnDong
+ * @author luoandong
  * @since 2023年8月3日 上午6:23:43
  */
 @SpringBootApplication
