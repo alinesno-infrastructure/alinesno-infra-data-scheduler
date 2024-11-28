23ffd16dac1e894e7f158ff4e4ddf1dae24d2634
陈伟(Victor) <victorAip@gmail.com>
2024-11-29 00:16:30 +0800

    feat:在 `CategoryController` 中添加数据权限查询和列表接口。

--- alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/CategoryController.java
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/CategoryController.java
@@ -1,23 +1,29 @@
 package com.alinesno.infra.data.scheduler.api.controller;
 
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
+import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
+import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.BaseController;
+import com.alinesno.infra.data.scheduler.api.session.CurrentProjectSession;
 import com.alinesno.infra.data.scheduler.entity.CategoryEntity;
 import com.alinesno.infra.data.scheduler.service.ICategoryService;
 import io.swagger.annotations.Api;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 处理与CategoryEntity相关的请求的Controller。
@@ -49,8 +55,52 @@ public class CategoryController extends BaseController<CategoryEntity, ICategory
     @PostMapping("/datatables")
     public TableDataInfo datatables(HttpServletRequest request, Model model, DatatablesPageBean page) {
         log.debug("page = {}", ToStringBuilder.reflectionToString(page));
-        return this.toPage(model, this.getFeign(), page);
+        return this.toPage(model, this.getFeign() , page) ;
     }
+
+    @DataPermissionQuery
+    @GetMapping("/list")
+    public AjaxResult list(CategoryEntity promptCatalog , PermissionQuery query) {
+
+        long currentProject = CurrentProjectSession.get().getId() ;
+        List<CategoryEntity> promptCatalogEntities = service.selectCatalogList(promptCatalog , query , currentProject);
+
+        return AjaxResult.success("操作成功." , promptCatalogEntities) ;
+    }
+
+    /**
+     * 保存角色类型
+     * @return
+     */
+    @DataPermissionSave
+    @PostMapping("/insertCatalog")
+    public AjaxResult insertCatalog(@RequestBody CategoryEntity entity){
+
+        long currentProject = CurrentProjectSession.get().getId() ;
+        entity.setProjectId(currentProject);
+
+        service.insertCatalog(entity) ;
+
+        return AjaxResult.success("操作成功.") ;
+    }
+
+    /**
+     * 获取到子类
+     * @param deptId
+     * @return
+     */
+    @DataPermissionQuery
+    @GetMapping("/excludeChild/{id}")
+    public AjaxResult excludeChild(@PathVariable(value = "id", required = false) Long deptId , PermissionQuery query) {
+
+        long currentProject = CurrentProjectSession.get().getId() ;
+        List<CategoryEntity> depts = service.selectCatalogList(new CategoryEntity(), query, currentProject);
+
+        depts.removeIf(d -> d.getId().longValue() == deptId || ArrayUtils.contains(StringUtils.split(d.getAncestors(), ","), deptId + ""));
+
+        return AjaxResult.success("操作成功." , depts);
+    }
+
 
     @Override
     public ICategoryService getFeign() {
