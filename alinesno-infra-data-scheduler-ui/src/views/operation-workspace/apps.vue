12e64eb6c81a850dbc3f1fc965d169b48e9d688a
周健(James) <jamesAip@gmail.com>
2024-12-21 00:15:03 +0800

    feat: 更新apps.vue文件，替换图标显示方式并优化内容结构

--- alinesno-infra-data-scheduler-ui/src/views/operation-workspace/apps.vue
+++ alinesno-infra-data-scheduler-ui/src/views/operation-workspace/apps.vue
@@ -12,8 +12,9 @@
                   <li class="box-item" v-for="item in directContent" :key="item">
                     <div class="dire-panel">
                       <div class="panel-title">
-                        <img class="dire-panel-icon" :src="item.icon" :alt="item.name" />
-                        {{ item.name }} 
+                        <!-- <img class="dire-panel-icon" :src="item.icon" :alt="item.name" /> -->
+                        <i class="dire-panel-icon" :class="item.icon" :alt="item.name" />
+                        {{ item.name }}
                       </div>
                       <div class="panel-describe">{{ item.desc }}</div>
                       <!-- <div class="panel-tip">访问链接</div> -->
@@ -33,11 +34,9 @@
             <div class="acp-app-list">
               <ul>
                 <li class="app-items" style="width:100%" v-for="item in apps" :key="item">
-                  <div class="app-icon">
-                    <img :src="item.icon" :alt="item.name" />
-                  </div>
                   <div class="app-info">
-                    <div class="app-item-title">{{ item.name }} <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                    <div class="app-item-title">
+                         <i :class="item.icon" :alt="item.name" /> {{ item.name }}
                     </div>
                     <div class="app-item desc">{{ item.desc }}</div>
                   </div>
@@ -60,54 +59,54 @@ export default {
     return {
       directContent: [
       {
-        "icon": "http://data.linesno.com/icons/dashboard/1.svg",
+        "icon": "fa-solid fa-chart-line",
         "name": "快速数据集成迁移",
         "desc": "提供快速且无缝的数据集成迁移服务，帮助用户将数据从一个系统或平台迁移到另一个系统或平台。"
       },
       {
-        "icon": "http://data.linesno.com/icons/dashboard/2.svg",
+        "icon": "fa-solid fa-box-open",
         "name": "数据离线分析计算",
         "desc": "通过离线分析计算，对大量数据进行处理和分析，以获得有关数据集的洞察和结论。"
       },
       {
-        "icon": "http://data.linesno.com/icons/dashboard/3.svg",
+        "icon": "fa-solid fa-sort-amount-down-alt",
         "name": "数据分层关系规范",
         "desc": "定义和规范数据的分层关系，确保数据在不同层级之间的正确关联和一致性。"
       },
       {
-        "icon": "http://data.linesno.com/icons/dashboard/4.svg",
+        "icon": "fa-solid fa-lock",
         "name": "数据生命周期和仓库",
         "desc": "管理数据的完整生命周期，包括数据的创建、存储、使用、归档和销毁等各个阶段，并提供相应的数据仓库支持。"
       },
       {
-        "icon": "http://data.linesno.com/icons/dashboard/5.svg",
+        "icon": "fa-solid fa-network-wired",
         "name": "轻量级湖仓一体化",
         "desc": "将数据湖和数据仓库相结合，提供一体化的数据存储和管理解决方案，具有轻量级和高效的特点。"
       }
       ],
       apps: [
         {
-          "icon": "http://data.linesno.com/icons/dashboard/7.svg",
+          "icon": "fa-solid fa-chart-line",
           "name": "数据安全备份",
           "desc": "数据安全备份"
         },
         {
-          "icon": "http://data.linesno.com/icons/dashboard/6.svg",
+          "icon": "fa-solid fa-sort-amount-down-alt",
           "name": "企业数据迁移",
           "desc": "企业数据迁移"
         },
         {
-          "icon": "http://data.linesno.com/icons/dashboard/8.svg",
+          "icon": "fa-solid fa-lock",
           "name": "数据智能治理方案",
           "desc": "数据智能治理方案"
         },
         {
-          "icon": "http://data.linesno.com/icons/dashboard/4.svg",
+          "icon": "fa-solid fa-chart-line",
           "name": "DataOps集成一体",
           "desc": "DataOps集成一体"
         },
         {
-          "icon": "http://data.linesno.com/icons/dashboard/3.svg",
+          "icon": "fa-solid fa-sort-amount-down-alt",
           "name": "数据离线分析",
           "desc": "数据离线分析"
         }
