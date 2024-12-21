adb42da8f495a45c5656ed1907946f83640aa863
王磊(Levi) <leviAip@gmail.com>
2024-12-22 00:13:37 +0800

    feat:新增 FileStoreController 控制器类，支持文件上传与图片显示功能。

---
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/FileStoreController.java
@@ -0,0 +1,83 @@
+package com.alinesno.infra.data.scheduler.api.controller;
+
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.IdUtil;
+import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.facade.response.R;
+import com.alinesno.infra.data.scheduler.adapter.CloudStorageConsumer;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Objects;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/infra/data/scheduler/fileStore")
+public class FileStoreController {
+
+    @Autowired
+    private CloudStorageConsumer storageConsumer ;
+
+    @Value("${alinesno.file.local.path:${java.io.tmpdir}}")
+    private String localPath  ;
+
+    /**
+     * 显示图片
+     * @return
+     */
+    @GetMapping("/displayImage/{imageId}")
+    public ResponseEntity<byte[]> displayImage(@PathVariable("imageId") String imageId){
+
+        byte[] byteBody = null ;
+        try{
+            byteBody = storageConsumer.download(imageId , progress -> {}) ;
+        }catch(Exception e){
+            log.error("文件下载失败:{}" , e.getMessage());
+            byteBody =  ResourceUtil.readBytes("default_avatar.jpeg") ;
+        }
+
+        return new ResponseEntity<>(byteBody, new HttpHeaders(), HttpStatus.OK);
+    }
+
+    /**
+     * 文件上传
+     * @return
+     */
+    @SneakyThrows
+    @PostMapping("/importData")
+    public AjaxResult importData(@RequestPart("file") MultipartFile file, String type , String updateSupport){
+
+        // 新生成的文件名称
+        String fileSuffix = Objects.requireNonNull(file.getOriginalFilename()).substring(file.getOriginalFilename().lastIndexOf(".")+1);
+        String newFileName = IdUtil.getSnowflakeNextId() + "." + fileSuffix;
+
+        // 复制文件
+        File targetFile = new File(localPath , newFileName);
+        FileUtils.writeByteArrayToFile(targetFile, file.getBytes());
+
+        log.debug("newFileName = {} , targetFile = {}" , newFileName , targetFile.getAbsoluteFile());
+
+        if("img".equals(type)){  // 图片上传类型
+            R<String> r = storageConsumer.upload(targetFile , "qiniu-kodo" , progress -> {
+                System.out.println("total bytes: " + progress.getTotalBytes());
+                System.out.println("current bytes: " + progress.getCurrentBytes());
+                System.out.println("progress: " + Math.round(progress.getRate() * 100) + "%");
+            }) ;
+
+            log.debug("ajaxResult= {}" , r);
+            return AjaxResult.success("上传成功." , r.getData()) ;
+        }
+
+        return AjaxResult.success();
+    }
+
+}
