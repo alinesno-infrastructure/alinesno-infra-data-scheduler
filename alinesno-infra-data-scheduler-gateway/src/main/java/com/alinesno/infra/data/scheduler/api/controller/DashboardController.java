e2a9a506f201a6d2951e35855e3c1c34714a4dde
王磊(Levi) <leviAip@gmail.com>
2024-12-27 00:15:19 +0800

    feat: 添加 DashboardController 的简单统计数据功能

--- alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/DashboardController.java
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/DashboardController.java
@@ -7,7 +7,13 @@ import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.rest.SuperController;
 import com.alinesno.infra.data.scheduler.api.session.CurrentProjectSession;
 import com.alinesno.infra.data.scheduler.entity.ProcessDefinitionEntity;
+import com.alinesno.infra.data.scheduler.entity.ProcessInstanceEntity;
+import com.alinesno.infra.data.scheduler.entity.ProjectEntity;
+import com.alinesno.infra.data.scheduler.enums.ProcessStatusEnums;
 import com.alinesno.infra.data.scheduler.service.IProcessDefinitionService;
+import com.alinesno.infra.data.scheduler.service.IProcessInstanceService;
+import com.alinesno.infra.data.scheduler.service.IProjectService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +22,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 仪表盘控制器
@@ -31,6 +39,12 @@ public class DashboardController extends SuperController {
     @Autowired
     private IProcessDefinitionService processDefinitionService ;
 
+    @Autowired
+    private IProjectService projectService; ;
+
+    @Autowired
+    private IProcessInstanceService processInstanceService;
+
     /**
      * 查询出最近的8条自定义流程
      */
@@ -40,6 +54,53 @@ public class DashboardController extends SuperController {
         long projectId = CurrentProjectSession.get().getId() ;
         List<ProcessDefinitionEntity> list = processDefinitionService.queryRecentlyProcess(11 , query , projectId) ;
         return AjaxResult.success(list) ;
+    }
+
+    /**
+     * 简单数据统计
+     */
+    @GetMapping(value = "/simpleStatistics")
+    public AjaxResult simpleStatistics() {
+
+        // 统计包括项目总数/总任务/运行中/异常任务/完成任务
+        long projectCount = 0 ;
+        long taskCount = 0 ;
+        long runningTaskCount = 0 ;
+        long errorTaskCount = 0 ;
+        long completeTaskCount = 0 ;
+
+        long orgId = CurrentProjectSession.get().getOrgId() ;
+
+        LambdaQueryWrapper<ProjectEntity> wrapper = new LambdaQueryWrapper<>() ;
+        wrapper.eq(ProjectEntity::getOrgId, orgId);
+        projectCount = projectService.count(wrapper) ;
+
+        LambdaQueryWrapper<ProcessDefinitionEntity> wrapper2 = new LambdaQueryWrapper<>() ;
+        wrapper2.eq(ProcessDefinitionEntity::getOrgId, orgId);
+        taskCount = processDefinitionService.count(wrapper2) ;
+
+        LambdaQueryWrapper<ProcessInstanceEntity> wrapper3 = new LambdaQueryWrapper<>() ;
+        wrapper3.eq(ProcessInstanceEntity::getOrgId, orgId);
+        completeTaskCount = processInstanceService.count(wrapper3) ;
+
+        LambdaQueryWrapper<ProcessInstanceEntity> wrapper4 = new LambdaQueryWrapper<>() ;
+        wrapper4.eq(ProcessInstanceEntity::getState, ProcessStatusEnums.FAIL.getCode());
+        wrapper4.eq(ProcessInstanceEntity::getOrgId, orgId);
+        errorTaskCount = processInstanceService.count(wrapper4) ;
+
+        LambdaQueryWrapper<ProcessInstanceEntity> wrapper5 = new LambdaQueryWrapper<>() ;
+        wrapper5.eq(ProcessInstanceEntity::getState, ProcessStatusEnums.RUNNING.getCode());
+        wrapper5.eq(ProcessInstanceEntity::getOrgId, orgId);
+        runningTaskCount = processInstanceService.count(wrapper5) ;
+
+        Map<String, Long> statistics = new HashMap<>() ;
+        statistics.put("projectCount", projectCount);
+        statistics.put("taskCount", taskCount);
+        statistics.put("runningTaskCount", runningTaskCount);
+        statistics.put("errorTaskCount", errorTaskCount);
+        statistics.put("completeTaskCount", completeTaskCount);
+
+        return AjaxResult.success(statistics) ;
     }
 
 }
