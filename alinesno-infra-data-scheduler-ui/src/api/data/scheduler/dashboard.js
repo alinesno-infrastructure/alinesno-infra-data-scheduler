244e9e0dae8af118a63ff15cd6e085f0c3ef0ddd
王磊(Levi) <leviAip@gmail.com>
2024-12-27 00:15:19 +0800

    feat:添加simpleStatistics接口及其实现

--- alinesno-infra-data-scheduler-ui/src/api/data/scheduler/dashboard.js
+++ alinesno-infra-data-scheduler-ui/src/api/data/scheduler/dashboard.js
@@ -3,7 +3,16 @@ import request from '@/utils/request'
 // 接口配置项
 var prefix = '/api/infra/data/scheduler/dashboard/' ;
 var managerUrl = {
-   recentlyProcess: prefix + 'recentlyProcess'
+   recentlyProcess: prefix + 'recentlyProcess',
+   simpleStatistics: prefix + 'simpleStatistics'
+}
+
+// 简单统计
+export function simpleStatistics() {
+  return request({
+    url: managerUrl.simpleStatistics,
+    method: 'get'
+  })
 }
 
 // 最近执行任务
