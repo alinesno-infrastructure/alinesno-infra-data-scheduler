7ad53a8dc0562304596bceefba3b6122449b6b94
梁明树(Nina) <ninaAip@gmail.com>
2025-09-13 09:58:27 +0800

    chore:更新ElementPlus导入方式为按需导入

--- alinesno-infra-data-scheduler-ui/src/main.js
+++ alinesno-infra-data-scheduler-ui/src/main.js
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import Cookies from 'js-cookie'
 
 import ElementPlus from 'element-plus'
-import locale from 'element-plus/lib/locale/lang/zh-cn' // 中文语言
+import locale from 'element-plus/es/locale/lang/zh-cn' // 中文语言
 
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
