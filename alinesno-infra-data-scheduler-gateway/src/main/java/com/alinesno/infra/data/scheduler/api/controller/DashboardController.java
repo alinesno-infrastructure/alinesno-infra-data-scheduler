9f19208c6c2d470e19622da90e2a66aafd4273a6
周健(James) <jamesAip@gmail.com>
2025-09-13 09:58:27 +0800

    fix:注释掉项目服务依赖，修改获取项目ID方式为固定值1L

--- alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/DashboardController.java
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/DashboardController.java
@@ -4,15 +4,16 @@ import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionQuery;
 import com.alinesno.infra.common.facade.datascope.PermissionQuery;
 import com.alinesno.infra.common.facade.response.AjaxResult;
+import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountJwt;
 import com.alinesno.infra.common.web.adapter.rest.SuperController;
-import com.alinesno.infra.data.scheduler.api.session.CurrentProjectSession;
+//import com.alinesno.infra.data.scheduler.api.session.CurrentProjectSession;
 import com.alinesno.infra.data.scheduler.entity.ProcessDefinitionEntity;
 import com.alinesno.infra.data.scheduler.entity.ProcessInstanceEntity;
-import com.alinesno.infra.data.scheduler.entity.ProjectEntity;
+//import com.alinesno.infra.data.scheduler.entity.ProjectEntity;
 import com.alinesno.infra.data.scheduler.enums.ProcessStatusEnums;
 import com.alinesno.infra.data.scheduler.service.IProcessDefinitionService;
 import com.alinesno.infra.data.scheduler.service.IProcessInstanceService;
-import com.alinesno.infra.data.scheduler.service.IProjectService;
+//import com.alinesno.infra.data.scheduler.service.IProjectService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
@@ -39,8 +40,8 @@ public class DashboardController extends SuperController {
     @Autowired
     private IProcessDefinitionService processDefinitionService ;
 
-    @Autowired
-    private IProjectService projectService; ;
+//    @Autowired
+//    private IProjectService projectService; ;
 
     @Autowired
     private IProcessInstanceService processInstanceService;
@@ -51,8 +52,8 @@ public class DashboardController extends SuperController {
     @DataPermissionQuery
     @GetMapping(value = "/recentlyProcess")
     public AjaxResult recentlyProcess(PermissionQuery query) {
-        long projectId = CurrentProjectSession.get().getId() ;
-        List<ProcessDefinitionEntity> list = processDefinitionService.queryRecentlyProcess(11 , query , projectId) ;
+//        long projectId = CurrentProjectSession.get().getId() ;
+        List<ProcessDefinitionEntity> list = processDefinitionService.queryRecentlyProcess(11 , query , 1L) ;
         return AjaxResult.success(list) ;
     }
 
@@ -69,11 +70,13 @@ public class DashboardController extends SuperController {
         long errorTaskCount = 0 ;
         long completeTaskCount = 0 ;
 
-        long orgId = CurrentProjectSession.get().getOrgId() ;
+//        if(CurrentProjectSession.get() != null){
 
-        LambdaQueryWrapper<ProjectEntity> wrapper = new LambdaQueryWrapper<>() ;
-        wrapper.eq(ProjectEntity::getOrgId, orgId);
-        projectCount = projectService.count(wrapper) ;
+        long orgId = CurrentAccountJwt.get().getOrgId() ; // CurrentProjectSession.get().getOrgId() ;
+
+//            LambdaQueryWrapper<ProjectEntity> wrapper = new LambdaQueryWrapper<>() ;
+//            wrapper.eq(ProjectEntity::getOrgId, orgId);
+//            projectCount = projectService.count(wrapper) ;
 
         LambdaQueryWrapper<ProcessDefinitionEntity> wrapper2 = new LambdaQueryWrapper<>() ;
         wrapper2.eq(ProcessDefinitionEntity::getOrgId, orgId);
@@ -92,6 +95,8 @@ public class DashboardController extends SuperController {
         wrapper5.eq(ProcessInstanceEntity::getState, ProcessStatusEnums.RUNNING.getCode());
         wrapper5.eq(ProcessInstanceEntity::getOrgId, orgId);
         runningTaskCount = processInstanceService.count(wrapper5) ;
+
+//        }
 
         Map<String, Long> statistics = new HashMap<>() ;
         statistics.put("projectCount", projectCount);
