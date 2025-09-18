43cb6a5f96f96c5f5d2ed0433117f6c369b8b5dc
周健(James) <jamesAip@gmail.com>
2025-09-19 00:33:38 +0800

    refactor:移除ProjectController.java中未使用的导入和注解

--- alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/ProjectController.java
+++ alinesno-infra-data-scheduler-gateway/src/main/java/com/alinesno/infra/data/scheduler/api/controller/ProjectController.java
@@ -2,23 +2,13 @@ package com.alinesno.infra.data.scheduler.api.controller;
 
 import com.alinesno.infra.common.core.constants.SpringInstanceScope;
 import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionSave;
-import com.alinesno.infra.common.extend.datasource.annotation.DataPermissionScope;
-import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
-import com.alinesno.infra.common.facade.pageable.TableDataInfo;
 import com.alinesno.infra.common.facade.response.AjaxResult;
 import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountBean;
-import com.alinesno.infra.common.web.adapter.login.account.CurrentAccountJwt;
 import com.alinesno.infra.common.web.adapter.login.annotation.CurrentAccount;
-import com.alinesno.infra.common.web.adapter.rest.BaseController;
 import com.alinesno.infra.data.scheduler.api.ProjectDto;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.annotations.Api;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
