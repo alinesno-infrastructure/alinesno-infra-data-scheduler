ceccc138efdcface6b3fa45e95a0683163d23bb0
周健(James) <jamesAip@gmail.com>
2025-09-19 00:33:34 +0800

    feat:创建TestLlmModelDto类，包含模型测试所需字段及校验规则。

---
+++ alinesno-infra-data-scheduler-llm/src/main/java/com/alinesno/infra/data/scheduler/llm/dto/TestLlmModelDto.java
@@ -0,0 +1,39 @@
+package com.alinesno.infra.data.scheduler.llm.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TestLlmModelDto {
+
+    @NotBlank(message = "模型类型不能为空")
+    private String modelType ;  // 模型类型
+
+    @NotBlank(message = "模型提供者不能为空")
+    private String providerCode ; // 模型提供者
+
+    @NotBlank(message = "模型接口链接不能为空")
+    private String apiUrl ; // 模型接口链接
+
+    @NotBlank(message = "模型接口key不能为空")
+    private String apiKey ;  // 模型接口key
+
+    @NotBlank(message = "模型名称不能为空")
+    private String model ;  // 模型名称
+
+    @Min(value = 1 , message = "测试频道ID不能为空")
+    private long testChannelId;
+
+    // 图像处理服务的密钥，用于验证身份
+    private String secretKey ;
+
+    // 语音合成，发音人
+    private String voice ;
+
+    // 录音识别，录音列表
+    private List<String>  audioList;
+
+}
