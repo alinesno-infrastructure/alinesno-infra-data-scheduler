37708e7d7697bae8c1c700ba5853e33f6bd3ddd1
周健(James) <jamesAip@gmail.com>
2025-09-19 00:33:37 +0800

    refactor:移除BaseResourceService中的无用导入和注解

--- alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/BaseResourceService.java
+++ alinesno-infra-data-scheduler-executor/src/main/java/com/alinesno/infra/data/scheduler/executor/BaseResourceService.java
@@ -1,10 +1,6 @@
 package com.alinesno.infra.data.scheduler.executor;
 
-import com.alinesno.infra.data.scheduler.adapter.CloudStorageConsumer;
-import com.alinesno.infra.data.scheduler.service.IResourceService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
 @Slf4j
 public class BaseResourceService {
