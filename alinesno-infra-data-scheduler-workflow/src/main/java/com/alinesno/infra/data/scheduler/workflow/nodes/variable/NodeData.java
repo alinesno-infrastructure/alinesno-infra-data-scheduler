c631a6d708b5bbe8b1862c4274833c70e0237151
周健(James) <jamesAip@gmail.com>
2025-09-19 00:33:36 +0800

    feat:创建NodeData类，包含isPrint属性和Lombok注解

---
+++ alinesno-infra-data-scheduler-workflow/src/main/java/com/alinesno/infra/data/scheduler/workflow/nodes/variable/NodeData.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.scheduler.workflow.nodes.variable;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 节点配置内容
+ */
+@ToString
+@Data
+public class NodeData {
+
+    // 是否打印的标志
+    private boolean isPrint;
+
+}
