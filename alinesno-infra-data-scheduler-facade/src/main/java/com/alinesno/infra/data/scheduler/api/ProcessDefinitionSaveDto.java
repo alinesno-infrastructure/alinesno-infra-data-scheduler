c14b9260b25bcdab8227024589046d619719aa48
周健(James) <jamesAip@gmail.com>
2025-09-19 00:33:35 +0800

    feat:创建ProcessDefinitionSaveDto类，包含流程定义保存所需字段及校验规则。

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/api/ProcessDefinitionSaveDto.java
@@ -0,0 +1,45 @@
+package com.alinesno.infra.data.scheduler.api;
+
+import com.alinesno.infra.common.facade.base.BaseDto;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * ProcessDefinitionSaveDto 类
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ProcessDefinitionSaveDto extends BaseDto {
+
+    /**
+     * 分类ID
+     */
+    @NotNull(message = "分类ID不能为空")
+    private Long categoryId ;
+
+    /**
+     * 流程描述
+     */
+    @NotNull(message = "流程描述不能为空")
+    private String taskDesc ;
+
+    /**
+     * 环境ID
+     */
+    @NotNull(message = "环境ID不能为空")
+    private Long envId ;
+
+    /**
+     * 图标
+     */
+    @NotNull(message = "图标不能为空")
+    private String icon ;
+
+    /**
+     * 任务名称
+     */
+    @NotNull(message = "任务名称不能为空")
+    private String taskName ;
+
+}
