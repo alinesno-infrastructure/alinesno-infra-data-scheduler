fe2ac029312f81d47636625de255a70c141c204a
周健(James) <jamesAip@gmail.com>
2025-09-19 00:33:38 +0800

    fix:修改引入路径，引用数据调度模块的AppNode类

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/workflow/nodes/start-node/index.js
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/workflow/nodes/start-node/index.js
@@ -1,6 +1,6 @@
 // 引入所需模块
 import StartNodeVue from './index.vue'
-import { AppNode, AppNodeModel } from '@/views/smart/assistant/workflow/common/appNode'
+import { AppNode, AppNodeModel } from '@/views/data/scheduler/workflow/common/appNode'
 
 // 定义 StartNode 类，继承自 AppNode
 class StartNode extends AppNode {
