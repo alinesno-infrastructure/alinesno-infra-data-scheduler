25ca6d3371110cdb2818e4f7a94ec79f2aa46d03
周健(James) <jamesAip@gmail.com>
2025-09-19 00:33:34 +0800

    feat:创建LLmAdapterService接口，定义多种模型获取方法。

---
+++ alinesno-infra-data-scheduler-adapter/src/main/java/com/alinesno/infra/data/scheduler/adapter/service/ILLmAdapterService.java
@@ -0,0 +1,62 @@
+package com.alinesno.infra.data.scheduler.adapter.service;
+
+import com.agentsflex.core.image.ImageConfig;
+import com.agentsflex.core.image.ImageModel;
+import com.agentsflex.core.llm.Llm;
+import com.agentsflex.core.llm.LlmConfig;
+import com.agentsflex.core.ocr.OcrConfig;
+import com.agentsflex.core.ocr.OcrModel;
+import com.agentsflex.core.reranker.ReRanker;
+import com.agentsflex.core.reranker.ReRankerConfig;
+import com.agentsflex.core.speech.SpeechConfig;
+import com.agentsflex.core.speech.SpeechModel;
+
+/**
+ * LLmAdapter 大模型适配器
+ */
+public interface ILLmAdapterService {
+
+    /**
+     * 根据不同的类返回对应的Llm对象
+     *
+     * @param type
+     * @return
+     */
+    Llm getLlm(String type, LlmConfig config);
+
+    /**
+     * 根据不同的类返回对应的ImageModel对象
+     *
+     * @param type
+     * @param config
+     * @return
+     */
+    ImageModel getImageModel(String type, ImageConfig config);
+
+    /**
+     * 重排序模型
+     *
+     * @param config
+     * @return
+     */
+    ReRanker reranker(String type , ReRankerConfig config);
+
+    /**
+     * 语音模型
+     * @param type
+     * @param config
+     * @return
+     */
+    SpeechModel speechModel(String type ,  SpeechConfig config);
+
+    /**
+     * 获取OCR模型
+     */
+    OcrModel ocrModel(String type , OcrConfig config);
+
+    /**
+     * 获取视觉模型
+     */
+    Llm visionModel(String type , LlmConfig config)  ;
+
+}
