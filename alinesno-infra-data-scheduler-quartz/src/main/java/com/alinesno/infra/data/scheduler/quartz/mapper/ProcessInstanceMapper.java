76636204ab714aa1f16dfb07b00ec9375175aed7
陈伟(Victor) <victorAip@gmail.com>
2025-09-25 05:24:29 +0800

    feat:创建ProcessInstanceMapper接口，继承IBaseMapper

---
+++ alinesno-infra-data-scheduler-quartz/src/main/java/com/alinesno/infra/data/scheduler/quartz/mapper/ProcessInstanceMapper.java
@@ -0,0 +1,10 @@
+package com.alinesno.infra.data.scheduler.quartz.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.entity.ProcessInstanceEntity;
+
+/**
+ * ProcessDefinitionMapper接口
+ */
+public interface ProcessInstanceMapper extends IBaseMapper<ProcessInstanceEntity> {
+}
