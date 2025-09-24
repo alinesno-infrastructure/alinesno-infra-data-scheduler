347587e2d4c5c5d3c7b07a1ac24afaab622198b5
陈伟(Victor) <victorAip@gmail.com>
2025-09-25 05:24:29 +0800

    feat:创建SparkSqlApiApplication类，用于启动Spring Boot应用

---
+++ alinesno-infra-data-scheduler-spark-application/src/main/java/com/alinesno/infra/data/scheduler/spark/SparkSqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.alinesno.infra.data.scheduler.spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkSqlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkSqlApiApplication.class, args);
+    }
+
+}
