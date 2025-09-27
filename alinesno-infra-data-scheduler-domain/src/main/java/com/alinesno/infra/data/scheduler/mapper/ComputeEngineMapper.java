b5d547a8334df471fd3cd7f125a65a84c554df52
周健(James) <jamesAip@gmail.com>
2025-09-28 00:19:00 +0800

    feat:创建ComputeEngineMapper接口，用于AI分布式计算引擎配置的数据库操作

---
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/mapper/ComputeEngineMapper.java
@@ -0,0 +1,16 @@
+package com.alinesno.infra.data.scheduler.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.entity.ComputeEngineEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * AI 分布式计算引擎配置 Mapper 接口
+ *
+ * @version 1.0.0
+ * @author luoxiaodong
+ */
+@Mapper
+public interface ComputeEngineMapper extends IBaseMapper<ComputeEngineEntity> {
+    // 如需自定义 SQL 方法，可在此处添加
+}
