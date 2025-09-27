8de7bc3c5867ba05efdc9a88306aaff611988a6b
周健(James) <jamesAip@gmail.com>
2025-09-28 00:19:00 +0800

    feat:创建IndexController类，实现健康检查接口并验证管理员用户

---
+++ alinesno-infra-data-scheduler-spark-application/src/main/java/com/alinesno/infra/data/scheduler/spark/controller/IndexController.java
@@ -0,0 +1,29 @@
+package com.alinesno.infra.data.scheduler.spark.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+/**
+ * SparkSqlController
+ */
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    // 控制器字段注入
+    @Value("#{'${spark-sql.admin-users}'.split(',')}")
+    private Set<String> adminUsers;
+
+    /**
+     * 写一个是否健康的接口，并且adminUsers传递fpgor是正确，并返回SUCCESS
+     */
+    @GetMapping("/health")
+    public String health(String adminUser) {
+        return adminUsers.contains(adminUser) ? "SUCCESS" : "FAILURE";
+    }
+
+}
