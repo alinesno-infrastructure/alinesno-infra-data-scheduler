238656dfb90803b70cc5cb9c8468effee6c614b6
王磊(Levi) <leviAip@gmail.com>
2025-10-02 00:23:20 +0800

    docs:注释掉ProjectAccountMapper.java文件内容

--- alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/mapper/ProjectAccountMapper.java
+++ alinesno-infra-data-scheduler-domain/src/main/java/com/alinesno/infra/data/scheduler/mapper/ProjectAccountMapper.java
@@ -1,16 +1,16 @@
-package com.alinesno.infra.data.scheduler.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.data.scheduler.entity.ProjectAccountEntity;
-
-/**
- * <p>
- * 持久层接口
- * </p>
- *
- * @author luoxiaodong
- * @version 1.0.0
- */
-public interface ProjectAccountMapper extends IBaseMapper<ProjectAccountEntity> {
-
-}
+//package com.alinesno.infra.data.scheduler.mapper;
+//
+//import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+//import com.alinesno.infra.data.scheduler.entity.ProjectAccountEntity;
+//
+///**
+// * <p>
+// * 持久层接口
+// * </p>
+// *
+// * @author luoxiaodong
+// * @version 1.0.0
+// */
+//public interface ProjectAccountMapper extends IBaseMapper<ProjectAccountEntity> {
+//
+//}
