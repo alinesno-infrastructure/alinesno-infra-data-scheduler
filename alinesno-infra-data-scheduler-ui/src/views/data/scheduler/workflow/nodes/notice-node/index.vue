301506cd1799bd11d347e8095e88e13a83f15580
周健(James) <jamesAip@gmail.com>
2025-10-03 00:20:39 +0800

    feat:新增通知节点组件，包含设置表单和内容编辑功能。

---
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/workflow/nodes/notice-node/index.vue
@@ -0,0 +1,156 @@
+<template>
+  <!-- 工作流节点容器，包含节点标题、设置和输出参数等内容 -->
+  <FlowContainer :nodeModel="nodeModel" :properties="properties">
+
+    <!-- 节点设置部分 -->
+    <div class="node-settings">
+      <!-- 节点设置标题 -->
+      <div class="settings-title">节点设置</div>
+      <!-- 节点设置表单区域 -->
+      <div class="settings-form">
+        <el-form :model="formData" label-width="auto" label-position="top">
+          <el-form-item label="通知渠道">
+            <IMSelector :nodeModel="props.nodeModel" v-model="formData.imId" />
+          </el-form-item>
+          <el-form-item label="通知内容">
+
+            <div class="function-CodemirrorEditor mb-8" style="height: 120px;width:100%">
+              <ScriptEditorPanel ref="auditEditorRef" :lang="'markdown'" />
+              <div class="function-CodemirrorEditor__footer">
+                <el-button text @click="openCodemirrorDialog" style="background-color: transparent !important;" class="magnify">
+                  <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                </el-button>
+              </div>
+            </div>
+
+          </el-form-item>
+          <el-form-item label="重试次数">
+            <el-input-number v-model="formData.retryCount" :step="1" />
+          </el-form-item>
+          <el-form-item label="返回内容">
+            <el-switch v-model="formData.isPrint" size="small" />
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+
+    <el-dialog
+      v-model="dialogVisible"
+      :title="'函数内容（Groovy）'"
+      append-to-body
+      width="800"
+    >
+      <template #header>
+        <div class="dialog-footer mt-24" style="display: flex;align-items: center; justify-content: space-between; ">
+          <div>
+            提示词
+          </div>
+          <div>
+            <el-button type="primary" size="large" text bg @click="confirmAndSave"> 确定保存 </el-button>
+          </div>
+        </div>
+      </template>
+      <ScriptEditorFullPanel ref="auditFullEditorRef" :lang="'markdown'" />
+    </el-dialog>
+
+  </FlowContainer>
+</template>
+
+<script setup>
+import { set } from 'lodash'
+import { ref, reactive } from 'vue'
+
+import FlowContainer from '@/views/data/scheduler/workflow/common/FlowContainer'
+import ScriptEditorPanel from '@/views/data/scheduler/workflow/components/ScriptEditor';
+import ScriptEditorFullPanel from '@/views/data/scheduler/workflow/components/NodeScriptEditor';
+
+import IMSelector from '@/views/data/scheduler/workflow/components/IMSelector'
+
+const props = defineProps({
+  properties: {
+    type: Object,
+    default: () => ({})
+  },
+  isSelected: {
+    type: Boolean,
+    default: false
+  },
+  nodeModel: {
+    type: Object
+  }
+});
+
+// 绑定选择框的值
+const auditEditorRef = ref(null)
+const auditFullEditorRef = ref(null)
+
+const chatDataCode = ref('')
+// const value1 = ref('')
+// const num = ref('1')
+
+const dialogVisible = ref(false)
+
+// 表单数据对象
+const form = reactive({
+  imId: '' , 
+  noticeContent: '' , 
+  retryCount: 2,
+  isPrint: true 
+})
+
+const formData = computed({
+  get: () => {
+    if (props.nodeModel.properties.node_data) {
+      return props.nodeModel.properties.node_data
+    } else {
+      set(props.nodeModel.properties, 'node_data', form)
+    }
+    return props.nodeModel.properties.node_data
+  },
+  set: (value) => {
+    set(props.nodeModel.properties, 'node_data', value)
+  }
+})
+
+// 确认保存
+function confirmAndSave() {
+  console.log('confirmAndSave')
+
+  chatDataCode.value = auditFullEditorRef.value.getRawScript()
+  auditEditorRef.value.setRawScript(chatDataCode.value)
+
+  console.log('auditFullEditorRef.value.getRawScript() = ' + auditFullEditorRef.value.getRawScript())
+  dialogVisible.value = false
+
+  // 配置
+  formData.value.noticeContent = chatDataCode.value
+}
+
+function openCodemirrorDialog() {
+
+  console.log('auditEditorRef.value.getRawScript() = ' + auditEditorRef.value.getRawScript());
+
+  chatDataCode.value = auditEditorRef.value.getRawScript();
+  console.log('chatDatacode = ' + chatDataCode.value)
+  dialogVisible.value = true
+
+  nextTick(() => {
+    auditFullEditorRef.value.setRawScript(chatDataCode.value)
+  })
+}
+
+onMounted(() => {
+  auditEditorRef.value.setRawScript(formData.value.noticeContent)
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+.function-CodemirrorEditor__footer {
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+}
+
+</style>
