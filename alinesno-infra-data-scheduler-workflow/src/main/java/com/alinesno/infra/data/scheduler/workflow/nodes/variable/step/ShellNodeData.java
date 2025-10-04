2ba039543cc0e226186c9b09a1f6137c19d71b9b
刘芳(Luna) <lunaAip@gmail.com>
2025-10-05 00:21:06 +0800

    feat:在ShellNodeData类中添加运行环境属性

--- alinesno-infra-data-scheduler-workflow/src/main/java/com/alinesno/infra/data/scheduler/workflow/nodes/variable/step/ShellNodeData.java
+++ alinesno-infra-data-scheduler-workflow/src/main/java/com/alinesno/infra/data/scheduler/workflow/nodes/variable/step/ShellNodeData.java
@@ -4,14 +4,15 @@ import com.alinesno.infra.data.scheduler.workflow.nodes.variable.NodeData;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.List;
-
 /**
  * 文本转语音（TextToSpeech）的节点数据类
  */
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class ShellNodeData extends NodeData {
+
+    // 运行环境
+    private String environment ;
 
     // 原始脚本内容
     private String shellScript;
