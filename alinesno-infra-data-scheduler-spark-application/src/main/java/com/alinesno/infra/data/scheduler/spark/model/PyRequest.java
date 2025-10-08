bd05dddcd1a39df81fe4315ad0d27ae670911234
张婷(Tina) <tinaAip@gmail.com>
2025-10-08 20:35:00 +0800

    fix:将PyRequest类中的async默认值从true改为false

--- alinesno-infra-data-scheduler-spark-application/src/main/java/com/alinesno/infra/data/scheduler/spark/model/PyRequest.java
+++ alinesno-infra-data-scheduler-spark-application/src/main/java/com/alinesno/infra/data/scheduler/spark/model/PyRequest.java
@@ -12,7 +12,7 @@ public class PyRequest {
     private String script;        // 脚本内容（优先）
     private String scriptFile;    // 脚本 URL 或集群可访问路径（可选）
     private Map<String, String> params; // 额外参数（保留）
-    private boolean async = true;
+    private boolean async = false ;
     private String user;
 }
 
