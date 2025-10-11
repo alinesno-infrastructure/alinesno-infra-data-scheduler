13fe490bfb06a536cc607cbc52489b817e5960e6
李娜(Allen) <allenAip@gmail.com>
2025-10-11 17:40:19 +0800

    feat: 添加删除按钮到流程实例页面

--- alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processInstance/index.vue
+++ alinesno-infra-data-scheduler-ui/src/views/data/scheduler/processInstance/index.vue
@@ -27,6 +27,7 @@
                <el-form-item>
                   <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
                   <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+                  <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete">删除</el-button>
                </el-form-item>
             </el-form>
 
