65f7d547949d27318313ecf843681d3d2ae38e66
李伟(Michael) <michaelAip@gmail.com>
2025-10-25 21:21:09 +0800

    feat:创建NotificationConfigMapper接口，继承IBaseMapper

---
+++ alinesno-infra-data-scheduler-notice/src/main/java/com/alinesno/infra/data/scheduler/notice/mapper/NotificationConfigMapper.java
@@ -0,0 +1,8 @@
+// NotificationConfigMapper.java
+package com.alinesno.infra.data.scheduler.notice.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.entity.NotificationConfigEntity;
+
+public interface NotificationConfigMapper extends IBaseMapper<NotificationConfigEntity> {
+}
