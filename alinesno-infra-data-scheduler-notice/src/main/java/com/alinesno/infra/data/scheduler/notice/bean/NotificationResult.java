6e3a81bb495ff1eb0c71e46c6071ea459614c4f1
李伟(Michael) <michaelAip@gmail.com>
2025-10-25 21:21:09 +0800

    feat:创建NotificationResult类，用于封装通知结果信息

---
+++ alinesno-infra-data-scheduler-notice/src/main/java/com/alinesno/infra/data/scheduler/notice/bean/NotificationResult.java
@@ -0,0 +1,23 @@
+// NotificationResult.java
+package com.alinesno.infra.data.scheduler.notice.bean;
+
+import lombok.Data;
+
+@Data
+public class NotificationResult {
+    private boolean success;
+    private String code;
+    private String message;
+    private String rawResponse;
+
+    public static NotificationResult success(String rawResponse) {
+        NotificationResult r = new NotificationResult();
+        r.success = true; r.rawResponse = rawResponse; r.message = "ok";
+        return r;
+    }
+    public static NotificationResult fail(String code, String message, String rawResponse) {
+        NotificationResult r = new NotificationResult();
+        r.success = false; r.code = code; r.message = message; r.rawResponse = rawResponse;
+        return r;
+    }
+}
