d6fdfb7422a5a8d15cad854ad92d9b6ec393b0a4
李伟(Michael) <michaelAip@gmail.com>
2025-10-25 21:21:08 +0800

    更新:alinesno-infra-data-scheduler-im/src/main/java/com/alinesno/infra/data/scheduler/im/mapper/NotificationConfigMapper.java

--- alinesno-infra-data-scheduler-im/src/main/java/com/alinesno/infra/data/scheduler/im/mapper/NotificationConfigMapper.java
+++
@@ -1,8 +0,0 @@
-// NotificationConfigMapper.java
-package com.alinesno.infra.data.scheduler.im.mapper;
-
-import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.data.scheduler.entity.NotificationConfigEntity;
-
-public interface NotificationConfigMapper extends IBaseMapper<NotificationConfigEntity> {
-}
