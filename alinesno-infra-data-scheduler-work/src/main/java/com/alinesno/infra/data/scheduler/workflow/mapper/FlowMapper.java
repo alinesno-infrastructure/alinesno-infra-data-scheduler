1f911a9bdc0c9fe39469dea8667a7bfb88076107
刘芳(Luna) <lunaAip@gmail.com>
2025-10-27 00:21:46 +0800

    refactor:调整FlowEntity导入路径，优化包结构

--- alinesno-infra-data-scheduler-work/src/main/java/com/alinesno/infra/data/scheduler/workflow/mapper/FlowMapper.java
+++ alinesno-infra-data-scheduler-work/src/main/java/com/alinesno/infra/data/scheduler/workflow/mapper/FlowMapper.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.data.scheduler.workflow.mapper;
 
 import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
-import com.alinesno.infra.data.scheduler.workflow.entity.FlowEntity;
+import com.alinesno.infra.data.scheduler.entity.worker.FlowEntity;
 
 /**
  * 工作流表 Mapper 接口
