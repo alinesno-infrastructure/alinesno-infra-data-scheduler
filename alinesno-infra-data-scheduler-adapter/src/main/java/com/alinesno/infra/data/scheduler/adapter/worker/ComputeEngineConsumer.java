f886ffbb2d7ab7d306bb5f2148cd21fb16d3a3fb
刘芳(Luna) <lunaAip@gmail.com>
2025-10-27 00:21:46 +0800

    feat: 创建ComputeEngineConsumer接口，包含获取配置、保存配置和测试引擎健康方法。

---
+++ alinesno-infra-data-scheduler-adapter/src/main/java/com/alinesno/infra/data/scheduler/adapter/worker/ComputeEngineConsumer.java
@@ -0,0 +1,38 @@
+package com.alinesno.infra.data.scheduler.adapter.worker;
+
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.response.R;
+import com.alinesno.infra.data.scheduler.api.ProbeResultDto;
+import com.alinesno.infra.data.scheduler.entity.ComputeEngineEntity;
+import com.dtflys.forest.annotation.*;
+
+/**
+ * ComputeEngineConsumer
+ */
+@BaseRequest(baseURL = "#{alinesno.data.scheduler.worker-node}/api/infra/data/scheduler/computeEngine" , connectTimeout = 30*1000)
+public interface ComputeEngineConsumer {
+
+    /**
+     * 获取当前配置
+     * @param query
+     * @return
+     */
+    @Get("/getConfig")
+    R<ComputeEngineEntity> getCurrentConfig(@JSONBody PermissionQuery query);
+
+    /**
+     * 保存配置
+     * @param entity
+     */
+    @Post("/saveConfig")
+    R<Boolean> saveOrUpdateConfig(@JSONBody ComputeEngineEntity entity);
+
+    /**
+     * 测试引擎健康
+     * @param engineAddress
+     * @param adminUser
+     * @return
+     */
+    @Get("/probeHealth")
+    R<ProbeResultDto> probeEngineHealth(@Query("engineAddress") String engineAddress, @Query("adminUser") String adminUser);
+}
