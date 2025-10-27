1602a157e6951c759e77d1c2a52d5de69d37d1fc
张婷(Tina) <tinaAip@gmail.com>
2025-10-28 00:25:40 +0800

    feat:创建工作流节点执行实体类FlowNodeExecutionEntity

---
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/entity/worker/FlowNodeExecutionEntity.java
@@ -0,0 +1,148 @@
+package com.alinesno.infra.data.scheduler.entity.worker;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnComment;
+import com.gitee.sunchenbin.mybatis.actable.annotation.ColumnType;
+import com.gitee.sunchenbin.mybatis.actable.annotation.TableComment;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 工作流节点执行表实体类，记录每个节点执行的情况
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("flow_node_executions")
+@TableComment(value = "工作流节点执行表")
+public class FlowNodeExecutionEntity extends InfraBaseEntity {
+
+    /**
+     * 所属工作流执行实例的 ID
+     */
+    @TableField(value = "flow_execution_id")
+    @ColumnType(value = MySqlTypeConstant.BIGINT)
+    @ColumnComment("所属工作流执行实例的 ID")
+    private Long flowExecutionId;
+
+    /**
+     * 节点 ID
+     */
+    @TableField(value = "node_id")
+    @ColumnType(value = MySqlTypeConstant.BIGINT)
+    @ColumnComment("节点 ID")
+    private Long nodeId;
+
+    /**
+     * 界面工作流步骤节点ID
+     */
+    @TableField(value = "step_id")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR , length = 64)
+    @ColumnComment("界面工作流步骤节点ID")
+    private String stepId;
+
+    /**
+     * 节点名称
+     */
+    @TableField(value = "node_name")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 100)
+    @ColumnComment("节点名称")
+    private String nodeName;
+
+    /**
+     * 执行信息
+     */
+    @TableField(value = "execute_info")
+    @ColumnType(value = MySqlTypeConstant.TEXT)
+    @ColumnComment("执行信息")
+    private String executeInfo ;
+
+    /**
+     * 节点类型
+     */
+    @TableField(value = "node_type")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 50)
+    @ColumnComment("节点类型")
+    private String nodeType;
+
+    /**
+     * 执行进度按最高100进行表示，0-100之间
+     */
+    @TableField(value = "progress")
+    @ColumnType(value = MySqlTypeConstant.INT)
+    @ColumnComment("执行进度按最高100进行表示，0-100之间")
+    private Integer progress;
+
+    /**
+     * 节点的属性，采用键值对的形式存储额外信息
+     * 这里假设以 JSON 字符串形式存储，实际使用时可根据需求调整
+     */
+    @TableField(value = "properties")
+    @ColumnType(value = MySqlTypeConstant.TEXT)
+    @ColumnComment("节点的属性，采用键值对的形式存储额外信息")
+    private String properties;
+
+    /**
+     * 执行状态
+     */
+    @TableField(value = "execution_status")
+    @ColumnType(value = MySqlTypeConstant.VARCHAR, length = 50)
+    @ColumnComment("执行状态")
+    private String executionStatus;
+
+    /**
+     * 执行的序号
+     */
+    @TableField(value = "execution_order")
+    @ColumnType(value = MySqlTypeConstant.INT)
+    @ColumnComment("执行的序号")
+    private Integer executionOrder;
+
+    /**
+     * 执行的图形深度
+     */
+    @TableField(value = "execution_depth")
+    @ColumnType(value = MySqlTypeConstant.INT)
+    @ColumnComment("执行的图形深度")
+    private Integer executionDepth;
+
+    /**
+     * 开始时间
+     */
+    @TableField(value = "execute_time")
+    @ColumnType(value = MySqlTypeConstant.DATETIME)
+    @ColumnComment("开始执行时间")
+    private Date executeTime;
+
+    /**
+     * 结束时间
+     */
+    @TableField(value = "finish_time")
+    @ColumnType(value = MySqlTypeConstant.DATETIME)
+    @ColumnComment("结束时间")
+    private Date finishTime;
+
+    /**
+     * 错误信息
+     */
+    @TableField(value = "error_msg")
+    @ColumnType(value = MySqlTypeConstant.TEXT)
+    @ColumnComment("错误信息")
+    private String errorMsg ;
+
+    public void setExecuteInfo(String executeInfo) {
+        // 最大长度不能超过10000个字符
+        if (executeInfo != null && executeInfo.length() > 10000) {
+            executeInfo = executeInfo.substring(0, 10000);
+        }
+        this.executeInfo = executeInfo;
+    }
+}
