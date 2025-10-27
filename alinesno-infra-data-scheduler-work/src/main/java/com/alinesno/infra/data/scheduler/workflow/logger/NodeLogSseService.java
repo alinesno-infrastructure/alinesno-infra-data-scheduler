c0fb44dfa19746a2c09062b3c40632604bd3029c
张婷(Tina) <tinaAip@gmail.com>
2025-10-28 00:25:41 +0800

    fix:添加NodeLog类的导入依赖

--- alinesno-infra-data-scheduler-work/src/main/java/com/alinesno/infra/data/scheduler/workflow/logger/NodeLogSseService.java
+++ alinesno-infra-data-scheduler-work/src/main/java/com/alinesno/infra/data/scheduler/workflow/logger/NodeLogSseService.java
@@ -1,5 +1,6 @@
 package com.alinesno.infra.data.scheduler.workflow.logger;
 
+import com.alinesno.infra.data.scheduler.api.logger.NodeLog;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
