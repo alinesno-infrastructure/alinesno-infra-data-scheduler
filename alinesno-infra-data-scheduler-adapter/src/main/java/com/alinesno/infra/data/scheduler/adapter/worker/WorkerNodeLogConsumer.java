f6ab003eb2ed0609e1467d65d1848c049552fee8
张婷(Tina) <tinaAip@gmail.com>
2025-10-28 00:25:40 +0800

    feat:创建WorkerNodeLogConsumer接口，用于读取任务日志

---
+++ alinesno-infra-data-scheduler-adapter/src/main/java/com/alinesno/infra/data/scheduler/adapter/worker/WorkerNodeLogConsumer.java
@@ -0,0 +1,29 @@
+package com.alinesno.infra.data.scheduler.adapter.worker;
+
+import com.alinesno.infra.common.facade.response.R;
+import com.dtflys.forest.annotation.BaseRequest;
+import com.dtflys.forest.annotation.Get;
+import com.dtflys.forest.annotation.Query;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+/**
+ * WorkerNodeLogConsumer
+ */
+@BaseRequest(baseURL = "#{alinesno.data.scheduler.worker-node}/api/infra/data/scheduler/nodeLogs" , connectTimeout = 30*1000)
+public interface WorkerNodeLogConsumer {
+
+    /**
+     * 读取任务日志
+     * @param processInstanceId
+     * @param nodeId
+     * @param start
+     * @return
+     */
+    @Get("/readLog")
+    R<Map<String, Object>> readLog(@Query("processInstanceId") String processInstanceId,
+                                   @Query("nodeId") String nodeId,
+                                   @Query("start") int start);
+
+}
