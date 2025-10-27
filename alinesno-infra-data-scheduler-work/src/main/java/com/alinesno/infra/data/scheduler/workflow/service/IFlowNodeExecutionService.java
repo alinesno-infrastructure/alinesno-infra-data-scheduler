d5de955644bebe4f6d0ae18d225a0365cf5888a4
张婷(Tina) <tinaAip@gmail.com>
2025-10-28 00:25:40 +0800

    refactor:调整FlowNodeExecutionEntity的导入路径

--- alinesno-infra-data-scheduler-work/src/main/java/com/alinesno/infra/data/scheduler/workflow/service/IFlowNodeExecutionService.java
+++ alinesno-infra-data-scheduler-work/src/main/java/com/alinesno/infra/data/scheduler/workflow/service/IFlowNodeExecutionService.java
@@ -1,7 +1,7 @@
 package com.alinesno.infra.data.scheduler.workflow.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
-import com.alinesno.infra.data.scheduler.workflow.entity.FlowNodeExecutionEntity;
+import com.alinesno.infra.data.scheduler.entity.worker.FlowNodeExecutionEntity;
 
 /**
  * 工作流节点执行服务接口，用于处理工作流节点执行情况的业务。
