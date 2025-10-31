4e43ad3d2be6fd5229aba1375a89b8b35f64bc9a
张婷(Tina) <tinaAip@gmail.com>
2025-11-01 00:23:39 +0800

    feat:创建JobMapper接口，继承IBaseMapper

---
+++ alinesno-infra-data-scheduler-trigger/src/main/java/com/alinesno/infra/data/scheduler/trigger/mapper/JobMapper.java
@@ -0,0 +1,11 @@
+package com.alinesno.infra.data.scheduler.trigger.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.trigger.entity.JobEntity;
+
+/**
+ * JobMapper
+ */
+public interface JobMapper extends IBaseMapper<JobEntity> {
+
+}
