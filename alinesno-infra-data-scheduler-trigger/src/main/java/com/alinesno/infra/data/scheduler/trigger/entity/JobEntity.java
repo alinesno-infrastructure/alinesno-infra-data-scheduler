c86e7fed0da671657fcda512f5e5d4716213aa73
张婷(Tina) <tinaAip@gmail.com>
2025-11-01 00:23:39 +0800

    feat: 创建 JobEntity 实体类，包含 name 属性及数据库映射注解

---
+++ alinesno-infra-data-scheduler-trigger/src/main/java/com/alinesno/infra/data/scheduler/trigger/entity/JobEntity.java
@@ -0,0 +1,24 @@
+package com.alinesno.infra.data.scheduler.trigger.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Table;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Job 实体：使用 Lombok、MyBatis-Plus 的 ActiveRecord，并用 mybatis-actable 注解以自动建表
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("job")
+@Table(name = "job") // mybatis-actable 注解，自动建表时会读取
+public class JobEntity extends InfraBaseEntity {
+
+    @TableField
+    @Column(name = "name", type = MySqlTypeConstant.VARCHAR, length = 255, isNull = false)
+    private String name;
+}
