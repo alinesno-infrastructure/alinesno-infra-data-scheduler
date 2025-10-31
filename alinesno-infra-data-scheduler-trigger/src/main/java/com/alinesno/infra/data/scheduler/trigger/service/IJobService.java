fcc9d3aaa01d4ca4c5df5902f4432fc7720e59e2
张婷(Tina) <tinaAip@gmail.com>
2025-11-01 00:23:39 +0800

    feat: 创建IJobService接口，继承IBaseService并指定JobEntity类型

---
+++ alinesno-infra-data-scheduler-trigger/src/main/java/com/alinesno/infra/data/scheduler/trigger/service/IJobService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.scheduler.trigger.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.trigger.entity.JobEntity;
+
+public interface IJobService extends IBaseService<JobEntity> {
+}
