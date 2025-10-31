a5072108249fc6c4848c0948c0681a18725f4295
张婷(Tina) <tinaAip@gmail.com>
2025-11-01 00:23:39 +0800

    feat:创建ITriggerService接口，继承IBaseService并关联TriggerEntity实体类。

---
+++ alinesno-infra-data-scheduler-trigger/src/main/java/com/alinesno/infra/data/scheduler/trigger/service/ITriggerService.java
@@ -0,0 +1,7 @@
+package com.alinesno.infra.data.scheduler.trigger.service;
+
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.trigger.entity.TriggerEntity;
+
+public interface ITriggerService extends IBaseService<TriggerEntity> {
+}
