072ef68618120e8c911e3ab93be045d86f75442d
张婷(Tina) <tinaAip@gmail.com>
2025-11-01 00:23:39 +0800

    feat:创建TriggerEntity实体类，包含任务ID和cron表达式字段。

---
+++ alinesno-infra-data-scheduler-trigger/src/main/java/com/alinesno/infra/data/scheduler/trigger/entity/TriggerEntity.java
@@ -0,0 +1,27 @@
+package com.alinesno.infra.data.scheduler.trigger.entity;
+
+import com.alinesno.infra.common.facade.mapper.entity.InfraBaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Trigger 实体：保存 cron 表达式（支持 H(...)）
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("trigger_config")
+@Table(name = "trigger_config")
+public class TriggerEntity extends InfraBaseEntity {
+
+    @TableField
+    @Column(name = "job_id" , comment = "任务ID")
+    private Long jobId;
+
+    @Column(name = "cron" , comment = "cron表达式")
+    @TableField
+    private String cron;
+}
