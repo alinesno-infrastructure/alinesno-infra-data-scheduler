7e69d8b27a61a9fbd3d46fe02cedf04c1839d724
张婷(Tina) <tinaAip@gmail.com>
2025-11-01 00:23:39 +0800

    feat:创建TriggerMapper接口，继承IBaseMapper<TriggerEntity>

---
+++ alinesno-infra-data-scheduler-trigger/src/main/java/com/alinesno/infra/data/scheduler/trigger/mapper/TriggerMapper.java
@@ -0,0 +1,11 @@
+package com.alinesno.infra.data.scheduler.trigger.mapper;
+
+import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
+import com.alinesno.infra.data.scheduler.trigger.entity.TriggerEntity;
+
+/**
+ * TriggerMapper接口
+ */
+public interface TriggerMapper extends IBaseMapper<TriggerEntity> {
+
+}
