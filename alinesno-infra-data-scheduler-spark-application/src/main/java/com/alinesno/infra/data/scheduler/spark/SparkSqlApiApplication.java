761d9f03934032edceabea45629a7bec873a0081
张婷(Tina) <tinaAip@gmail.com>
2025-11-01 00:23:39 +0800

    feat:添加@EnableEncryptableProperties注解，启用加密属性功能。

--- alinesno-infra-data-scheduler-spark-application/src/main/java/com/alinesno/infra/data/scheduler/spark/SparkSqlApiApplication.java
+++ alinesno-infra-data-scheduler-spark-application/src/main/java/com/alinesno/infra/data/scheduler/spark/SparkSqlApiApplication.java
@@ -1,8 +1,13 @@
 package com.alinesno.infra.data.scheduler.spark;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SparkSqlApiApplication
+ */
+@EnableEncryptableProperties
 @SpringBootApplication
 public class SparkSqlApiApplication {
 
