194c0946c266b222381717596a4cd76837379350
张婷(Tina) <tinaAip@gmail.com>
2025-11-02 00:20:40 +0800

    fix:调整WorkerFlowExecutionConsumer接口超时设置

--- alinesno-infra-data-scheduler-adapter/src/main/java/com/alinesno/infra/data/scheduler/adapter/worker/WorkerFlowExecutionConsumer.java
+++ alinesno-infra-data-scheduler-adapter/src/main/java/com/alinesno/infra/data/scheduler/adapter/worker/WorkerFlowExecutionConsumer.java
@@ -2,16 +2,14 @@ package com.alinesno.infra.data.scheduler.adapter.worker;
 
 import com.alinesno.infra.common.facade.pageable.DatatablesPageBean;
 import com.alinesno.infra.common.facade.pageable.TableDataInfo;
-import com.alinesno.infra.common.facade.response.R;
 import com.dtflys.forest.annotation.BaseRequest;
-import com.dtflys.forest.annotation.Header;
 import com.dtflys.forest.annotation.JSONBody;
 import com.dtflys.forest.annotation.Post;
 
 /**
  * 流程执行工作流
  */
-@BaseRequest(baseURL = "#{alinesno.data.scheduler.worker-node}/api/infra/data/scheduler/processInstance" , connectTimeout = 30*1000)
+@BaseRequest(baseURL = "#{alinesno.data.scheduler.worker-node}/api/infra/data/scheduler/processInstance" , readTimeout = 3600_000)
 public interface WorkerFlowExecutionConsumer {
 
     /**
