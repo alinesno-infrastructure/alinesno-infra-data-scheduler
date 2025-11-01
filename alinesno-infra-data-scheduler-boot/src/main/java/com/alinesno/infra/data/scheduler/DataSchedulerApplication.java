d2fc855550c4029e7d9c70f7e85de1f60a57630d
张婷(Tina) <tinaAip@gmail.com>
2025-11-02 00:20:40 +0800

    refactor:移除初始化任务数据的代码，简化启动逻辑。

--- alinesno-infra-data-scheduler-boot/src/main/java/com/alinesno/infra/data/scheduler/DataSchedulerApplication.java
+++ alinesno-infra-data-scheduler-boot/src/main/java/com/alinesno/infra/data/scheduler/DataSchedulerApplication.java
@@ -1,14 +1,7 @@
 package com.alinesno.infra.data.scheduler;
 
-import cn.hutool.core.util.IdUtil;
-import com.alinesno.infra.data.scheduler.trigger.entity.JobEntity;
-import com.alinesno.infra.data.scheduler.trigger.entity.TriggerEntity;
-import com.alinesno.infra.data.scheduler.trigger.service.IJobService;
-import com.alinesno.infra.data.scheduler.trigger.service.ITriggerService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -22,38 +15,4 @@ public class DataSchedulerApplication {
 		SpringApplication.run(DataSchedulerApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner init(IJobService jobService, ITriggerService triggerService) {
-		return args -> {
-			if (jobService.count() == 0) {
-				JobEntity job1 = new JobEntity();
-				job1.setId(IdUtil.getSnowflakeNextId());
-				job1.setName("Demo Job 1");
-				jobService.save(job1);
-
-				JobEntity job2 = new JobEntity();
-				job2.setId(IdUtil.getSnowflakeNextId());
-				job2.setName("Demo Job 2");
-				jobService.save(job2);
-
-				TriggerEntity t1 = new TriggerEntity();
-				t1.setId(IdUtil.getSnowflakeNextId());
-				t1.setJobId(job1.getId());
-				t1.setCron("H * * * *"); // 5-field
-				triggerService.save(t1);
-
-				TriggerEntity t2 = new TriggerEntity();
-				t2.setId(IdUtil.getSnowflakeNextId());
-				t2.setJobId(job2.getId());
-				t2.setCron("0 H * * *");
-				triggerService.save(t2);
-
-				TriggerEntity t3 = new TriggerEntity();
-				t3.setId(IdUtil.getSnowflakeNextId());
-				t3.setJobId(job2.getId());
-				t3.setCron("H(10-20) H/2 * * MON-FRI");
-				triggerService.save(t3);
-			}
-		};
-	}
 }
