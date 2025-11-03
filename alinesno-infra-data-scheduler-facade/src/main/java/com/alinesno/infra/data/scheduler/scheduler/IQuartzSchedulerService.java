83d1a3f3a16d0eedf142b7b36a5b20b25baa81da
刘芳(Luna) <lunaAip@gmail.com>
2025-11-04 00:21:24 +0800

    chore:移除IQuartzSchedulerService.java文件中的包声明和注释

--- alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/scheduler/IQuartzSchedulerService.java
+++ alinesno-infra-data-scheduler-facade/src/main/java/com/alinesno/infra/data/scheduler/scheduler/IQuartzSchedulerService.java
@@ -1,42 +1,42 @@
-package com.alinesno.infra.data.scheduler.scheduler;
-
-import com.alinesno.infra.data.scheduler.enums.JobStatusEnums;
-import lombok.SneakyThrows;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
-/**
- * 分布式调度任务服务接口
- * 用于创建、停止、移除和列举任务
- */
-public interface IQuartzSchedulerService {
-
-    /**
-     * 执行定时任务
-     * @param jobId
-     */
-    void createCronJob(Long jobId , long jobInstanceId) throws SQLException, IOException;
-
-    /**
-     * 添加任务
-     * @param id
-     * @param jobCron
-     */
-    void addJob(String id, String jobCron);
-
-    /**
-     * 更新任务
-     * @param jobId
-     * @param newCron
-     */
-    void updateJobCron(String jobId, String newCron);
-
-    /**
-     * 获取任务状态
-     * @param jobId
-     * @return
-     */
-    @SneakyThrows
-    JobStatusEnums getJobStatus(String jobId);
-}
+//package com.alinesno.infra.data.scheduler.scheduler;
+//
+//import com.alinesno.infra.data.scheduler.enums.JobStatusEnums;
+//import lombok.SneakyThrows;
+//
+//import java.io.IOException;
+//import java.sql.SQLException;
+//
+///**
+// * 分布式调度任务服务接口
+// * 用于创建、停止、移除和列举任务
+// */
+//public interface IQuartzSchedulerService {
+//
+//    /**
+//     * 执行定时任务
+//     * @param jobId
+//     */
+//    void createCronJob(Long jobId , long jobInstanceId) throws SQLException, IOException;
+//
+//    /**
+//     * 添加任务
+//     * @param id
+//     * @param jobCron
+//     */
+//    void addJob(String id, String jobCron);
+//
+//    /**
+//     * 更新任务
+//     * @param jobId
+//     * @param newCron
+//     */
+//    void updateJobCron(String jobId, String newCron);
+//
+//    /**
+//     * 获取任务状态
+//     * @param jobId
+//     * @return
+//     */
+//    @SneakyThrows
+//    JobStatusEnums getJobStatus(String jobId);
+//}
