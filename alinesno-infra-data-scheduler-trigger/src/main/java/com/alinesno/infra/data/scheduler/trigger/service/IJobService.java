76155ef4a2cddb936e8e9bf89e303bccf8a40c55
刘芳(Luna) <lunaAip@gmail.com>
2025-11-04 00:21:24 +0800

    feat:新增任务服务接口方法，包括创建、删除和更新任务计划

--- alinesno-infra-data-scheduler-trigger/src/main/java/com/alinesno/infra/data/scheduler/trigger/service/IJobService.java
+++ alinesno-infra-data-scheduler-trigger/src/main/java/com/alinesno/infra/data/scheduler/trigger/service/IJobService.java
@@ -1,7 +1,30 @@
 package com.alinesno.infra.data.scheduler.trigger.service;
 
 import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.entity.ProcessDefinitionEntity;
 import com.alinesno.infra.data.scheduler.trigger.entity.JobEntity;
 
+/**
+ * 任务服务接口
+ */
 public interface IJobService extends IBaseService<JobEntity> {
+
+    /**
+     * 创建任务
+     * @param processDefinition
+     */
+    void createJob(ProcessDefinitionEntity processDefinition);
+
+    /**
+     * 删除任务
+     * @param processId
+     */
+    void deleteJob(String processId);
+
+    /**
+     * 更新任务计划
+     * @param processId
+     * @param cron
+     */
+    void updateJobCron(String processId , String cron);
 }
