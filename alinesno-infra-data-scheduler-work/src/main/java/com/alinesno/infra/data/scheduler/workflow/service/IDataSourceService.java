c78525fc60269113689b0de208006e7e4bb4e743
王磊(Levi) <leviAip@gmail.com>
2025-11-14 00:24:16 +0800

    feat:创建数据源服务接口IDataSourceService，包含数据库连接校验、获取数据源和获取可用数据源方法。

---
+++ alinesno-infra-data-scheduler-work/src/main/java/com/alinesno/infra/data/scheduler/workflow/service/IDataSourceService.java
@@ -0,0 +1,33 @@
+package com.alinesno.infra.data.scheduler.workflow.service;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alinesno.infra.common.facade.datascope.PermissionQuery;
+import com.alinesno.infra.common.facade.services.IBaseService;
+import com.alinesno.infra.data.scheduler.api.CheckDbConnectResult;
+import com.alinesno.infra.data.scheduler.entity.DataSourceEntity;
+
+import java.util.List;
+
+public interface IDataSourceService extends IBaseService<DataSourceEntity> {
+
+    /**
+     * 数据库连接校验
+     * @param dbListEntity
+     * @return
+     */
+    CheckDbConnectResult checkDbConnect(DataSourceEntity dbListEntity);
+
+    /**
+     * 通过数据库ID获取数据源
+     * @param dataSourceId
+     * @return
+     */
+    DruidDataSource getDataSource(long dataSourceId);
+
+    /**
+     * 获取可用数据源
+     * @param query
+     * @return
+     */
+    List<DataSourceEntity> listAvailableDataSources(PermissionQuery query);
+}
