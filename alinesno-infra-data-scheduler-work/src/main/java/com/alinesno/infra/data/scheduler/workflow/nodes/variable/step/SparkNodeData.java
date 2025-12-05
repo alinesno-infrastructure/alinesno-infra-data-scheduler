d6d2b4865f498dc18bf714e16229f19ac5726c9b
王磊(Levi) <leviAip@gmail.com>
2025-12-06 00:28:13 +0800

    feat: 添加SparkNodeData类的轮询间隔和最大等待时间属性

--- alinesno-infra-data-scheduler-work/src/main/java/com/alinesno/infra/data/scheduler/workflow/nodes/variable/step/SparkNodeData.java
+++ alinesno-infra-data-scheduler-work/src/main/java/com/alinesno/infra/data/scheduler/workflow/nodes/variable/step/SparkNodeData.java
@@ -16,5 +16,9 @@ public class SparkNodeData extends NodeData {
     private String pysparkContent;  // pyspark 内容
     private boolean isAsync = true;  // 是否异步执行
 
+
+    private Integer pollIntervalSeconds = 5 ;  // 轮询间隔5S执行一次
+    private Long maxWaitSeconds = 7200L ; // 最大等待时间60*60*2=7200
+
 }
 
