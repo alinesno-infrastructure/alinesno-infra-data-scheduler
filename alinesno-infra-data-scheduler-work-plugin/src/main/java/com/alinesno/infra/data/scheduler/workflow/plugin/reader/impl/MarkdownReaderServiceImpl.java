d516654d8dc07ac5e84814f3002950304ceb225f
陈伟(Victor) <victorAip@gmail.com>
2025-12-26 04:14:34 +0800

    feat:实现Markdown附件读取服务类MarkdownReaderServiceImpl

---
+++ alinesno-infra-data-scheduler-work-plugin/src/main/java/com/alinesno/infra/data/scheduler/workflow/plugin/reader/impl/MarkdownReaderServiceImpl.java
@@ -0,0 +1,39 @@
+package com.alinesno.infra.data.scheduler.workflow.plugin.reader.impl;
+
+import com.alinesno.infra.data.scheduler.adapter.dto.FileAttachmentDto;
+import com.alinesno.infra.data.scheduler.workflow.plugin.extractor.dto.UploadData;
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+/**
+ * MarkdownReaderServiceImpl 类是 Markdown 附件读取服务的具体实现，
+ * 它实现了 IAttachmentReaderService 接口，主要负责读取 Markdown 格式的附件。
+ * 此类提供了读取指定 Markdown 附件内容的功能。
+ */
+@Scope("prototype")
+@Service
+public class MarkdownReaderServiceImpl extends BaseReaderServiceImpl {
+
+    /**
+     * 该方法用于读取指定 ID 的 Markdown 附件内容。
+     *
+     * @param attachmentDto 要读取的 Markdown 附件的唯一标识符。
+     * @param uploadData
+     * @return 返回读取到的 Markdown 附件内容，如果读取失败或者附件无内容则返回 null。
+     */
+    @SneakyThrows
+    @Override
+    public String readAttachment(FileAttachmentDto attachmentDto, UploadData uploadData) {
+        File file = getFileById(attachmentDto.getFileId(), attachmentDto.getFileType());
+        // 读取文件内容
+        if (file != null) {
+            String content = FileUtils.readFileToString(file, "UTF-8") ;
+            return cleanText(content);
+        }
+        return null ;
+    }
+}
