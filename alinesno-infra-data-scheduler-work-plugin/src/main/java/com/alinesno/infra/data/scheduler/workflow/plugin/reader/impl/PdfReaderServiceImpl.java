9d19e8b91ac1061dc05f12e9262ca8abc4a69666
陈伟(Victor) <victorAip@gmail.com>
2025-12-26 04:14:34 +0800

    feat:实现PDF附件读取服务PdfReaderServiceImpl类

---
+++ alinesno-infra-data-scheduler-work-plugin/src/main/java/com/alinesno/infra/data/scheduler/workflow/plugin/reader/impl/PdfReaderServiceImpl.java
@@ -0,0 +1,52 @@
+package com.alinesno.infra.data.scheduler.workflow.plugin.reader.impl;
+
+import com.agentsflex.core.document.Document;
+import com.agentsflex.document.parser.PdfBoxDocumentParser;
+import com.alinesno.infra.data.scheduler.adapter.dto.FileAttachmentDto;
+import com.alinesno.infra.data.scheduler.workflow.plugin.extractor.dto.UploadData;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+/**
+ * PdfReaderServiceImpl 类是 PDF 附件读取服务的具体实现，
+ * 它实现了 IAttachmentReaderService 接口，专门用于处理 PDF 附件的读取操作。
+ * 通过该类可以根据附件的唯一标识来读取对应的 PDF 附件内容。
+ */
+@Slf4j
+@Scope("prototype")
+@Service
+public class PdfReaderServiceImpl  extends BaseReaderServiceImpl {
+
+    /**
+     * 此方法用于根据给定的附件 ID 读取对应的 PDF 附件内容。
+     *
+     * @param attachmentDto 要读取的 PDF 附件的唯一标识符。
+     * @param uploadData
+     * @return 返回读取到的 PDF 附件内容，若读取失败或无内容则返回 null。
+     */
+    @SneakyThrows
+    @Override
+    public String readAttachment(FileAttachmentDto attachmentDto, UploadData uploadData) {
+
+        File file ;
+        if(attachmentDto.getFile() != null){
+            file = attachmentDto.getFile() ;
+        }else{
+            file = getFileById(attachmentDto.getFileId(), attachmentDto.getFileType());
+        }
+
+        FileInputStream stream = new FileInputStream(file);
+        PdfBoxDocumentParser parser = new PdfBoxDocumentParser();
+        Document document = parser.parse(stream);
+
+        String pdfContent = document.getContent() ;
+        log.debug("pdfContent = {}" , pdfContent);
+
+        return pdfContent ; // cleanText(document.getContent());
+    }
+}
