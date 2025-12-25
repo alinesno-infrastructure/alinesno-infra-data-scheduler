e9893114bb55d719eefc03b38e50be2663086911
陈伟(Victor) <victorAip@gmail.com>
2025-12-26 04:14:34 +0800

    feat:实现CSV附件读取服务，支持读取CSV文件内容

---
+++ alinesno-infra-data-scheduler-work-plugin/src/main/java/com/alinesno/infra/data/scheduler/workflow/plugin/reader/impl/CvsReaderServiceImpl.java
@@ -0,0 +1,62 @@
+package com.alinesno.infra.data.scheduler.workflow.plugin.reader.impl;
+
+
+import com.alinesno.infra.data.scheduler.adapter.dto.FileAttachmentDto;
+import com.alinesno.infra.data.scheduler.workflow.plugin.extractor.dto.UploadData;
+import lombok.SneakyThrows;
+import com.opencsv.CSVReader;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * CvsReaderServiceImpl 类是 CSV 附件读取服务的具体实现，
+ * 它实现了 IAttachmentReaderService 接口，用于处理 CSV 附件的读取操作。
+ * 该类提供了读取指定 CSV 附件内容的功能，具体实现逻辑由 readAttachment 方法完成。
+ */
+@Scope("prototype")
+@Service
+public class CvsReaderServiceImpl extends BaseReaderServiceImpl {
+
+    /**
+     * 此方法用于读取指定 ID 的 CSV 附件内容。
+     *
+     * @param attachmentDto 要读取的 CSV 附件的唯一标识符。
+     * @param uploadData
+     * @return 返回读取到的 CSV 附件内容，若读取失败或无内容则返回 null。
+     */
+    @SneakyThrows
+    @Override
+    public String readAttachment(FileAttachmentDto attachmentDto, UploadData uploadData) {
+
+        File file ;
+        if(attachmentDto.getFile() != null){
+            file = attachmentDto.getFile() ;
+        }else{
+            file = getFileById(attachmentDto.getFileId(), attachmentDto.getFileType());
+        }
+
+        if (file == null || !file.exists()) {
+            return null;
+        }
+
+        try (CSVReader reader = new CSVReader(new FileReader(file, StandardCharsets.UTF_8))) {
+            StringBuilder content = new StringBuilder();
+            String[] nextLine;
+
+            while ((nextLine = reader.readNext()) != null) {
+                content.append(Arrays.stream(nextLine)
+                                .map(s -> s == null ? "" : s)
+                                .collect(Collectors.joining(" | ")))
+                        .append("\n");
+            }
+
+            return content.toString();
+        }
+    }
+}
