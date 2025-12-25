e6bb8f9d73962356e8c4da6b717eb46e2ad4e622
陈伟(Victor) <victorAip@gmail.com>
2025-12-26 04:14:34 +0800

    feat: 创建文件附件数据传输对象 FileAttachmentDto

---
+++ alinesno-infra-data-scheduler-adapter/src/main/java/com/alinesno/infra/data/scheduler/adapter/dto/FileAttachmentDto.java
@@ -0,0 +1,64 @@
+package com.alinesno.infra.data.scheduler.adapter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.File;
+
+/**
+ * 聊天消息中的文件列表
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FileAttachmentDto {
+
+    /**
+     * 文件 ID
+     */
+    private long fileId;
+
+    /**
+     * 文件长度
+     */
+    private long length;
+
+    /**
+     * 文件名
+     */
+    private String fileName;
+
+    /**
+     * 文件类型
+     */
+    private String fileType;
+
+    /**
+     * 文件内容
+     */
+    private String fileContent = "" ;
+
+    /**
+     * 文字数量
+     */
+    private int wordCount;
+
+    /**
+     * 附件顺序
+     */
+    private int order;
+
+    /**
+     * 一对一聊天 ID
+     */
+    private String oneChatId ;
+
+    /**
+     * 已经传递指定的文件
+     */
+    private File file ;
+
+}
